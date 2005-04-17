fcbb3c8b389bd1568a52f06e8066fd529e5173a0
Junio C Hamano <junkio@cox.net>
2005-04-17 09:53:35 -0700

    [PATCH] show-diff.c: do not include unused header file
    This is my bad.  I added #include <ctype.h> to the file,
    which I ended up not using and failed to remove it.
    
    Signed-off-by: Junio C Hamano <junkio@cox.net>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>
    
    cregit: 8f41523fc1a8cd127ff39fa111b3b5bb5105cc84 http://github.com/git/git/commit/8f41523fc1a8cd127ff39fa111b3b5bb5105cc84

--- show-diff.c
+++ show-diff.c
@@ -7,11 +7,6 @@ include|#
 directive|include
 file|"cache.h"
 end_include
-begin_include
-include|#
-directive|include
-file|<ctype.h>
-end_include
 begin_decl_stmt
 DECL|variable|diff_cmd
 specifier|static
