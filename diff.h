54550aab0435b08610df5067f3100f95cbe1471f
Junio C Hamano <junkio@cox.net>
2005-04-26 09:25:05 -0700

    [PATCH] Diff-tree-helper take two.
    This reworks the diff-tree-helper and show-diff to further make external
    diff command interface simpler.
    
    These commands now honor GIT_EXTERNAL_DIFF environment variable which
    can point at an arbitrary program that takes 7 parameters:
    
      name file1 file1-sha1 file1-mode file2 file2-sha1 file2-mode
    
    The parameters for an external diff command are as follows:
    
      name        this invocation of the command is to emit diff
    	      for the named cache/tree entry.
    
      file1       pathname that holds the contents of the first
    	      file.  This can be a file inside the working
    	      tree, or a temporary file created from the blob
    	      object, or /dev/null.  The command should not
    	      attempt to unlink it -- the temporary is
    	      unlinked by the caller.
    
      file1-sha1  sha1 hash if file1 is a blob object, or "."
    	      otherwise.
    
      file1-mode  mode bits for file1, or "." for a deleted file.
    
    If GIT_EXTERNAL_DIFF environment variable is not set, the
    default is to invoke diff with the set of parameters old
    show-diff used to use.  This built-in implementation honors the
    GIT_DIFF_CMD and GIT_DIFF_OPTS environment variables as before.
    
    Signed-off-by: Junio C Hamano <junkio@cox.net>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>
    
    cregit: be3cfa85f45e32722a65349d023667be906a66b6 http://github.com/git/git/commit/be3cfa85f45e32722a65349d023667be906a66b6

--- diff.h
+++ diff.h
@@ -1,4 +1,7 @@
 begin_unit
+begin_comment
+comment|/*  * Copyright (C) 2005 Junio C Hamano  */
+end_comment
 begin_ifndef
 ifndef|#
 directive|ifndef
@@ -10,46 +13,22 @@ define|#
 directive|define
 name|DIFF_H
 end_define
-begin_function_decl
-specifier|extern
-name|void
-name|prepare_diff_cmd
-parameter_list|(
-name|void
-parameter_list|)
-function_decl|;
-end_function_decl
+begin_comment
+comment|/* These two are for backward compatibility with show-diff;  * new users should not use them.  */
+end_comment
 begin_function_decl
 specifier|extern
 name|void
 name|show_differences
 parameter_list|(
 specifier|const
-name|char
+name|struct
+name|cache_entry
 modifier|*
-name|name
+name|ce
 parameter_list|,
-comment|/* filename on the filesystem */
-specifier|const
-name|char
-modifier|*
-name|label
-parameter_list|,
-comment|/* diff label to use */
-name|void
-modifier|*
-name|old_contents
-parameter_list|,
-comment|/* contents in core */
-name|unsigned
-name|long
-name|long
-name|old_size
-parameter_list|,
-comment|/* size in core */
 name|int
 name|reverse
-comment|/* 0: diff core file 					    1: diff file core */
 parameter_list|)
 function_decl|;
 end_function_decl
@@ -59,18 +38,83 @@ name|void
 name|show_diff_empty
 parameter_list|(
 specifier|const
-name|unsigned
+name|struct
+name|cache_entry
+modifier|*
+name|ce
+parameter_list|,
+name|int
+name|reverse
+parameter_list|)
+function_decl|;
+end_function_decl
+begin_struct
+DECL|struct|diff_spec
+struct|struct
+name|diff_spec
+block|{
+union|union
+block|{
+DECL|member|name
+specifier|const
 name|char
 modifier|*
+name|name
+decl_stmt|;
+comment|/* path on the filesystem */
+DECL|member|sha1
+name|unsigned
+name|char
 name|sha1
-parameter_list|,
+index|[
+literal|20
+index|]
+decl_stmt|;
+comment|/* blob object ID */
+block|}
+DECL|member|u
+name|u
+union|;
+DECL|member|mode
+name|unsigned
+name|short
+name|mode
+decl_stmt|;
+comment|/* file mode */
+DECL|member|sha1_valid
+name|unsigned
+name|sha1_valid
+range|:
+literal|1
+decl_stmt|;
+comment|/* if true, use u.sha1 and trust mode. 				  * (however with a NULL SHA1, read them 				  * from the file!). 				  * if false, use u.name and read mode from 				  * the filesystem. 				  */
+DECL|member|file_valid
+name|unsigned
+name|file_valid
+range|:
+literal|1
+decl_stmt|;
+comment|/* if false the file does not even exist */
+block|}
+struct|;
+end_struct
+begin_function_decl
+specifier|extern
+name|void
+name|run_external_diff
+parameter_list|(
 specifier|const
 name|char
 modifier|*
 name|name
 parameter_list|,
-name|int
-name|reverse
+name|struct
+name|diff_spec
+modifier|*
+parameter_list|,
+name|struct
+name|diff_spec
+modifier|*
 parameter_list|)
 function_decl|;
 end_function_decl
