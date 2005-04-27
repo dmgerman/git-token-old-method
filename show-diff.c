83afc7fd7b903d3369b9bad47ed138773f2eedb0
Linus Torvalds <torvalds@ppc970.osdl.org>
2005-04-26 17:37:11 -0700

    show-diff: don't print out the full "ce" format for unmerged files
    It really doesn't make sense, since there are potentially _many_ different
    cache entries for an unmerged file. So just do the "U pathname" thing.
    
    cregit: b8a55ce71d0d46204d64fe35787956e994d2a802 http://github.com/git/git/commit/b8a55ce71d0d46204d64fe35787956e994d2a802

--- show-diff.c
+++ show-diff.c
@@ -428,11 +428,15 @@ name|ce
 argument_list|)
 condition|)
 block|{
-name|show_file
+name|printf
 argument_list|(
-literal|"U"
+literal|"U %s%c"
 argument_list|,
 name|ce
+operator|->
+name|name
+argument_list|,
+name|line_termination
 argument_list|)
 expr_stmt|;
 while|while
