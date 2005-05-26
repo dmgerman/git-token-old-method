2a394218c6a70b2e54c3516646a443fa697ca147
Junio C Hamano <junkio@cox.net>
2005-05-26 02:24:30 -0700

    [PATCH] Diff updates to express type changes
    With the introduction of type 'T' in the diff-raw output, and
    the "apply-patch" program Linus has been quietly working on
    without much advertisement, it started to make sense to emit
    usable information in the "diff --git" patch output format as
    well.  Earlier built-in diff driver punted and did not say
    anything about a symbolic link changing into a file or vice
    versa, but this version represents it as a pair of deletion
    and creation.
    
    It also fixes a minor problem dealing with old archive created
    with ancient git.  The earlier code was reporting file mode
    change between 100664 and 100644 (we shouldn't).  The linux-2.6
    git tree has a good example that exposes this problem.  A good
    test case is commit ce1dc02f76432a46db149241e015a4f782974623.
    
    Signed-off-by: Junio C Hamano <junkio@cox.net>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>
    
    cregit: 4130b99571903fb93e4c6e0d6677be7b6b986426 http://github.com/git/git/commit/4130b99571903fb93e4c6e0d6677be7b6b986426

--- diffcore.h
+++ diffcore.h
@@ -214,6 +214,27 @@ parameter_list|)
 define|\
 value|((S_IFMT& (p)->one->mode) != (S_IFMT& (p)->two->mode))
 end_define
+begin_define
+DECL|macro|DIFF_PAIR_MODE_CHANGED
+define|#
+directive|define
+name|DIFF_PAIR_MODE_CHANGED
+parameter_list|(
+name|p
+parameter_list|)
+value|((p)->one->mode != (p)->two->mode)
+end_define
+begin_define
+DECL|macro|DIFF_FILE_CANON_MODE
+define|#
+directive|define
+name|DIFF_FILE_CANON_MODE
+parameter_list|(
+name|mode
+parameter_list|)
+define|\
+value|(S_ISREG(mode) ? (S_IFREG | ce_permissions(mode)) : \ 	S_ISLNK(mode) ? S_IFLNK : S_IFDIR)
+end_define
 begin_function_decl
 specifier|extern
 name|int
