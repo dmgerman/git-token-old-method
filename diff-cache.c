ae83859b3cb5699d37a6276aedb8690fd46857cf
Linus Torvalds <torvalds@g5.osdl.org>
2005-07-14 13:19:19 -0700

    Start using the partial tree reading in "git-diff-cache"
    The reason I say "start using" is that we really should also limit the
    index checking by name - now we limit the tree object accesses by name,
    but we still check the whole index.
    
    Still, this should help.
    
    cregit: a74ba54bd15d63bfb962b584fa96937b06cdfc87 http://github.com/git/git/commit/a74ba54bd15d63bfb962b584fa96937b06cdfc87

--- diff-cache.c
+++ diff-cache.c
@@ -1331,7 +1331,7 @@ name|size
 argument_list|,
 literal|1
 argument_list|,
-name|NULL
+name|pathspec
 argument_list|)
 condition|)
 name|die
@@ -1353,9 +1353,6 @@ expr_stmt|;
 name|diffcore_std
 argument_list|(
 name|pathspec
-condition|?
-else|:
-name|NULL
 argument_list|,
 name|detect_rename
 argument_list|,
