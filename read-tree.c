1e472225aacfd9aefc5cdbda005e0835895021ec
Junio C Hamano <junkio@cox.net>
2005-09-10 17:46:27 -0700

    Add a new merge strategy by Fredrik Kuivinen.
    I really wanted to try this out, instead of asking for an adjustment
    to the 'git merge' driver and waiting.  For now the new strategy is
    called 'fredrik' and not in the list of default strategies to be tried.
    
    The script wants Python 2.4 so this commit also adjusts Debian and RPM
    build procecure files.
    
    Signed-off-by: Junio C Hamano <junkio@cox.net>
    
    cregit: 720d150c48fc35fca13c6dfb3c76d60e4ee83b87 http://github.com/git/git/commit/720d150c48fc35fca13c6dfb3c76d60e4ee83b87

--- read-tree.c
+++ read-tree.c
@@ -43,6 +43,15 @@ literal|0
 decl_stmt|;
 end_decl_stmt
 begin_decl_stmt
+DECL|variable|index_only
+specifier|static
+name|int
+name|index_only
+init|=
+literal|0
+decl_stmt|;
+end_decl_stmt
+begin_decl_stmt
 DECL|variable|head_idx
 specifier|static
 name|int
@@ -1624,6 +1633,11 @@ name|st
 decl_stmt|;
 if|if
 condition|(
+name|index_only
+condition|)
+return|return;
+if|if
+condition|(
 operator|!
 name|lstat
 argument_list|(
@@ -2790,7 +2804,7 @@ name|char
 name|read_tree_usage
 index|[]
 init|=
-literal|"git-read-tree (<sha> | -m [-u]<sha1> [<sha2> [<sha3>]])"
+literal|"git-read-tree (<sha> | -m [-u | -i]<sha1> [<sha2> [<sha3>]])"
 decl_stmt|;
 end_decl_stmt
 begin_decl_stmt
@@ -2892,7 +2906,7 @@ index|[
 name|i
 index|]
 decl_stmt|;
-comment|/* "-u" means "update", meaning that a merge will update the working directory */
+comment|/* "-u" means "update", meaning that a merge will update 		 * the working tree. 		 */
 if|if
 condition|(
 operator|!
@@ -2905,6 +2919,24 @@ argument_list|)
 condition|)
 block|{
 name|update
+operator|=
+literal|1
+expr_stmt|;
+continue|continue;
+block|}
+comment|/* "-i" means "index only", meaning that a merge will 		 * not even look at the working tree. 		 */
+if|if
+condition|(
+operator|!
+name|strcmp
+argument_list|(
+name|arg
+argument_list|,
+literal|"-i"
+argument_list|)
+condition|)
+block|{
+name|index_only
 operator|=
 literal|1
 expr_stmt|;
@@ -3015,6 +3047,20 @@ literal|1
 expr_stmt|;
 continue|continue;
 block|}
+comment|/* using -u and -i at the same time makes no sense */
+if|if
+condition|(
+literal|1
+operator|<
+name|index_only
+operator|+
+name|update
+condition|)
+name|usage
+argument_list|(
+name|read_tree_usage
+argument_list|)
+expr_stmt|;
 if|if
 condition|(
 name|get_sha1
