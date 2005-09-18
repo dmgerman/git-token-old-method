dae8a423c25621f6f5c99ed0a7db771d3bd06f05
Junio C Hamano <junkio@cox.net>
2005-09-18 14:11:53 -0700

    fetch() assumes we do not have the object.
    Bugfix for the previous one.
    
    Signed-off-by: Junio C Hamano <junkio@cox.net>
    
    cregit: 029f6de377c7e0484f5c4cf070934599580f1784 http://github.com/git/git/commit/029f6de377c7e0484f5c4cf070934599580f1784

--- fetch.c
+++ fetch.c
@@ -916,6 +916,14 @@ condition|)
 block|{
 if|if
 condition|(
+operator|!
+name|has_sha1_file
+argument_list|(
+name|obj
+operator|->
+name|sha1
+argument_list|)
+operator|&&
 name|fetch
 argument_list|(
 name|obj
