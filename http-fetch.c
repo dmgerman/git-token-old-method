4d23b4f0e4db731064b72e95cf651f9a914c07bf
Petr Baudis <pasky@suse.cz>
2005-11-12 01:49:59 +0100

    Show URL in the "Getting <foo> list" http-fetch messages
    Signed-off-by: Petr Baudis <pasky@suse.cz>
    Signed-off-by: Junio C Hamano <junkio@cox.net>
    
    cregit: 6fd72e39af8a69615d32c3fd4d5fe0788f54a2ea http://github.com/git/git/commit/6fd72e39af8a69615d32c3fd4d5fe0788f54a2ea

--- http-fetch.c
+++ http-fetch.c
@@ -4115,7 +4115,9 @@ name|fprintf
 argument_list|(
 name|stderr
 argument_list|,
-literal|"Getting alternates list\n"
+literal|"Getting alternates list for %s\n"
+argument_list|,
+name|base
 argument_list|)
 expr_stmt|;
 name|url
@@ -4821,7 +4823,11 @@ name|fprintf
 argument_list|(
 name|stderr
 argument_list|,
-literal|"Getting pack list\n"
+literal|"Getting pack list for %s\n"
+argument_list|,
+name|repo
+operator|->
+name|base
 argument_list|)
 expr_stmt|;
 name|url
