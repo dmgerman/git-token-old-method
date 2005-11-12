1d57921db525c031ebc248bdcf4328cff116ba7e
Kai Ruemmler <kai.ruemmler@gmx.net>
2005-11-12 17:33:24 +0100

    Fix compilation warnings in pack-redundant.c
    This fixes compilation warnings where "%ld" was used to print values of
    type size_t.
    
    Signed-off-by: Kai Ruemmler <kai.ruemmler@gmx.net>
    Signed-off-by: Junio C Hamano <junkio@cox.net>
    
    cregit: abacbe4166bcf9d62f430a4a75de2e82df3799b1 http://github.com/git/git/commit/abacbe4166bcf9d62f430a4a75de2e82df3799b1

--- pack-redundant.c
+++ pack-redundant.c
@@ -2963,8 +2963,12 @@ name|fprintf
 argument_list|(
 name|stderr
 argument_list|,
-literal|"There are %ld packs available in alt-odbs.\n"
+literal|"There are %lu packs available in alt-odbs.\n"
 argument_list|,
+operator|(
+name|unsigned
+name|long
+operator|)
 name|pack_list_size
 argument_list|(
 name|altodb_packs
@@ -3011,14 +3015,22 @@ name|fprintf
 argument_list|(
 name|stderr
 argument_list|,
-literal|"containing %ld duplicate objects "
-literal|"with a total size of %ldkb.\n"
+literal|"containing %lu duplicate objects "
+literal|"with a total size of %lukb.\n"
 argument_list|,
+operator|(
+name|unsigned
+name|long
+operator|)
 name|get_pack_redundancy
 argument_list|(
 name|min
 argument_list|)
 argument_list|,
+operator|(
+name|unsigned
+name|long
+operator|)
 name|pack_set_bytecount
 argument_list|(
 name|min
@@ -3031,8 +3043,12 @@ name|fprintf
 argument_list|(
 name|stderr
 argument_list|,
-literal|"A total of %ld unique objects were considered.\n"
+literal|"A total of %lu unique objects were considered.\n"
 argument_list|,
+operator|(
+name|unsigned
+name|long
+operator|)
 name|all_objects
 operator|->
 name|size
