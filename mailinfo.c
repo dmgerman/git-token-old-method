cf9f2f3b42b9b58341c91101a0be163e4dc111b0
Jason Riedy <ejr@EECS.Berkeley.EDU>
2005-12-06 14:20:16 -0800

    [PATCH] Initial AIX portability fixes.
    Added an AIX clause in the Makefile; that clause likely
    will be wrong for any AIX pre-5.2, but I can only test
    on 5.3.  mailinfo.c was missing the compat header file,
    and convert-objects.c needs to define a specific
    _XOPEN_SOURCE as well as _XOPEN_SOURCE_EXTENDED.
    
    Signed-off-by: E. Jason Riedy <ejr@cs.berkeley.edu>
    Signed-off-by: Junio C Hamano <junkio@cox.net>
    
    cregit: a6da9395a5b6d3df901ce0a6cb61d123f77d7342 http://github.com/git/git/commit/a6da9395a5b6d3df901ce0a6cb61d123f77d7342

--- mailinfo.c
+++ mailinfo.c
@@ -36,6 +36,11 @@ end_include
 begin_include
 include|#
 directive|include
+file|"git-compat-util.h"
+end_include
+begin_include
+include|#
+directive|include
 file|"cache.h"
 end_include
 begin_decl_stmt
