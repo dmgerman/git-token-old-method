4b4fe2621dfad9cf9f978ad4952fda6cbee1e90f
Junio C Hamano <junkio@cox.net>
2006-04-16 18:12:49 -0700

    rev-list --boundary: show boundary commits even when limited otherwise.
    The boundary commits are shown for UI like gitk to draw them as
    soon as topo-order sorting allows, and should not be omitted by
    get_revision() filtering logic.  As long as their immediate
    child commits are shown, we should not filter them out.
    
    Signed-off-by: Junio C Hamano <junkio@cox.net>
    
    cregit: 1b65a5aa449e2ea99a68f5d13c5521d0dc64dfd1 http://github.com/git/git/commit/1b65a5aa449e2ea99a68f5d13c5521d0dc64dfd1

--- http-push.c
+++ http-push.c
@@ -305,35 +305,35 @@ name|LOCK_REFRESH
 value|30
 end_define
 begin_comment
-comment|/* bits #0-6 in revision.h */
+comment|/* bits #0-15 in revision.h */
 end_comment
 begin_define
 DECL|macro|LOCAL
 define|#
 directive|define
 name|LOCAL
-value|(1u<< 7)
+value|(1u<<16)
 end_define
 begin_define
 DECL|macro|REMOTE
 define|#
 directive|define
 name|REMOTE
-value|(1u<< 8)
+value|(1u<<17)
 end_define
 begin_define
 DECL|macro|FETCHING
 define|#
 directive|define
 name|FETCHING
-value|(1u<< 9)
+value|(1u<<18)
 end_define
 begin_define
 DECL|macro|PUSHING
 define|#
 directive|define
 name|PUSHING
-value|(1u<< 10)
+value|(1u<<19)
 end_define
 begin_comment
 comment|/* We allow "recursive" symbolic refs. Only within reason, though */
