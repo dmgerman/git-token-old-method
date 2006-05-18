f3ba844f445fcd669a1344d4d4e68c3ff9f00ab5
Junio C Hamano <junkio@cox.net>
2006-05-18 14:35:37 -0700

    builtin-diff: do not say files are renamed when blob and file are given
    Signed-off-by: Junio C Hamano <junkio@cox.net>
    
    cregit: 065e0b126f47e0c280da6830956766ff2ea8cf17 http://github.com/git/git/commit/065e0b126f47e0c280da6830956766ff2ea8cf17

--- builtin-diff.c
+++ builtin-diff.c
@@ -656,12 +656,7 @@ name|sha1
 argument_list|,
 name|null_sha1
 argument_list|,
-name|blob
-index|[
-literal|0
-index|]
-operator|.
-name|name
+name|path
 argument_list|,
 name|path
 argument_list|)
