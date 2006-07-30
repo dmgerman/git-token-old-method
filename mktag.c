3fad41ec6d98404667350d1e3c7687d1278ebd30
Ramsay Allan Jones <ramsay@ramsay1.demon.co.uk>
2006-07-30 16:38:28 +0100

    Add NO_C99_FORMAT to support older compilers.
    The NO_C99_FORMAT macro allows compilers that lack support for the
    ll,hh,j,z,t size specifiers (eg. gcc 2.95.2) to adapt the code to avoid
    runtime errors in the formatted IO functions.
    
    Signed-off-by: Ramsay Allan Jones <ramsay@ramsay1.demon.co.uk>
    Signed-off-by: Junio C Hamano <junkio@cox.net>
    
    cregit: 579d1fbfaf25550254014fa472faac95f88eb779 http://github.com/git/git/commit/579d1fbfaf25550254014fa472faac95f88eb779

--- mktag.c
+++ mktag.c
@@ -111,6 +111,33 @@ name|ret
 return|;
 block|}
 end_function
+begin_ifdef
+ifdef|#
+directive|ifdef
+name|NO_C99_FORMAT
+end_ifdef
+begin_define
+DECL|macro|PD_FMT
+define|#
+directive|define
+name|PD_FMT
+value|"%d"
+end_define
+begin_else
+else|#
+directive|else
+end_else
+begin_define
+DECL|macro|PD_FMT
+define|#
+directive|define
+name|PD_FMT
+value|"%td"
+end_define
+begin_endif
+endif|#
+directive|endif
+end_endif
 begin_function
 DECL|function|verify_tag
 specifier|static
@@ -264,7 +291,9 @@ condition|)
 return|return
 name|error
 argument_list|(
-literal|"char%td: could not find next \"\\n\""
+literal|"char"
+name|PD_FMT
+literal|": could not find next \"\\n\""
 argument_list|,
 name|type_line
 operator|-
@@ -295,7 +324,9 @@ condition|)
 return|return
 name|error
 argument_list|(
-literal|"char%td: no \"tag \" found"
+literal|"char"
+name|PD_FMT
+literal|": no \"tag \" found"
 argument_list|,
 name|tag_line
 operator|-
@@ -326,7 +357,9 @@ condition|)
 return|return
 name|error
 argument_list|(
-literal|"char%td: type too long"
+literal|"char"
+name|PD_FMT
+literal|": type too long"
 argument_list|,
 name|type_line
 operator|+
@@ -412,7 +445,9 @@ continue|continue;
 return|return
 name|error
 argument_list|(
-literal|"char%td: could not verify tag name"
+literal|"char"
+name|PD_FMT
+literal|": could not verify tag name"
 argument_list|,
 name|tag_line
 operator|-
@@ -448,7 +483,9 @@ condition|)
 return|return
 name|error
 argument_list|(
-literal|"char%td: could not find \"tagger\""
+literal|"char"
+name|PD_FMT
+literal|": could not find \"tagger\""
 argument_list|,
 name|tagger_line
 operator|-
@@ -463,6 +500,12 @@ literal|0
 return|;
 block|}
 end_function
+begin_undef
+DECL|macro|PD_FMT
+undef|#
+directive|undef
+name|PD_FMT
+end_undef
 begin_function
 DECL|function|main
 name|int
