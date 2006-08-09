d5bf0a9404565d0321541c88c26c99e557a3f22a
Junio C Hamano <junkio@cox.net>
2006-08-09 12:45:27 -0700

    check return value from diff_setup_done()
    Signed-off-by: Junio C Hamano <junkio@cox.net>
    
    cregit: 72ee96c0f1861fcdb9eaf39e78d86888437a27a9 http://github.com/git/git/commit/72ee96c0f1861fcdb9eaf39e78d86888437a27a9

--- revision.c
+++ revision.c
@@ -4854,12 +4854,21 @@ name|revs
 operator|->
 name|abbrev
 expr_stmt|;
+if|if
+condition|(
 name|diff_setup_done
 argument_list|(
 operator|&
 name|revs
 operator|->
 name|diffopt
+argument_list|)
+operator|<
+literal|0
+condition|)
+name|die
+argument_list|(
+literal|"diff_setup_done failed"
 argument_list|)
 expr_stmt|;
 return|return
