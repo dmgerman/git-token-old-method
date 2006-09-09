d3ebfd30edd051606f1c5c430da8537ae8e7ced9
Rene Scharfe <rene.scharfe@lsrfire.ath.cx>
2006-09-09 17:02:38 +0200

    git-archive: make compression level of ZIP archives configurable
    Signed-off-by: Rene Scharfe <rene.scharfe@lsrfire.ath.cx>
    Signed-off-by: Junio C Hamano <junkio@cox.net>
    
    cregit: 854c4168e77a692dc198311f04bf31939355f2a3 http://github.com/git/git/commit/854c4168e77a692dc198311f04bf31939355f2a3

--- builtin-archive.c
+++ builtin-archive.c
@@ -71,7 +71,7 @@ operator|.
 name|write_archive
 operator|=
 name|write_tar_archive
-block|}
+block|, 	}
 block|,
 block|{
 operator|.
@@ -83,7 +83,12 @@ operator|.
 name|write_archive
 operator|=
 name|write_zip_archive
-block|}
+block|,
+operator|.
+name|parse_extra
+operator|=
+name|parse_extra_zip_args
+block|, 	}
 block|, }
 decl_stmt|;
 end_decl_stmt
