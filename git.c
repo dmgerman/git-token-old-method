4b5204769469af84107666e4a36d3888e674bd6d
Junio C Hamano <junkio@cox.net>
2006-10-19 16:00:04 -0700

    git-pickaxe: blame rewritten.
    Currently it does what git-blame does, but only faster.
    
    More importantly, its internal structure is designed to support
    content movement (aka cut-and-paste) more easily by allowing
    more than one paths to be taken from the same commit.
    
    Signed-off-by: Junio C Hamano <junkio@cox.net>
    
    cregit: cee7f245dcaef6dade28464f59420095a9949aac http://github.com/git/git/commit/cee7f245dcaef6dade28464f59420095a9949aac

--- git.c
+++ git.c
@@ -1405,6 +1405,14 @@ name|RUN_SETUP
 block|}
 block|,
 block|{
+literal|"pickaxe"
+block|,
+name|cmd_pickaxe
+block|,
+name|RUN_SETUP
+block|}
+block|,
+block|{
 literal|"prune"
 block|,
 name|cmd_prune
