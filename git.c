98368ad0b5fe989d9b33770e8d4da892ada8602d
Johannes Schindelin <Johannes.Schindelin@gmx.de>
2006-12-20 17:39:41 +0100

    Make git-rerere a builtin
    The perl version used modules which are non-standard in some setups.
    This patch brings the full power of rerere to a wider audience.
    
    Signed-off-by: Johannes Schindelin <Johannes.Schindelin@gmx.de>
    Signed-off-by: Junio C Hamano <junkio@cox.net>
    
    cregit: 658f3650273abd6888c964b9fb1291c603306df8 http://github.com/git/git/commit/658f3650273abd6888c964b9fb1291c603306df8

--- git.c
+++ git.c
@@ -1444,6 +1444,14 @@ name|cmd_repo_config
 block|}
 block|,
 block|{
+literal|"rerere"
+block|,
+name|cmd_rerere
+block|,
+name|RUN_SETUP
+block|}
+block|,
+block|{
 literal|"rev-list"
 block|,
 name|cmd_rev_list
