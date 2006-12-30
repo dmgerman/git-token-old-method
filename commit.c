a5dd8a632acb02edc560b20fe2461cbcfc4155fd
Junio C Hamano <junkio@cox.net>
2006-12-30 02:18:24 -0800

    commit re-encoding: fix confusion between no and default conversion.
    Telling the git-log family not to do any character conversion is
    done with --encoding=none, which sets log_output_encoding to an
    empty string.  However, logmsg_reencode() confused this with
    log_output_encoding and commit_encoding set to NULL.  The latter
    means we should use the default encoding (i.e. utf-8).
    
    Signed-off-by: Junio C Hamano <junkio@cox.net>
    
    cregit: 4b46e22d48271d1a220133a925dc5009048eb577 http://github.com/git/git/commit/4b46e22d48271d1a220133a925dc5009048eb577

--- commit.c
+++ commit.c
@@ -3513,6 +3513,17 @@ condition|(
 operator|!
 name|output_encoding
 condition|)
+name|output_encoding
+operator|=
+literal|"utf-8"
+expr_stmt|;
+elseif|else
+if|if
+condition|(
+operator|!
+operator|*
+name|output_encoding
+condition|)
 return|return
 name|NULL
 return|;
