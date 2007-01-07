d014f6af03fcfd315dfa7335cbfc48839833a448
Junio C Hamano <junkio@cox.net>
2007-01-07 01:35:34 -0800

    Move initialization of log_all_ref_updates
    The patches to prevent Porcelainish that require working tree
    from doing any damage in a bare repository make a lot of sense,
    and I want to make the is_bare_git_dir() function more reliable.
    
    In order to allow the repository owner override the heuristic
    implemented in is_bare_git_dir() if/when it misidentifies a
    particular repository, it would make sense to introduce a new
    configuration variable "[core] bare = true/false", and make
    is_bare_git_dir() notice it.
    
    The scripts would do a 'repo-config --bool --get core.bare' and
    iff the command fails (i.e. there is no such variable in the
    configuration file), it would use the heuristic implemented at
    the script level [*1*].
    
    However, setup_git_env() which is called a lot earlier than we
    even read from the repository configuration currently makes a
    call to is_bare_git_dir(), in order to change the default
    setting for log_all_ref_updates.  It somehow feels that this is
    a hack.
    
    By the way, [*1*] is another thing I hate about the current
    config mechanism.  "git-repo-config --get" does not know what
    the possible configuration variables are, let alone what the
    default values for them are.  It allows us not to maintain a
    centralized configuration table, which makes it easy to
    introduce ad-hoc variables and gives a warm fuzzy feeling of
    being modular, but my feeling is that it is turning out to be a
    rather high price to pay for scripts.
    
    Signed-off-by: Junio C Hamano <junkio@cox.net>
    
    cregit: 510c5a8ee392fd2ab954b676bae486b53f444013 http://github.com/git/git/commit/510c5a8ee392fd2ab954b676bae486b53f444013

--- environment.c
+++ environment.c
@@ -57,8 +57,15 @@ begin_decl_stmt
 DECL|variable|log_all_ref_updates
 name|int
 name|log_all_ref_updates
+init|=
+operator|-
+literal|1
 decl_stmt|;
 end_decl_stmt
+begin_comment
+DECL|variable|log_all_ref_updates
+comment|/* unspecified */
+end_comment
 begin_decl_stmt
 DECL|variable|warn_ambiguous_refs
 name|int
@@ -306,14 +313,6 @@ name|git_path
 argument_list|(
 literal|"info/grafts"
 argument_list|)
-argument_list|)
-expr_stmt|;
-name|log_all_ref_updates
-operator|=
-operator|!
-name|is_bare_git_dir
-argument_list|(
-name|git_dir
 argument_list|)
 expr_stmt|;
 block|}
