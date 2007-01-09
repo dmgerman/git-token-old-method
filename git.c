64a28a1088bfb39b7d3599cabe23b5d54c93848f
Junio C Hamano <junkio@cox.net>
2007-01-09 00:50:02 -0800

    Make merge-base a built-in.
    Signed-off-by: Junio C Hamano <junkio@cox.net>
    
    cregit: 71dfbf224ff980f4085f75868dc409118418731e http://github.com/git/git/commit/71dfbf224ff980f4085f75868dc409118418731e

--- git.c
+++ git.c
@@ -1387,6 +1387,14 @@ name|cmd_mailsplit
 block|}
 block|,
 block|{
+literal|"merge-base"
+block|,
+name|cmd_merge_base
+block|,
+name|RUN_SETUP
+block|}
+block|,
+block|{
 literal|"merge-file"
 block|,
 name|cmd_merge_file
