973a6db4f45c3647eef8e9f100ffc70766c99bce
Uwe Kleine-König <zeisberg@informatik.uni-freiburg.de>
2007-01-19 13:49:27 +0100

    rename --exec to --receive-pack for push and send-pack
    For now it's just to get a more descriptive name.  Later we might update the
    push protocol to run more than one program on the other end.  Moreover this
    matches better the corresponding config option remote.<name>. receivepack.
    
    --exec continues to work
    
    Signed-off-by: Uwe Kleine-König <zeisberg@informatik.uni-freiburg.de>
    Signed-off-by: Junio C Hamano <junkio@cox.net>
    
    cregit: d23842fd53e61f32c189a6ec902c4133abf29878 http://github.com/git/git/commit/d23842fd53e61f32c189a6ec902c4133abf29878

--- send-pack.c
+++ send-pack.c
@@ -37,17 +37,17 @@ name|char
 name|send_pack_usage
 index|[]
 init|=
-literal|"git-send-pack [--all] [--force] [--exec=<git-receive-pack>] [--verbose] [--thin] [<host>:]<directory> [<ref>...]\n"
+literal|"git-send-pack [--all] [--force] [--receive-pack=<git-receive-pack>] [--verbose] [--thin] [<host>:]<directory> [<ref>...]\n"
 literal|"  --all and explicit<ref> specification are mutually exclusive."
 decl_stmt|;
 end_decl_stmt
 begin_decl_stmt
-DECL|variable|exec
+DECL|variable|receivepack
 specifier|static
 specifier|const
 name|char
 modifier|*
-name|exec
+name|receivepack
 init|=
 literal|"git-receive-pack"
 decl_stmt|;
@@ -1796,13 +1796,34 @@ name|strncmp
 argument_list|(
 name|arg
 argument_list|,
+literal|"--receive-pack="
+argument_list|,
+literal|15
+argument_list|)
+condition|)
+block|{
+name|receivepack
+operator|=
+name|arg
+operator|+
+literal|15
+expr_stmt|;
+continue|continue;
+block|}
+if|if
+condition|(
+operator|!
+name|strncmp
+argument_list|(
+name|arg
+argument_list|,
 literal|"--exec="
 argument_list|,
 literal|7
 argument_list|)
 condition|)
 block|{
-name|exec
+name|receivepack
 operator|=
 name|arg
 operator|+
@@ -1944,7 +1965,7 @@ name|fd
 argument_list|,
 name|dest
 argument_list|,
-name|exec
+name|receivepack
 argument_list|)
 expr_stmt|;
 if|if
