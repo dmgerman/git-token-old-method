21f456a5120c45b1c1c223bd57595d7b8e2172a8
Junio C Hamano <junkio@cox.net>
2007-02-04 17:50:14 -0800

    Rename get_ident() to fmt_ident() and make it available to outside
    This makes the functionality of ident.c::get_ident() available to
    other callers.
    
    Signed-off-by: Junio C Hamano <junkio@cox.net>
    
    cregit: 798123af21e1660cb606ab730ce721f354957719 http://github.com/git/git/commit/798123af21e1660cb606ab730ce721f354957719

--- ident.c
+++ ident.c
@@ -859,12 +859,11 @@ literal|"\n"
 decl_stmt|;
 end_decl_stmt
 begin_function
-DECL|function|get_ident
-specifier|static
+DECL|function|fmt_ident
 specifier|const
 name|char
 modifier|*
-name|get_ident
+name|fmt_ident
 parameter_list|(
 specifier|const
 name|char
@@ -1157,7 +1156,7 @@ name|error_on_no_name
 parameter_list|)
 block|{
 return|return
-name|get_ident
+name|fmt_ident
 argument_list|(
 name|getenv
 argument_list|(
@@ -1191,7 +1190,7 @@ name|error_on_no_name
 parameter_list|)
 block|{
 return|return
-name|get_ident
+name|fmt_ident
 argument_list|(
 name|getenv
 argument_list|(
