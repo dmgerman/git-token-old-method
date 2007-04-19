80930b0774fe6710ea708dbcd0a2d9af2c562a76
Andy Whitcroft <apw@shadowen.org>
2007-04-19 03:08:15 +0100

    fix up strtoul_ui error handling
    Two scanf() calls were converted to strtoul_ui() but the return
    values were not updated to match.  scanf() returns the number of
    matched "values" which for this usage is 1 on success.  strtoul_ui()
    return 0 on success.  Update these call sites to match.
    
    Signed-off-by: Andy Whitcroft <apw@shadowen.org>
    Signed-off-by: Junio C Hamano <junkio@cox.net>
    
    cregit: 6e6db39afc305e1205a4d2dcf34680bfa80bfc24 http://github.com/git/git/commit/6e6db39afc305e1205a4d2dcf34680bfa80bfc24

--- builtin-update-index.c
+++ builtin-update-index.c
@@ -2638,7 +2638,6 @@ argument_list|)
 expr_stmt|;
 if|if
 condition|(
-operator|(
 name|strtoul_ui
 argument_list|(
 name|argv
@@ -2653,9 +2652,6 @@ argument_list|,
 operator|&
 name|mode
 argument_list|)
-operator|!=
-literal|1
-operator|)
 operator|||
 name|get_sha1_hex
 argument_list|(
