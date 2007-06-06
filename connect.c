d5dfff7c76c2fa5c86713283bcddc80559d67398
Matthias Lederhofer <matled@gmx.net>
2007-06-06 09:10:42 +0200

    introduce GIT_WORK_TREE to specify the work tree
    setup_gdg is used as abbreviation for setup_git_directory_gently.
    
    The work tree can be specified using the environment variable
    GIT_WORK_TREE and the config option core.worktree (the environment
    variable has precendence over the config option).  Additionally
    there is a command line option --work-tree which sets the
    environment variable.
    
    setup_gdg does the following now:
    
    GIT_DIR unspecified
    repository in .git directory
        parent directory of the .git directory is used as work tree,
        GIT_WORK_TREE is ignored
    
    GIT_DIR unspecified
    repository in cwd
        GIT_DIR is set to cwd
        see the cases with GIT_DIR specified what happens next and
        also see the note below
    
    GIT_DIR specified
    GIT_WORK_TREE/core.worktree unspecified
        cwd is used as work tree
    
    GIT_DIR specified
    GIT_WORK_TREE/core.worktree specified
        the specified work tree is used
    
    Note on the case where GIT_DIR is unspecified and repository is in cwd:
        GIT_WORK_TREE is used but is_inside_git_dir is always true.
        I did it this way because setup_gdg might be called multiple
        times (e.g. when doing alias expansion) and in successive calls
        setup_gdg should do the same thing every time.
    
    Meaning of is_bare/is_inside_work_tree/is_inside_git_dir:
    
    (1) is_bare_repository
        A repository is bare if core.bare is true or core.bare is
        unspecified and the name suggests it is bare (directory not
        named .git).  The bare option disables a few protective
        checks which are useful with a working tree.  Currently
        this changes if a repository is bare:
            updates of HEAD are allowed
            git gc packs the refs
            the reflog is disabled by default
    
    (2) is_inside_work_tree
        True if the cwd is inside the associated working tree (if there
        is one), false otherwise.
    
    (3) is_inside_git_dir
        True if the cwd is inside the git directory, false otherwise.
        Before this patch is_inside_git_dir was always true for bare
        repositories.
    
    When setup_gdg finds a repository git_config(git_default_config) is
    always called.  This ensure that is_bare_repository makes use of
    core.bare and does not guess even though core.bare is specified.
    
    inside_work_tree and inside_git_dir are set if setup_gdg finds a
    repository.  The is_inside_work_tree and is_inside_git_dir functions
    will die if they are called before a successful call to setup_gdg.
    
    Signed-off-by: Matthias Lederhofer <matled@gmx.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    cregit: 892c41b98ae2e6baf3aa13901cb10db9ac67d2f3 http://github.com/git/git/commit/892c41b98ae2e6baf3aa13901cb10db9ac67d2f3

--- connect.c
+++ connect.c
@@ -3051,6 +3051,11 @@ argument_list|)
 expr_stmt|;
 name|unsetenv
 argument_list|(
+name|GIT_WORK_TREE_ENVIRONMENT
+argument_list|)
+expr_stmt|;
+name|unsetenv
+argument_list|(
 name|GRAFT_ENVIRONMENT
 argument_list|)
 expr_stmt|;
