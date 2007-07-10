c36c19544ddbf800f502ee5f15c4fddadd7454de
Daniel Barkalow <barkalow@iabervon.org>
2007-07-10 00:47:23 -0400

    Add allocation and freeing functions for struct refs
    Instead of open-coding allocation wherever it happens, have a function.
    Also, add a function to free a list of refs, which we currently never
    actually do.
    
    Signed-off-by: Daniel Barkalow <barkalow@iabervon.org>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    cregit: dfd255dd1a7c04ad24891db50e5d80d96f93fd4a http://github.com/git/git/commit/dfd255dd1a7c04ad24891db50e5d80d96f93fd4a

--- connect.c
+++ connect.c
@@ -382,16 +382,8 @@ condition|)
 continue|continue;
 name|ref
 operator|=
-name|xcalloc
+name|alloc_ref
 argument_list|(
-literal|1
-argument_list|,
-sizeof|sizeof
-argument_list|(
-operator|*
-name|ref
-argument_list|)
-operator|+
 name|len
 operator|-
 literal|40
