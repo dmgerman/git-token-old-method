10abb46cce03b9a711c5f0cdb7dea529bd1a26aa
Carlos Rica <jasampler@gmail.com>
2007-09-11 05:19:34 +0200

    Make "git reset" a builtin.
    This replaces the script "git-reset.sh" with "builtin-reset.c".
    
    A few git commands used in the script are called from the builtin also:
    "ls-files" to check for unmerged files, "read-tree" for resetting
    the index file in "mixed" and "hard" resets, and "update-index" to
    refresh at the end in the "mixed" reset and also for the option that
    gets selected paths into the index.
    
    The reset option with paths was implemented by Johannes Schindelin.
    
    Since the option that gets selected paths into the index is not
    a "reset" like the others because it does not change the HEAD at all,
    now the command is showing a warning when the "--mixed" option
    is supplied for that purpose.
    
    The following table shows the behaviour of "git reset" for
    the different supported options, where X means "changing"
    the HEAD, index or working tree:
    
    reset:  --soft  --mixed  --hard  -- <paths>
    HEAD       X       X        X        -
    index      -       X        X        X
    files      -       -        X        -
    
    Signed-off-by: Carlos Rica <jasampler@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    cregit: 0e5a7faa3a903cf7a0a66c81e20a76b91f17faab http://github.com/git/git/commit/0e5a7faa3a903cf7a0a66c81e20a76b91f17faab

--- git.c
+++ git.c
@@ -2172,6 +2172,14 @@ name|RUN_SETUP
 block|}
 block|,
 block|{
+literal|"reset"
+block|,
+name|cmd_reset
+block|,
+name|RUN_SETUP
+block|}
+block|,
+block|{
 literal|"rev-list"
 block|,
 name|cmd_rev_list
