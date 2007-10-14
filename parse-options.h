91cdd1786c28e6586bd582d13fb609e3bd2e474f
Pierre Habouzit <madcoder@debian.org>
2007-10-15 01:45:45 +0200

    parse-options: make some arguments optional, add callbacks.
    * add the possibility to use callbacks to parse some options, this can
      help implementing new options kinds with great flexibility. struct option
      gains a callback pointer and a \`defval' where callbacks user can put
      either integers or pointers. callbacks also can use the \`value' pointer
      for anything, preferably to the pointer to the final storage for the value
      though.
    
    * add a \`flag' member to struct option to make explicit that this option may
      have an optional argument. The semantics depends on the option type. For
      INTEGERS, it means that if the switch is not used in its
      --long-form=<value> form, and that there is no token after it or that the
      token does not starts with a digit, then it's assumed that the switch has
      no argument. For STRING or CALLBACK it works the same, except that the
      condition is that the next atom starts with a dash. This is needed to
      implement backward compatible behaviour with existing ways to parse the
      command line. Its use for new options is discouraged.
    
    Signed-off-by: Pierre Habouzit <madcoder@debian.org>
    Signed-off-by: Shawn O. Pearce <spearce@spearce.org>
    
    cregit: ffe659f94d793375fca97dd296422fc10c155016 http://github.com/git/git/commit/ffe659f94d793375fca97dd296422fc10c155016

--- parse-options.h
+++ parse-options.h
@@ -29,6 +29,9 @@ name|OPTION_STRING
 block|,
 DECL|enumerator|OPTION_INTEGER
 name|OPTION_INTEGER
+block|,
+DECL|enumerator|OPTION_CALLBACK
+name|OPTION_CALLBACK
 block|, }
 enum|;
 end_enum
@@ -44,6 +47,44 @@ literal|1
 block|, }
 enum|;
 end_enum
+begin_enum
+DECL|enum|parse_opt_option_flags
+enum|enum
+name|parse_opt_option_flags
+block|{
+DECL|enumerator|PARSE_OPT_OPTARG
+name|PARSE_OPT_OPTARG
+init|=
+literal|1
+block|, }
+enum|;
+end_enum
+begin_struct_decl
+struct_decl|struct
+name|option
+struct_decl|;
+end_struct_decl
+begin_typedef
+DECL|typedef|parse_opt_cb
+typedef|typedef
+name|int
+name|parse_opt_cb
+parameter_list|(
+specifier|const
+name|struct
+name|option
+modifier|*
+parameter_list|,
+specifier|const
+name|char
+modifier|*
+name|arg
+parameter_list|,
+name|int
+name|unset
+parameter_list|)
+function_decl|;
+end_typedef
 begin_struct
 DECL|struct|option
 struct|struct
@@ -80,6 +121,20 @@ specifier|const
 name|char
 modifier|*
 name|help
+decl_stmt|;
+DECL|member|flags
+name|int
+name|flags
+decl_stmt|;
+DECL|member|callback
+name|parse_opt_cb
+modifier|*
+name|callback
+decl_stmt|;
+comment|/* holds default value for PARSE_OPT_OPTARG, 	   though callbacks can use it like they want */
+DECL|member|defval
+name|intptr_t
+name|defval
 decl_stmt|;
 block|}
 struct|;
@@ -151,6 +206,27 @@ parameter_list|,
 name|h
 parameter_list|)
 value|{ OPTION_STRING,  (s), (l), (v), (a), (h) }
+end_define
+begin_define
+DECL|macro|OPT_CALLBACK
+define|#
+directive|define
+name|OPT_CALLBACK
+parameter_list|(
+name|s
+parameter_list|,
+name|l
+parameter_list|,
+name|v
+parameter_list|,
+name|a
+parameter_list|,
+name|h
+parameter_list|,
+name|f
+parameter_list|)
+define|\
+value|{ OPTION_CALLBACK, (s), (l), (v), (a), (h), 0, (f) }
 end_define
 begin_comment
 comment|/* parse_options() will filter out the processed options and leave the  * non-option argments in argv[].  * Returns the number of arguments left in argv[].  */
