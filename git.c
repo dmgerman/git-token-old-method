e030a8acb737fa0c0f1d7c7495a50ba93ec074fa
Shawn Bohrer <shawn.bohrer@gmail.com>
2007-11-11 19:48:47 -0600

    Make git-clean a builtin
    This replaces git-clean.sh with builtin-clean.c, and moves
    git-clean.sh to the examples.
    
    This also introduces a change in behavior when removing directories
    explicitly specified as a path.  For example currently:
    
    1. When dir has only untracked files, these two behave differently:
    
        $ git clean -n dir
        $ git clean -n dir/
    
    the former says "Would not remove dir/", while the latter would say
    "Would remove dir/untracked" for all paths under it, but not the
    directory itself.
    
    With -d, the former would stop refusing, however since the user
    explicitly asked to remove the directory the -d is no longer required.
    
    2. When there are more parameters:
    
        $ git clean -n dir foo
        $ git clean -n dir/ foo
    
    both cases refuse to remove dir/ unless -d is specified.  Once again
    since both cases requested to remove dir the -d is no longer required.
    
    Thanks to Johannes Schindelin for the conversion to using the
    parse-options API.
    
    Signed-off-by: Shawn Bohrer <shawn.bohrer@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    cregit: 113f10f22f4b3b599e44e192e241e0bace9cc39e http://github.com/git/git/commit/113f10f22f4b3b599e44e192e241e0bace9cc39e

--- git.c
+++ git.c
@@ -1672,6 +1672,16 @@ name|NEED_WORK_TREE
 block|}
 block|,
 block|{
+literal|"clean"
+block|,
+name|cmd_clean
+block|,
+name|RUN_SETUP
+operator||
+name|NEED_WORK_TREE
+block|}
+block|,
+block|{
 literal|"commit-tree"
 block|,
 name|cmd_commit_tree
