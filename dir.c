7ed884d08f75e1d8b2a4a94a6acd92ecbba7b972
Junio C Hamano <gitster@pobox.com>
2007-11-16 01:15:41 -0800

    Fix per-directory exclude handing for "git add"
    In "dir_struct", each exclusion element in the exclusion stack records a
    base string (pointer to the beginning with length) so that we can tell
    where it came from, but this pointer is just pointing at the parameter
    that is given by the caller to the push_exclude_per_directory()
    function.
    
    While read_directory_recursive() runs, calls to excluded() makes use
    the data in the exclusion elements, including this base string.  The
    caller of read_directory_recursive() is not supposed to free the
    buffer it gave to push_exclude_per_directory() earlier, until it
    returns.
    
    The test case Bruce Stephens gave in the mailing list discussion
    was simplified and added to the t3700 test.
    
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    cregit: 41a7aa588f449d3b139480b102f510abc5ef7f85 http://github.com/git/git/commit/41a7aa588f449d3b139480b102f510abc5ef7f85

--- dir.c
+++ dir.c
@@ -3077,6 +3077,12 @@ argument_list|(
 name|pathspec
 argument_list|)
 decl_stmt|;
+name|char
+modifier|*
+name|pp
+init|=
+name|NULL
+decl_stmt|;
 comment|/* 	 * Make sure to do the per-directory exclude for all the 	 * directories leading up to our base. 	 */
 if|if
 condition|(
@@ -3093,17 +3099,16 @@ block|{
 name|char
 modifier|*
 name|p
-decl_stmt|,
-modifier|*
+decl_stmt|;
 name|pp
-init|=
+operator|=
 name|xmalloc
 argument_list|(
 name|baselen
 operator|+
 literal|1
 argument_list|)
-decl_stmt|;
+expr_stmt|;
 name|memcpy
 argument_list|(
 name|pp
@@ -3182,11 +3187,6 @@ operator|+
 name|baselen
 expr_stmt|;
 block|}
-name|free
-argument_list|(
-name|pp
-argument_list|)
-expr_stmt|;
 block|}
 block|}
 name|read_directory_recursive
@@ -3207,6 +3207,11 @@ expr_stmt|;
 name|free_simplify
 argument_list|(
 name|simplify
+argument_list|)
+expr_stmt|;
+name|free
+argument_list|(
+name|pp
 argument_list|)
 expr_stmt|;
 name|qsort
