2bc20dc4d05203d2169cbef506d6c7e757fefacb
Pierre Habouzit <madcoder@debian.org>
2007-12-11 15:43:51 +0100

    Invert numbers and names in the git-shortlog summary mode.
    Also make it \`cut\` friendly using a tab to separate the numbers and names.
    
    Signed-off-by: Pierre Habouzit <madcoder@debian.org>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    cregit: 97566ea72ea460576ccaea7b51dd32ab33e1c46c http://github.com/git/git/commit/97566ea72ea460576ccaea7b51dd32ab33e1c46c

--- builtin-shortlog.c
+++ builtin-shortlog.c
@@ -1724,7 +1724,11 @@ condition|)
 block|{
 name|printf
 argument_list|(
-literal|"%s: %d\n"
+literal|"%6d\t%s\n"
+argument_list|,
+name|onelines
+operator|->
+name|nr
 argument_list|,
 name|list
 operator|.
@@ -1734,10 +1738,6 @@ name|i
 index|]
 operator|.
 name|path
-argument_list|,
-name|onelines
-operator|->
-name|nr
 argument_list|)
 expr_stmt|;
 block|}
