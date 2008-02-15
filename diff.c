68591bcec8cd81264369a8d716119dcd55ae8910
Junio C Hamano <gitster@pobox.com>
2008-02-15 09:37:54 -0800

    diff.c: fixup garding of config parser from value=NULL
    Christian Couder noticed that there still were a handcrafted error()
    call that we should have converted to config_error_nonbool() where
    parse_lldiff_command() parses the configuration file.
    
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    cregit: 588071112c7ca14a04d674e4f019572f0bb77326 http://github.com/git/git/commit/588071112c7ca14a04d674e4f019572f0bb77326

--- diff.c
+++ diff.c
@@ -468,10 +468,8 @@ operator|!
 name|value
 condition|)
 return|return
-name|error
+name|config_error_nonbool
 argument_list|(
-literal|"%s: lacks value"
-argument_list|,
 name|var
 argument_list|)
 return|;
