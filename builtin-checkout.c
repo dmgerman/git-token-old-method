c2aad15165084ec5c99e46a5ecc4c48c254bc1ab
Junio C Hamano <gitster@pobox.com>
2008-02-23 15:45:19 -0800

    checkout: error out when index is unmerged even with -m
    Even when -m is given to allow fallilng back to 3-way merge
    while switching branches, we should refuse if the original index
    is unmerged.
    
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    Acked-by: Daniel Barkalow <barkalow@iabervon.org>
    
    cregit: 04c9e11f2cffaf84dd20602f811bf377f6033cb6 http://github.com/git/git/commit/04c9e11f2cffaf84dd20602f811bf377f6033cb6

--- builtin-checkout.c
+++ builtin-checkout.c
@@ -1337,23 +1337,16 @@ name|unmerged_cache
 argument_list|()
 condition|)
 block|{
-name|ret
-operator|=
-name|opts
-operator|->
-name|merge
-condition|?
-operator|-
-literal|1
-else|:
 name|error
 argument_list|(
 literal|"you need to resolve your current index first"
 argument_list|)
 expr_stmt|;
+return|return
+literal|1
+return|;
 block|}
-else|else
-block|{
+comment|/* 2-way merge to the new branch */
 name|topts
 operator|.
 name|update
@@ -1482,8 +1475,8 @@ operator|->
 name|size
 argument_list|)
 expr_stmt|;
-name|ret
-operator|=
+if|if
+condition|(
 name|unpack_trees
 argument_list|(
 literal|2
@@ -1493,11 +1486,6 @@ argument_list|,
 operator|&
 name|topts
 argument_list|)
-expr_stmt|;
-block|}
-if|if
-condition|(
-name|ret
 condition|)
 block|{
 comment|/* 			 * Unpack couldn't do a trivial merge; either 			 * give up or do a real merge, depending on 			 * whether the merge flag was used. 			 */
