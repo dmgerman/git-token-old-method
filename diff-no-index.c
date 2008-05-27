c9301ee06b4b87bcaded79160f718b3d438b5fb8
Junio C Hamano <gitster@pobox.com>
2008-05-26 22:35:07 -0700

    git diff --no-index: default to page like other diff frontends
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    cregit: b3fde6ccb1cbdd362dec19e317905f71528b3a53 http://github.com/git/git/commit/b3fde6ccb1cbdd362dec19e317905f71528b3a53

--- diff-no-index.c
+++ diff-no-index.c
@@ -1084,6 +1084,23 @@ else|:
 literal|"[--no-index]"
 argument_list|)
 expr_stmt|;
+comment|/* 	 * If the user asked for our exit code then don't start a 	 * pager or we would end up reporting its exit code instead. 	 */
+if|if
+condition|(
+operator|!
+name|DIFF_OPT_TST
+argument_list|(
+operator|&
+name|revs
+operator|->
+name|diffopt
+argument_list|,
+name|EXIT_WITH_STATUS
+argument_list|)
+condition|)
+name|setup_pager
+argument_list|()
+expr_stmt|;
 name|diff_setup
 argument_list|(
 operator|&
