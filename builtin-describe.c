f20cef612378741fb408f0ba8773298030a521b4
Shawn O. Pearce <spearce@spearce.org>
2008-07-03 02:32:45 +0000

    Fix describe --tags --long so it does not segfault
    If we match a lightweight (non-annotated tag) as the name to
    output and --long was requested we do not have a tag, nor do
    we have a tagged object to display.  Instead we must use the
    object we were passed as input for the long format display.
    
    Reported-by: Mark Burton <markb@ordern.com>
    Backtraced-by: Mikael Magnusson <mikachu@gmail.com>
    Signed-off-by: Shawn O. Pearce <spearce@spearce.org>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    cregit: 14d4642e2a472ba4c06e10a3f5623d944b869314 http://github.com/git/git/commit/14d4642e2a472ba4c06e10a3f5623d944b869314

--- builtin-describe.c
+++ builtin-describe.c
@@ -1201,9 +1201,15 @@ argument_list|,
 name|n
 operator|->
 name|tag
+condition|?
+name|n
+operator|->
+name|tag
 operator|->
 name|tagged
 operator|->
+name|sha1
+else|:
 name|sha1
 argument_list|)
 expr_stmt|;
