3696cb254ff6d6991870db1d68ce963bbfcccdc6
Johannes Schindelin <Johannes.Schindelin@gmx.de>
2008-07-07 14:41:34 +0100

    run_command(): respect GIT_TRACE
    When GIT_TRACE is set, the user is most likely wanting to see an external
    command that is about to be executed.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    cregit: 8852f5d704edc20896a84ca9107978c478a2941b http://github.com/git/git/commit/8852f5d704edc20896a84ca9107978c478a2941b

--- run-command.c
+++ run-command.c
@@ -334,6 +334,15 @@ literal|0
 index|]
 expr_stmt|;
 block|}
+name|trace_argv_printf
+argument_list|(
+name|cmd
+operator|->
+name|argv
+argument_list|,
+literal|"trace: run_command:"
+argument_list|)
+expr_stmt|;
 name|cmd
 operator|->
 name|pid
