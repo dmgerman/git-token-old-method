21d497214890c80910a68dea72982a3bed74851e
Junio C Hamano <gitster@pobox.com>
2008-07-30 01:12:19 -0700

    Fix merge name generation in "merge in C"
    When merging an early part of a branch, e.g. "git merge xyzzy~20", we were
    supposed to say "branch 'xyzzy' (early part)", but it incorrectly said
    "branch 'refs/heads/xy' (early part)" instead.
    
    The logic was supposed to first strip away "~20" part to make sure that
    what follows "~" is a non-zero posint, prefix it with "refs/heads/" and
    ask resolve_ref() if it is a ref.  If it is, then we know xyzzy was a
    branch, and we can give the correct message.
    
    However, there were a few bugs.  First of all, the logic to build this
    "true branch refname" did not count the characters correctly.  At this
    point of the code, "len" is the number of trailing, non-name part of the
    given extended SHA-1 expression given by the user, i.e. number of bytes in
    "~20" in the above example.
    
    In addition, the message forgot to skip "refs/heads/" it prefixed from the
    output.
    
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    cregit: 9b6bf4d575e216db9240907940e9f6c619877735 http://github.com/git/git/commit/9b6bf4d575e216db9240907940e9f6c619877735

--- builtin-merge.c
+++ builtin-merge.c
@@ -2158,9 +2158,11 @@ argument_list|(
 operator|&
 name|truname
 argument_list|,
+name|truname
+operator|.
 name|len
-operator|+
-literal|11
+operator|-
+name|len
 argument_list|)
 expr_stmt|;
 if|if
@@ -2195,6 +2197,8 @@ argument_list|,
 name|truname
 operator|.
 name|buf
+operator|+
+literal|11
 argument_list|,
 operator|(
 name|early
