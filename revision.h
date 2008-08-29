8e63829218bd7b2e02948d3bc5873346ddfed7e5
Thomas Rast <trast@student.ethz.ch>
2008-08-29 21:18:38 +0200

    rev-list: fix --reverse interaction with --parents
    --reverse did not interact well with --parents, as the included test
    case shows: in a history like
    
      A--B.
       \   \
        \`C--M--D
    
    the command
    
      git rev-list --reverse --parents --full-history HEAD
    
    erroneously lists D as having no parents at all.  (Without --reverse,
    it correctly lists M.)
    
    This is caused by the machinery driving --reverse: it first grabs all
    commits through the normal routines, then runs them through the same
    routines again, effectively simplifying them twice.
    
    Fix this by moving the --reverse one level up, into get_revision().
    This way we can cleanly grab all commits via the normal calls, then
    just pop them off the list one by one without interfering with
    get_revision_internal().
    
    Signed-off-by: Thomas Rast <trast@student.ethz.ch>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    cregit: 498bcd3159ae3711f2beea2ea497cdc09856ee79 http://github.com/git/git/commit/498bcd3159ae3711f2beea2ea497cdc09856ee79

--- revision.h
+++ revision.h
@@ -258,6 +258,11 @@ name|reverse
 range|:
 literal|1
 decl_stmt|,
+DECL|member|reverse_output_stage
+name|reverse_output_stage
+range|:
+literal|1
+decl_stmt|,
 DECL|member|cherry_pick
 name|cherry_pick
 range|:
