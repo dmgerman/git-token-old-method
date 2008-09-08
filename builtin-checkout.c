0e1e4b6b067225f690e0357734f87e0bb983fab5
Junio C Hamano <gitster@pobox.com>
2008-09-07 19:49:25 -0700

    checkout: do not lose staged removal
    The logic to checkout a different commit implements the safety to never
    lose user's local changes.  For example, switching from a commit to
    another commit, when you have changed a path that is different between
    them, need to merge your changes to the version from the switched-to
    commit, which you may not necessarily be able to resolve easily.  By
    default, "git checkout" refused to switch branches, to give you a chance
    to stash your local changes (or use "-m" to merge, accepting the risks of
    getting conflicts).
    
    This safety, however, had one deliberate hole since early June 2005.  When
    your local change was to remove a path (and optionally to stage that
    removal), the command checked out the path from the switched-to commit
    nevertheless.
    
    This was to allow an initial checkout to happen smoothly (e.g. an initial
    checkout is done by starting with an empty index and switching from the
    commit at the HEAD to the same commit).  We can tighten the rule slightly
    to allow this special case to pass, without losing sight of removal
    explicitly done by the user, by noticing if the index is truly empty when
    the operation begins.
    
    For historical background, see:
    
        http://thread.gmane.org/gmane.comp.version-control.git/4641/focus=4646
    
    This case is marked as *0* in the message, which both Linus and I said "it
    feels somewhat wrong but otherwise we cannot start from an empty index".
    
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    cregit: 5521883490e85f4d973141972cf16f89a79f1979 http://github.com/git/git/commit/5521883490e85f4d973141972cf16f89a79f1979

--- builtin-checkout.c
+++ builtin-checkout.c
@@ -1342,6 +1342,25 @@ block|}
 comment|/* 2-way merge to the new branch */
 name|topts
 operator|.
+name|initial_checkout
+operator|=
+operator|(
+operator|!
+name|active_nr
+operator|&&
+operator|(
+name|old
+operator|->
+name|commit
+operator|==
+name|new
+operator|->
+name|commit
+operator|)
+operator|)
+expr_stmt|;
+name|topts
+operator|.
 name|update
 operator|=
 literal|1
