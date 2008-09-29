df74fe116f0103be127f74078485ed586e889eea
Rafael Garcia-Suarez <rgarciasuarez@gmail.com>
2008-09-29 18:04:41 +0200

    Clarify commit error message for unmerged files
    Currently, trying to use git-commit with unmerged files in the index
    will show the message "Error building trees", which can be a bit
    obscure to the end user. This patch makes the error message clearer, and
    consistent with what git-write-tree reports in a similar situation.
    
    Signed-off-by: Rafael Garcia-Suarez <rgarciasuarez@gmail.com>
    Signed-off-by: Shawn O. Pearce <spearce@spearce.org>
    
    cregit: cbce6c0be30cbaf8c39051f85006d679338a8ddc http://github.com/git/git/commit/cbce6c0be30cbaf8c39051f85006d679338a8ddc

--- builtin-commit.c
+++ builtin-commit.c
@@ -3319,7 +3319,7 @@ condition|)
 block|{
 name|error
 argument_list|(
-literal|"Error building trees"
+literal|"Error building trees; the index is unmerged?"
 argument_list|)
 expr_stmt|;
 return|return
