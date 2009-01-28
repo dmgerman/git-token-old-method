2372bb82ce77f441ad25b2322e6addb9fdeae7db
Jeff King <peff@peff.net>
2009-01-28 02:38:14 -0500

    git: use run_command() to execute dashed externals
    We used to simply try calling execvp(); if it succeeded, then we were done
    and the new program was running. If it didn't, then we knew that it wasn't
    a valid command.
    
    Unfortunately, this interacted badly with the new pager handling. Now that
    git remains the parent process and the pager is spawned, git has to hang
    around until the pager is finished. We install an atexit handler to do
    this, but that handler never gets called if we successfully run execvp.
    
    You could see this behavior by running any dashed external using a pager
    (e.g., "git -p stash list"). The command finishes running, but the pager
    is still going. In the case of less, it then gets an error reading from
    the terminal and exits, potentially leaving the terminal in a broken state
    (and not showing the output).
    
    This patch just uses run_command() to try running the dashed external. The
    parent git process then waits for the external process to complete and
    then handles the pager cleanup as it would for an internal command.
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    cregit: d8e96fd86d415554a9c2e09ffb929a9e22fdad25 http://github.com/git/git/commit/d8e96fd86d415554a9c2e09ffb929a9e22fdad25

--- git.c
+++ git.c
@@ -19,6 +19,11 @@ include|#
 directive|include
 file|"quote.h"
 end_include
+begin_include
+include|#
+directive|include
+file|"run-command.h"
+end_include
 begin_decl_stmt
 DECL|variable|git_usage_string
 specifier|const
@@ -2381,6 +2386,9 @@ name|char
 modifier|*
 name|tmp
 decl_stmt|;
+name|int
+name|status
+decl_stmt|;
 name|strbuf_addf
 argument_list|(
 operator|&
@@ -2418,31 +2426,53 @@ argument_list|,
 literal|"trace: exec:"
 argument_list|)
 expr_stmt|;
-comment|/* execvp() can only ever return if it fails */
-name|execvp
+comment|/* 	 * if we fail because the command is not found, it is 	 * OK to return. Otherwise, we just pass along the status code. 	 */
+name|status
+operator|=
+name|run_command_v_opt
 argument_list|(
-name|cmd
-operator|.
-name|buf
-argument_list|,
-operator|(
-name|char
-operator|*
-operator|*
-operator|)
 name|argv
-argument_list|)
-expr_stmt|;
-name|trace_printf
-argument_list|(
-literal|"trace: exec failed: %s\n"
 argument_list|,
-name|strerror
-argument_list|(
-name|errno
-argument_list|)
+literal|0
 argument_list|)
 expr_stmt|;
+if|if
+condition|(
+name|status
+operator|!=
+operator|-
+name|ERR_RUN_COMMAND_EXEC
+condition|)
+block|{
+if|if
+condition|(
+name|IS_RUN_COMMAND_ERR
+argument_list|(
+name|status
+argument_list|)
+condition|)
+name|die
+argument_list|(
+literal|"unable to run '%s'"
+argument_list|,
+name|argv
+index|[
+literal|0
+index|]
+argument_list|)
+expr_stmt|;
+name|exit
+argument_list|(
+operator|-
+name|status
+argument_list|)
+expr_stmt|;
+block|}
+name|errno
+operator|=
+name|ENOENT
+expr_stmt|;
+comment|/* as if we called execvp */
 name|argv
 index|[
 literal|0
