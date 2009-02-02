3aeabe64c1814fb82e90fc2fe587a62304e6b474
Christian Couder <chriscool@tuxfamily.org>
2009-02-02 06:12:44 +0100

    Add new "git replace" command
    This command can only be used now to list replace refs in
    "refs/replace/" and to delete them.
    
    The option to list replace refs is "-l".
    The option to delete replace refs is "-d".
    
    The behavior should be consistent with how "git tag" and "git branch"
    are working.
    
    The code has been copied from "builtin-tag.c" by Kristian Høgsberg
    <krh@redhat.com> and Carlos Rica <jasampler@gmail.com> that was itself
    based on git-tag.sh and mktag.c by Linus Torvalds.
    
    Signed-off-by: Christian Couder <chriscool@tuxfamily.org>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    cregit: 54b0c1e041e50cc08b1520b7d557770916d0b7ab http://github.com/git/git/commit/54b0c1e041e50cc08b1520b7d557770916d0b7ab

--- git.c
+++ git.c
@@ -2018,6 +2018,14 @@ name|RUN_SETUP
 block|}
 block|,
 block|{
+literal|"replace"
+block|,
+name|cmd_replace
+block|,
+name|RUN_SETUP
+block|}
+block|,
+block|{
 literal|"repo-config"
 block|,
 name|cmd_config
