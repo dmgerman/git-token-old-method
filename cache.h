cb8c8c4f67df3cd5cfc9c8860da7235b2df23d3e
Johannes Schindelin <johannes.schindelin@gmx.de>
2009-02-19 20:10:49 +0100

    Introduce the function strip_path_suffix()
    The function strip_path_suffix() will try to strip a given suffix from
    a given path.  The suffix must start at a directory boundary (i.e. "core"
    is not a path suffix of "libexec/git-core", but "git-core" is).
    
    Arbitrary runs of directory separators ("slashes") are assumed identical.
    
    Example:
    
    	strip_path_suffix("C:\\msysgit/\\libexec\\git-core",
    		"libexec///git-core", &prefix)
    
    will set prefix to "C:\\msysgit" and return 0.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>
    Acked-by: Johannes Sixt <j6t@kdbg.org>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    cregit: 4fcc86b07db26da17f17af0fd777cab78fcaaf4c http://github.com/git/git/commit/4fcc86b07db26da17f17af0fd777cab78fcaaf4c

--- cache.h
+++ cache.h
@@ -3769,6 +3769,23 @@ name|prefix_list
 parameter_list|)
 function_decl|;
 end_function_decl
+begin_function_decl
+name|char
+modifier|*
+name|strip_path_suffix
+parameter_list|(
+specifier|const
+name|char
+modifier|*
+name|path
+parameter_list|,
+specifier|const
+name|char
+modifier|*
+name|suffix
+parameter_list|)
+function_decl|;
+end_function_decl
 begin_comment
 comment|/* Read and unpack a sha1 file into memory, write memory to a sha1 file */
 end_comment
