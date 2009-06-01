ce728d32d35a45ec0d7bbf4302397b7c63def8aa
René Scharfe <rene.scharfe@lsrfire.ath.cx>
2009-06-01 23:53:05 +0200

    grep: fix colouring of matches with zero length
    If a zero-length match is encountered, break out of loop and show the rest
    of the line uncoloured.  Otherwise we'd be looping forever, trying to make
    progress by advancing the pointer by zero characters.
    
    Signed-off-by: Rene Scharfe <rene.scharfe@lsrfire.ath.cx>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    cregit: 1f5b9cc40e80a8a7902236a26844be138887b1d9 http://github.com/git/git/commit/1f5b9cc40e80a8a7902236a26844be138887b1d9

--- grep.c
+++ grep.c
@@ -2843,6 +2843,17 @@ name|eflags
 argument_list|)
 condition|)
 block|{
+if|if
+condition|(
+name|match
+operator|.
+name|rm_so
+operator|==
+name|match
+operator|.
+name|rm_eo
+condition|)
+break|break;
 name|printf
 argument_list|(
 literal|"%.*s%s%.*s%s"
