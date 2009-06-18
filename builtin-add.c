495d6662e416170cf51a9dc0541fefc1461c1d83
Stephen Boyd <bebarino@gmail.com>
2009-06-18 02:17:54 -0700

    add: allow configurations to be overriden by command line
    Don't call git_config after parsing the command line options, otherwise
    the config settings will override any settings made by the command line.
    
    This can be seen by setting add.ignore_errors and then specifying
    --no-ignore-errors when using git-add.
    
    Signed-off-by: Stephen Boyd <bebarino@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    cregit: ed342fdea0b7fd466ed3257c5e68aa81c391b782 http://github.com/git/git/commit/ed342fdea0b7fd466ed3257c5e68aa81c391b782

--- builtin-add.c
+++ builtin-add.c
@@ -1424,6 +1424,13 @@ decl_stmt|;
 name|int
 name|require_pathspec
 decl_stmt|;
+name|git_config
+argument_list|(
+name|add_config
+argument_list|,
+name|NULL
+argument_list|)
+expr_stmt|;
 name|argc
 operator|=
 name|parse_options
@@ -1461,13 +1468,6 @@ name|argv
 argument_list|,
 name|prefix
 argument_list|)
-argument_list|)
-expr_stmt|;
-name|git_config
-argument_list|(
-name|add_config
-argument_list|,
-name|NULL
 argument_list|)
 expr_stmt|;
 if|if
