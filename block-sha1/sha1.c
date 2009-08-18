b9c03e0d2183475b7c79f9075c30e627b2ad4eb1
Nicolas Pitre <nico@cam.org>
2009-08-17 20:09:56 -0400

    remove ARM and Mozilla SHA1 implementations
    They are both slower than the new BLK_SHA1 implementation, so it is
    pointless to keep them around.
    
    Signed-off-by: Nicolas Pitre <nico@cam.org>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    cregit: 30ae47b4cc19dce42f51b4fa378d315a71b08957 http://github.com/git/git/commit/30ae47b4cc19dce42f51b4fa378d315a71b08957

--- block-sha1/sha1.c
+++ block-sha1/sha1.c
@@ -1,6 +1,6 @@
 begin_unit
 begin_comment
-comment|/*  * Based on the Mozilla SHA1 (see mozilla-sha1/sha1.c),  * optimized to do word accesses rather than byte accesses,  * and to avoid unnecessary copies into the context array.  */
+comment|/*  * SHA1 routine optimized to do word accesses rather than byte accesses,  * and to avoid unnecessary copies into the context array.  *  * This was initially based on the Mozilla SHA1 implementation, although  * none of the original Mozilla code remains.  */
 end_comment
 begin_comment
 comment|/* this is only to get definitions for memcpy(), ntohl() and htonl() */
