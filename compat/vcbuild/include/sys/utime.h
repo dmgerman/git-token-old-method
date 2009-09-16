e88fcb77ae42223fe96476f46d2930d7d41e1942
Frank Li <lznuaa@gmail.com>
2009-09-16 10:20:24 +0200

    Add MinGW header files to build git with MSVC
    Added the header files dirent.h, unistd.h and utime.h
    Add alloca.h, which simply includes malloc.h, which defines alloca().
    
    Signed-off-by: Frank Li <lznuaa@gmail.com>
    Signed-off-by: Marius Storm-Olsen <mstormo@gmail.com>
    Acked-by: Johannes Sixt <j6t@kdbg.org>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    cregit: 16fe1e03694e75a3923c3a757ec946aa6098aec7 http://github.com/git/git/commit/16fe1e03694e75a3923c3a757ec946aa6098aec7

---
+++ compat/vcbuild/include/sys/utime.h
@@ -0,0 +1,72 @@
+begin_unit
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|_UTIME_H_
+end_ifndef
+begin_define
+DECL|macro|_UTIME_H_
+define|#
+directive|define
+name|_UTIME_H_
+end_define
+begin_comment
+comment|/*  * UTIME.H  * This file has no copyright assigned and is placed in the Public Domain.  * This file is a part of the mingw-runtime package.  *  * The mingw-runtime package and its code is distributed in the hope that it  * will be useful but WITHOUT ANY WARRANTY.  ALL WARRANTIES, EXPRESSED OR  * IMPLIED ARE HEREBY DISCLAIMED.  This includes but is not limited to  * warranties of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  *  * You are free to use this package and its code without limitation.  */
+end_comment
+begin_comment
+comment|/*  * Structure used by _utime function.  */
+end_comment
+begin_struct
+DECL|struct|_utimbuf
+struct|struct
+name|_utimbuf
+block|{
+DECL|member|actime
+name|time_t
+name|actime
+decl_stmt|;
+comment|/* Access time */
+DECL|member|modtime
+name|time_t
+name|modtime
+decl_stmt|;
+comment|/* Modification time */
+block|}
+struct|;
+end_struct
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|_NO_OLDNAMES
+end_ifndef
+begin_comment
+comment|/* NOTE: Must be the same as _utimbuf above. */
+end_comment
+begin_struct
+DECL|struct|utimbuf
+struct|struct
+name|utimbuf
+block|{
+DECL|member|actime
+name|time_t
+name|actime
+decl_stmt|;
+DECL|member|modtime
+name|time_t
+name|modtime
+decl_stmt|;
+block|}
+struct|;
+end_struct
+begin_endif
+endif|#
+directive|endif
+end_endif
+begin_comment
+comment|/* Not _NO_OLDNAMES */
+end_comment
+begin_endif
+endif|#
+directive|endif
+end_endif
+end_unit
