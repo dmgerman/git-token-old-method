0aab7ca6e8719f6b03c53e1e13534a2fb7d7b709
Shawn O. Pearce <spearce@spearce.org>
2009-10-30 17:47:30 -0700

    Move WebDAV HTTP push under remote-curl
    The remote helper interface now supports the push capability,
    which can be used to ask the implementation to push one or more
    specs to the remote repository.  For remote-curl we implement this
    by calling the existing WebDAV based git-http-push executable.
    
    Internally the helper interface uses the push_refs transport hook
    so that the complexity of the refspec parsing and matching can be
    reused between remote implementations.  When possible however the
    helper protocol uses source ref name rather than the source SHA-1,
    thereby allowing the helper to access this name if it is useful.
    
    >From Clemens Buchacher <drizzd@aon.at>:
     update http tests according to remote-curl capabilities
    
     o Pushing packed refs is now fixed.
    
     o The transport helper fails if refs are already up-to-date. Add
       a test for that.
    
     o The transport helper will notice if refs are already
       up-to-date. We therefore need to update server info in the
       unpacked-refs test.
    
     o The transport helper will purge deleted branches automatically.
    
     o Use a variable ($ORIG_HEAD) instead of full SHA-1 name.
    
    Signed-off-by: Tay Ray Chuan <rctay89@gmail.com>
    Signed-off-by: Clemens Buchacher <drizzd@aon.at>
    Signed-off-by: Shawn O. Pearce <spearce@spearce.org>
    CC: Daniel Barkalow <barkalow@iabervon.org>
    CC: Mike Hommey <mh@glandium.org>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    cregit: ae4efe195752c27cb25fca9451852c0f4eebdb28 http://github.com/git/git/commit/ae4efe195752c27cb25fca9451852c0f4eebdb28

--- http-push.c
+++ http-push.c
@@ -399,6 +399,13 @@ name|dry_run
 decl_stmt|;
 end_decl_stmt
 begin_decl_stmt
+DECL|variable|helper_status
+specifier|static
+name|int
+name|helper_status
+decl_stmt|;
+end_decl_stmt
+begin_decl_stmt
 DECL|variable|objects
 specifier|static
 name|struct
@@ -10019,6 +10026,23 @@ name|strcmp
 argument_list|(
 name|arg
 argument_list|,
+literal|"--helper-status"
+argument_list|)
+condition|)
+block|{
+name|helper_status
+operator|=
+literal|1
+expr_stmt|;
+continue|continue;
+block|}
+if|if
+condition|(
+operator|!
+name|strcmp
+argument_list|(
+name|arg
+argument_list|,
 literal|"--verbose"
 argument_list|)
 condition|)
@@ -10478,6 +10502,7 @@ operator|==
 operator|-
 literal|1
 condition|)
+block|{
 name|fprintf
 argument_list|(
 name|stderr
@@ -10490,6 +10515,21 @@ literal|0
 index|]
 argument_list|)
 expr_stmt|;
+if|if
+condition|(
+name|helper_status
+condition|)
+name|printf
+argument_list|(
+literal|"error %s cannot remove\n"
+argument_list|,
+name|refspec
+index|[
+literal|0
+index|]
+argument_list|)
+expr_stmt|;
+block|}
 goto|goto
 name|cleanup
 goto|;
@@ -10538,6 +10578,15 @@ argument_list|(
 name|stderr
 argument_list|,
 literal|"No refs in common and none specified; doing nothing.\n"
+argument_list|)
+expr_stmt|;
+if|if
+condition|(
+name|helper_status
+condition|)
+name|printf
+argument_list|(
+literal|"error null no match\n"
 argument_list|)
 expr_stmt|;
 name|rc
@@ -10638,12 +10687,39 @@ operator|->
 name|name
 argument_list|)
 expr_stmt|;
+if|if
+condition|(
+name|helper_status
+condition|)
+name|printf
+argument_list|(
+literal|"error %s cannot remove\n"
+argument_list|,
+name|ref
+operator|->
+name|name
+argument_list|)
+expr_stmt|;
 name|rc
 operator|=
 operator|-
 literal|4
 expr_stmt|;
 block|}
+elseif|else
+if|if
+condition|(
+name|helper_status
+condition|)
+name|printf
+argument_list|(
+literal|"ok %s\n"
+argument_list|,
+name|ref
+operator|->
+name|name
+argument_list|)
+expr_stmt|;
 name|new_refs
 operator|++
 expr_stmt|;
@@ -10677,6 +10753,19 @@ argument_list|(
 name|stderr
 argument_list|,
 literal|"'%s': up-to-date\n"
+argument_list|,
+name|ref
+operator|->
+name|name
+argument_list|)
+expr_stmt|;
+if|if
+condition|(
+name|helper_status
+condition|)
+name|printf
+argument_list|(
+literal|"ok %s up to date\n"
 argument_list|,
 name|ref
 operator|->
@@ -10744,6 +10833,19 @@ argument_list|,
 name|ref
 operator|->
 name|peer_ref
+operator|->
+name|name
+argument_list|)
+expr_stmt|;
+if|if
+condition|(
+name|helper_status
+condition|)
+name|printf
+argument_list|(
+literal|"error %s non-fast forward\n"
+argument_list|,
+name|ref
 operator|->
 name|name
 argument_list|)
@@ -10847,7 +10949,22 @@ if|if
 condition|(
 name|dry_run
 condition|)
+block|{
+if|if
+condition|(
+name|helper_status
+condition|)
+name|printf
+argument_list|(
+literal|"ok %s\n"
+argument_list|,
+name|ref
+operator|->
+name|name
+argument_list|)
+expr_stmt|;
 continue|continue;
+block|}
 comment|/* Lock remote branch ref */
 name|ref_lock
 operator|=
@@ -10872,6 +10989,19 @@ argument_list|(
 name|stderr
 argument_list|,
 literal|"Unable to lock remote branch %s\n"
+argument_list|,
+name|ref
+operator|->
+name|name
+argument_list|)
+expr_stmt|;
+if|if
+condition|(
+name|helper_status
+condition|)
+name|printf
+argument_list|(
+literal|"error %s lock error\n"
 argument_list|,
 name|ref
 operator|->
@@ -11116,6 +11246,26 @@ argument_list|(
 name|stderr
 argument_list|,
 literal|"    done\n"
+argument_list|)
+expr_stmt|;
+if|if
+condition|(
+name|helper_status
+condition|)
+name|printf
+argument_list|(
+literal|"%s %s\n"
+argument_list|,
+operator|!
+name|rc
+condition|?
+literal|"ok"
+else|:
+literal|"error"
+argument_list|,
+name|ref
+operator|->
+name|name
 argument_list|)
 expr_stmt|;
 name|unlock_remote
