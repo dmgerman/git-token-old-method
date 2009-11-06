b6bbc485c91026feba4bf6dce424e5e0dbb09083
Brian Collins <bricollins@gmail.com>
2009-11-06 01:22:35 -0800

    grep: Allow case insensitive search of fixed-strings
    "git grep" currently an error when you combine the -F and -i flags.
    This isn't in line with how GNU grep handles it.
    
    This patch allows the simultaneous use of those flags.
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Brian Collins <bricollins@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    cregit: 5183bf67278ce5a0da9779d74f05169beac219b8 http://github.com/git/git/commit/5183bf67278ce5a0da9779d74f05169beac219b8

--- builtin-grep.c
+++ builtin-grep.c
@@ -1700,9 +1700,7 @@ if|if
 condition|(
 name|opt
 operator|->
-name|regflags
-operator|&
-name|REG_ICASE
+name|ignore_case
 condition|)
 name|push_arg
 argument_list|(
@@ -3692,7 +3690,7 @@ argument_list|,
 literal|"show non-matching lines"
 argument_list|)
 block|,
-name|OPT_BIT
+name|OPT_BOOLEAN
 argument_list|(
 literal|'i'
 argument_list|,
@@ -3701,11 +3699,9 @@ argument_list|,
 operator|&
 name|opt
 operator|.
-name|regflags
+name|ignore_case
 argument_list|,
 literal|"case insensitive matching"
-argument_list|,
-name|REG_ICASE
 argument_list|)
 block|,
 name|OPT_BOOLEAN
@@ -4468,6 +4464,23 @@ name|die
 argument_list|(
 literal|"no pattern given."
 argument_list|)
+expr_stmt|;
+if|if
+condition|(
+operator|!
+name|opt
+operator|.
+name|fixed
+operator|&&
+name|opt
+operator|.
+name|ignore_case
+condition|)
+name|opt
+operator|.
+name|regflags
+operator||=
+name|REG_ICASE
 expr_stmt|;
 if|if
 condition|(
