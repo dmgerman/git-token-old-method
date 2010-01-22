4bcd1ce6f2140b25e959f1463998d4b7fdcdec83
Linus Torvalds <torvalds@linux-foundation.org>
2010-01-22 07:34:44 -0800

    make "mktag" a built-in
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    cregit: 112dd5146589ecb76d52bf349bd51dbe2936e936 http://github.com/git/git/commit/112dd5146589ecb76d52bf349bd51dbe2936e936

--- git.c
+++ git.c
@@ -1973,6 +1973,14 @@ name|RUN_SETUP
 block|}
 block|,
 block|{
+literal|"mktag"
+block|,
+name|cmd_mktag
+block|,
+name|RUN_SETUP
+block|}
+block|,
+block|{
 literal|"mktree"
 block|,
 name|cmd_mktree
