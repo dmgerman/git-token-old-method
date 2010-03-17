b4d12e04658285735af8f1a451679b0abea9df6c
Lars R. Damerow <lars@pixar.com>
2010-03-17 12:55:52 -0700

    truncate cwd string before printing error message
    Without this truncation the error message printed only shows the cwd
    from the start of the search, not where it failed.
    
    Signed-off-by: Lars R. Damerow <lars@pixar.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    cregit: 502ffe3491bb6a3aff3f93e7eea19c01cf30c337 http://github.com/git/git/commit/502ffe3491bb6a3aff3f93e7eea19c01cf30c337

--- setup.c
+++ setup.c
@@ -1986,6 +1986,14 @@ argument_list|(
 literal|".."
 argument_list|)
 condition|)
+block|{
+name|cwd
+index|[
+name|offset
+index|]
+operator|=
+literal|'\0'
+expr_stmt|;
 name|die_errno
 argument_list|(
 literal|"Cannot change to '%s/..'"
@@ -1993,6 +2001,7 @@ argument_list|,
 name|cwd
 argument_list|)
 expr_stmt|;
+block|}
 block|}
 name|inside_git_dir
 operator|=
