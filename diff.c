4b5761f6a11b44974512c8ac27975dd6516c10bd
Jeff King <peff@peff.net>
2010-04-17 13:41:08 -0400

    diff: use large integers for diffstat calculations
    The diffstat "added" and "changed" fields generally store
    line counts; however, for binary files, they store file
    sizes. Since we store and print these values as ints, a
    diffstat on a file larger than 2G can show a negative size.
    Instead, let's use uintmax_t, which should be at least 64
    bits on modern platforms.
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    cregit: 0974c117ff4e17e8b6300519cae0fbc67d34adaa http://github.com/git/git/commit/0974c117ff4e17e8b6300519cae0fbc67d34adaa

--- diff.c
+++ diff.c
@@ -5793,8 +5793,7 @@ literal|1
 decl_stmt|;
 DECL|member|added
 DECL|member|deleted
-name|unsigned
-name|int
+name|uintmax_t
 name|added
 decl_stmt|,
 name|deleted
@@ -6338,7 +6337,7 @@ name|dels
 init|=
 literal|0
 decl_stmt|;
-name|int
+name|uintmax_t
 name|max_change
 init|=
 literal|0
@@ -6507,7 +6506,7 @@ index|[
 name|i
 index|]
 decl_stmt|;
-name|int
+name|uintmax_t
 name|change
 init|=
 name|file
@@ -6640,7 +6639,7 @@ index|]
 operator|->
 name|print_name
 decl_stmt|;
-name|int
+name|uintmax_t
 name|added
 init|=
 name|data
@@ -6652,7 +6651,7 @@ index|]
 operator|->
 name|added
 decl_stmt|;
-name|int
+name|uintmax_t
 name|deleted
 init|=
 name|data
@@ -6762,7 +6761,9 @@ name|options
 operator|->
 name|file
 argument_list|,
-literal|"%s%d%s"
+literal|"%s%"
+name|PRIuMAX
+literal|"%s"
 argument_list|,
 name|del_c
 argument_list|,
@@ -6786,7 +6787,9 @@ name|options
 operator|->
 name|file
 argument_list|,
-literal|"%s%d%s"
+literal|"%s%"
+name|PRIuMAX
+literal|"%s"
 argument_list|,
 name|add_c
 argument_list|,
@@ -6945,7 +6948,9 @@ name|options
 operator|->
 name|file
 argument_list|,
-literal|"%5d%s"
+literal|"%5"
+name|PRIuMAX
+literal|"%s"
 argument_list|,
 name|added
 operator|+
@@ -7257,7 +7262,11 @@ name|options
 operator|->
 name|file
 argument_list|,
-literal|"%d\t%d\t"
+literal|"%"
+name|PRIuMAX
+literal|"\t%"
+name|PRIuMAX
+literal|"\t"
 argument_list|,
 name|file
 operator|->
