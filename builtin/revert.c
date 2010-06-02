ca7bf2e002fbbc6a274fce229a1f40879d07b3ed
Christian Couder <chriscool@tuxfamily.org>
2010-06-02 07:58:34 +0200

    revert: cleanup code for -x option
    There was some dead code and option -x appeared in the short
    help message of git revert (when running "git revert -h")
    which was wrong.
    
    Signed-off-by: Christian Couder <chriscool@tuxfamily.org>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    cregit: 831244bd0df6871c618fc8becbb5c0f1fb8f5459 http://github.com/git/git/commit/831244bd0df6871c618fc8becbb5c0f1fb8f5459

--- builtin/revert.c
+++ builtin/revert.c
@@ -288,18 +288,6 @@ argument_list|)
 block|,
 name|OPT_BOOLEAN
 argument_list|(
-literal|'x'
-argument_list|,
-name|NULL
-argument_list|,
-operator|&
-name|no_replay
-argument_list|,
-literal|"append commit name when cherry-picking"
-argument_list|)
-block|,
-name|OPT_BOOLEAN
-argument_list|(
 literal|'r'
 argument_list|,
 name|NULL
@@ -377,6 +365,18 @@ name|cp_extra
 index|[]
 init|=
 block|{
+name|OPT_BOOLEAN
+argument_list|(
+literal|'x'
+argument_list|,
+name|NULL
+argument_list|,
+operator|&
+name|no_replay
+argument_list|,
+literal|"append commit name"
+argument_list|)
+block|,
 name|OPT_BOOLEAN
 argument_list|(
 literal|0
@@ -2170,21 +2170,6 @@ argument_list|,
 name|argv
 argument_list|)
 expr_stmt|;
-comment|/* this is copied from the shell script, but it's never triggered... */
-if|if
-condition|(
-name|action
-operator|==
-name|REVERT
-operator|&&
-operator|!
-name|no_replay
-condition|)
-name|die
-argument_list|(
-literal|"revert is incompatible with replay"
-argument_list|)
-expr_stmt|;
 if|if
 condition|(
 name|allow_ff
@@ -2997,10 +2982,6 @@ name|edit
 operator|=
 literal|1
 expr_stmt|;
-name|no_replay
-operator|=
-literal|1
-expr_stmt|;
 name|action
 operator|=
 name|REVERT
@@ -3035,10 +3016,6 @@ modifier|*
 name|prefix
 parameter_list|)
 block|{
-name|no_replay
-operator|=
-literal|0
-expr_stmt|;
 name|action
 operator|=
 name|CHERRY_PICK
