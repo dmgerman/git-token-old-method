f6cc41b4fbb1bcc798c519bb8d7d6d5e6ef00607
Christian Couder <chriscool@tuxfamily.org>
2010-06-12 18:05:12 +0200

    commit: use value of GIT_REFLOG_ACTION env variable as reflog message
    The environment variable GIT_REFLOG_ACTION was used by git-commit.sh,
    but when it was converted to a builtin
    (f5bbc3225c4b073a7ff3218164a0c820299bc9c6, Port git commit to C,
    Nov 8 2007) this was lost.
    
    Let's use it again as it is more user friendly when reverting or
    cherry-picking to see "revert" or "cherry-pick" in the reflog rather
    than to just see "commit".
    
    Signed-off-by: Christian Couder <chriscool@tuxfamily.org>
    Acked-by: Jonathan Nieder <jrnieder@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    cregit: 643cb5f7c99e77a4a3e97213dd226a7dc2650c02 http://github.com/git/git/commit/643cb5f7c99e77a4a3e97213dd226a7dc2650c02

--- builtin/commit.c
+++ builtin/commit.c
@@ -6561,11 +6561,23 @@ literal|1
 return|;
 block|}
 comment|/* Determine parents */
+name|reflog_msg
+operator|=
+name|getenv
+argument_list|(
+literal|"GIT_REFLOG_ACTION"
+argument_list|)
+expr_stmt|;
 if|if
 condition|(
 name|initial_commit
 condition|)
 block|{
+if|if
+condition|(
+operator|!
+name|reflog_msg
+condition|)
 name|reflog_msg
 operator|=
 literal|"commit (initial)"
@@ -6587,6 +6599,11 @@ name|commit
 modifier|*
 name|commit
 decl_stmt|;
+if|if
+condition|(
+operator|!
+name|reflog_msg
+condition|)
 name|reflog_msg
 operator|=
 literal|"commit (amend)"
@@ -6660,6 +6677,11 @@ name|FILE
 modifier|*
 name|fp
 decl_stmt|;
+if|if
+condition|(
+operator|!
+name|reflog_msg
+condition|)
 name|reflog_msg
 operator|=
 literal|"commit (merge)"
@@ -6846,6 +6868,11 @@ expr_stmt|;
 block|}
 else|else
 block|{
+if|if
+condition|(
+operator|!
+name|reflog_msg
+condition|)
 name|reflog_msg
 operator|=
 literal|"commit"
