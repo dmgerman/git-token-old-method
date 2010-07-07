6cf869b131219f9adf370b31fe9724995c5317f8
Pierre Habouzit <madcoder@debian.org>
2010-07-07 09:47:20 +0200

    fix git branch -m in presence of cross devices
    When you have for example a bare repository stored on NFS, and that you
    create new workdirs locally (using contrib's git-new-workdir), logs/refs
    is a symlink to a different device. Hence when the reflogs are renamed,
    all must happen below logs/refs or one gets cross device rename errors
    like:
    
      git branch -m foo
      error: unable to move logfile logs/refs/heads/master to tmp-renamed-log: Invalid cross-device link
      fatal: Branch rename failed
    
    The fix is hence to use logs/refs/.tmp-renamed-log as a temporary log
    name, instead of just tmp-renamed-log.
    
    Signed-off-by: Pierre Habouzit <madcoder@debian.org>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    cregit: 765c22588d9e410bf0100c4991b1945134da6959 http://github.com/git/git/commit/765c22588d9e410bf0100c4991b1945134da6959

--- refs.c
+++ refs.c
@@ -5621,6 +5621,16 @@ name|ret
 return|;
 block|}
 end_function
+begin_comment
+comment|/*  * People using contrib's git-new-workdir have .git/logs/refs ->  * /some/other/path/.git/logs/refs, and that may live on another device.  *  * IOW, to avoid cross device rename errors, the temporary renamed log must  * live into logs/refs.  */
+end_comment
+begin_define
+DECL|macro|TMP_RENAMED_LOG
+define|#
+directive|define
+name|TMP_RENAMED_LOG
+value|"logs/refs/.tmp-renamed-log"
+end_define
 begin_function
 DECL|function|rename_ref
 name|int
@@ -5866,14 +5876,16 @@ argument_list|)
 argument_list|,
 name|git_path
 argument_list|(
-literal|"tmp-renamed-log"
+name|TMP_RENAMED_LOG
 argument_list|)
 argument_list|)
 condition|)
 return|return
 name|error
 argument_list|(
-literal|"unable to move logfile logs/%s to tmp-renamed-log: %s"
+literal|"unable to move logfile logs/%s to "
+name|TMP_RENAMED_LOG
+literal|": %s"
 argument_list|,
 name|oldref
 argument_list|,
@@ -6012,7 +6024,7 @@ name|rename
 argument_list|(
 name|git_path
 argument_list|(
-literal|"tmp-renamed-log"
+name|TMP_RENAMED_LOG
 argument_list|)
 argument_list|,
 name|git_path
@@ -6068,7 +6080,9 @@ else|else
 block|{
 name|error
 argument_list|(
-literal|"unable to move logfile tmp-renamed-log to logs/%s: %s"
+literal|"unable to move logfile "
+name|TMP_RENAMED_LOG
+literal|" to logs/%s: %s"
 argument_list|,
 name|newref
 argument_list|,
@@ -6274,7 +6288,7 @@ name|rename
 argument_list|(
 name|git_path
 argument_list|(
-literal|"tmp-renamed-log"
+name|TMP_RENAMED_LOG
 argument_list|)
 argument_list|,
 name|git_path
@@ -6287,7 +6301,9 @@ argument_list|)
 condition|)
 name|error
 argument_list|(
-literal|"unable to restore logfile %s from tmp-renamed-log: %s"
+literal|"unable to restore logfile %s from "
+name|TMP_RENAMED_LOG
+literal|": %s"
 argument_list|,
 name|oldref
 argument_list|,
