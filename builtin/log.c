131a64a0ec74ae41eddc897fd78f466b1d5810cf
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2011-02-22 23:41:57 +0000

    i18n: git-log basic messages
    Signed-off-by: Ævar Arnfjörð Bjarmason <avarab@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    cregit: 5c5f1d7ce116b945ad74b4ae67ae36a2ddeb610c http://github.com/git/git/commit/5c5f1d7ce116b945ad74b4ae67ae36a2ddeb610c

--- builtin/log.c
+++ builtin/log.c
@@ -567,7 +567,10 @@ literal|0
 condition|)
 name|die
 argument_list|(
+name|_
+argument_list|(
 literal|"invalid --decorate option: %s"
+argument_list|)
 argument_list|,
 name|arg
 argument_list|)
@@ -634,7 +637,10 @@ block|}
 else|else
 name|die
 argument_list|(
+name|_
+argument_list|(
 literal|"unrecognized argument: %s"
+argument_list|)
 argument_list|,
 name|arg
 argument_list|)
@@ -812,7 +818,10 @@ expr_stmt|;
 block|}
 name|printf
 argument_list|(
+name|_
+argument_list|(
 literal|"Final output: %d %s\n"
+argument_list|)
 argument_list|,
 name|nr
 argument_list|,
@@ -1173,7 +1182,10 @@ argument_list|)
 condition|)
 name|die
 argument_list|(
+name|_
+argument_list|(
 literal|"revision walk setup failed"
+argument_list|)
 argument_list|)
 expr_stmt|;
 if|if
@@ -1705,7 +1717,10 @@ condition|)
 return|return
 name|error
 argument_list|(
+name|_
+argument_list|(
 literal|"Could not read object %s"
+argument_list|)
 argument_list|,
 name|sha1_to_hex
 argument_list|(
@@ -2262,7 +2277,10 @@ name|ret
 operator|=
 name|error
 argument_list|(
+name|_
+argument_list|(
 literal|"Could not read object %s"
+argument_list|)
 argument_list|,
 name|sha1_to_hex
 argument_list|(
@@ -2409,7 +2427,10 @@ name|ret
 operator|=
 name|error
 argument_list|(
+name|_
+argument_list|(
 literal|"Unknown type: %d"
+argument_list|)
 argument_list|,
 name|o
 operator|->
@@ -2940,7 +2961,10 @@ name|value
 condition|)
 name|die
 argument_list|(
+name|_
+argument_list|(
 literal|"format.headers without value"
+argument_list|)
 argument_list|)
 expr_stmt|;
 name|add_header
@@ -3366,7 +3390,10 @@ condition|)
 return|return
 name|error
 argument_list|(
+name|_
+argument_list|(
 literal|"name of output directory is too long"
+argument_list|)
 argument_list|)
 return|;
 if|if
@@ -3451,7 +3478,10 @@ condition|)
 return|return
 name|error
 argument_list|(
+name|_
+argument_list|(
 literal|"Cannot open patch file %s"
+argument_list|)
 argument_list|,
 name|filename
 operator|.
@@ -3525,7 +3555,10 @@ literal|2
 condition|)
 name|die
 argument_list|(
+name|_
+argument_list|(
 literal|"Need exactly one range."
+argument_list|)
 argument_list|)
 expr_stmt|;
 name|o1
@@ -3582,7 +3615,10 @@ operator|)
 condition|)
 name|die
 argument_list|(
+name|_
+argument_list|(
 literal|"Not a range."
+argument_list|)
 argument_list|)
 expr_stmt|;
 name|init_patch_ids
@@ -3641,7 +3677,10 @@ argument_list|)
 condition|)
 name|die
 argument_list|(
+name|_
+argument_list|(
 literal|"revision walk setup failed"
+argument_list|)
 argument_list|)
 expr_stmt|;
 while|while
@@ -3804,7 +3843,10 @@ literal|1
 condition|)
 name|die
 argument_list|(
+name|_
+argument_list|(
 literal|"Could not extract email from committer identity."
+argument_list|)
 argument_list|)
 expr_stmt|;
 name|strbuf_addf
@@ -3999,7 +4041,10 @@ name|CMIT_FMT_EMAIL
 condition|)
 name|die
 argument_list|(
+name|_
+argument_list|(
 literal|"Cover letter needs email format"
+argument_list|)
 argument_list|)
 expr_stmt|;
 name|committer
@@ -4460,7 +4505,10 @@ name|z
 condition|)
 name|die
 argument_list|(
+name|_
+argument_list|(
 literal|"insane in-reply-to: %s"
+argument_list|)
 argument_list|,
 name|msg_id
 argument_list|)
@@ -4837,7 +4885,10 @@ name|dir
 condition|)
 name|die
 argument_list|(
+name|_
+argument_list|(
 literal|"Two output directories?"
+argument_list|)
 argument_list|)
 expr_stmt|;
 operator|*
@@ -5914,7 +5965,10 @@ name|endpos
 condition|)
 name|die
 argument_list|(
+name|_
+argument_list|(
 literal|"bogus committer info %s"
+argument_list|)
 argument_list|,
 name|committer
 argument_list|)
@@ -6185,7 +6239,10 @@ name|keep_subject
 condition|)
 name|die
 argument_list|(
+name|_
+argument_list|(
 literal|"-n and -k are mutually exclusive."
+argument_list|)
 argument_list|)
 expr_stmt|;
 if|if
@@ -6196,7 +6253,10 @@ name|subject_prefix
 condition|)
 name|die
 argument_list|(
+name|_
+argument_list|(
 literal|"--subject-prefix and -k are mutually exclusive."
+argument_list|)
 argument_list|)
 expr_stmt|;
 name|argc
@@ -6222,7 +6282,10 @@ literal|1
 condition|)
 name|die
 argument_list|(
+name|_
+argument_list|(
 literal|"unrecognized argument: %s"
+argument_list|)
 argument_list|,
 name|argv
 index|[
@@ -6385,7 +6448,10 @@ name|use_stdout
 condition|)
 name|die
 argument_list|(
+name|_
+argument_list|(
 literal|"standard output, or directory, which one?"
+argument_list|)
 argument_list|)
 expr_stmt|;
 if|if
@@ -6405,7 +6471,10 @@ name|EEXIST
 condition|)
 name|die_errno
 argument_list|(
+name|_
+argument_list|(
 literal|"Could not create directory '%s'"
+argument_list|)
 argument_list|,
 name|output_directory
 argument_list|)
@@ -6638,7 +6707,10 @@ argument_list|)
 condition|)
 name|die
 argument_list|(
+name|_
+argument_list|(
 literal|"revision walk setup failed"
+argument_list|)
 argument_list|)
 expr_stmt|;
 name|rev
@@ -7003,7 +7075,10 @@ argument_list|)
 condition|)
 name|die
 argument_list|(
+name|_
+argument_list|(
 literal|"Failed to create output files"
+argument_list|)
 argument_list|)
 expr_stmt|;
 name|shown
@@ -7418,9 +7493,12 @@ name|fprintf
 argument_list|(
 name|stderr
 argument_list|,
+name|_
+argument_list|(
 literal|"Could not find a tracked"
 literal|" remote branch, please"
 literal|" specify<upstream> manually.\n"
+argument_list|)
 argument_list|)
 expr_stmt|;
 name|usage_with_options
@@ -7493,7 +7571,10 @@ argument_list|)
 condition|)
 name|die
 argument_list|(
+name|_
+argument_list|(
 literal|"Unknown commit %s"
+argument_list|)
 argument_list|,
 name|head
 argument_list|)
@@ -7512,7 +7593,10 @@ argument_list|)
 condition|)
 name|die
 argument_list|(
+name|_
+argument_list|(
 literal|"Unknown commit %s"
+argument_list|)
 argument_list|,
 name|upstream
 argument_list|)
@@ -7596,7 +7680,10 @@ argument_list|)
 condition|)
 name|die
 argument_list|(
+name|_
+argument_list|(
 literal|"Unknown commit %s"
+argument_list|)
 argument_list|,
 name|limit
 argument_list|)
@@ -7612,7 +7699,10 @@ argument_list|)
 condition|)
 name|die
 argument_list|(
+name|_
+argument_list|(
 literal|"revision walk setup failed"
+argument_list|)
 argument_list|)
 expr_stmt|;
 while|while
