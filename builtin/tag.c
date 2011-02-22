652d8834f901c880221e1ffeff3ecb5a684ac0b7
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2011-02-22 23:42:10 +0000

    i18n: git-tag tag_template message
    Mark the tag_template message as translatable with N_() and then use
    it later with _(). We need to skip a test under GETTEXT_POISON that
    relies on the output having a leading newline.
    
    Signed-off-by: Ævar Arnfjörð Bjarmason <avarab@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    cregit: 7fbff25a539128f9940c03c9e975aad955f7e92a http://github.com/git/git/commit/7fbff25a539128f9940c03c9e975aad955f7e92a

--- builtin/tag.c
+++ builtin/tag.c
@@ -1136,10 +1136,13 @@ name|char
 name|tag_template
 index|[]
 init|=
+name|N_
+argument_list|(
 literal|"\n"
 literal|"#\n"
 literal|"# Write a tag message\n"
 literal|"#\n"
+argument_list|)
 decl_stmt|;
 end_decl_stmt
 begin_function
@@ -1634,11 +1637,17 @@ name|write_or_die
 argument_list|(
 name|fd
 argument_list|,
+name|_
+argument_list|(
 name|tag_template
+argument_list|)
 argument_list|,
 name|strlen
 argument_list|(
+name|_
+argument_list|(
 name|tag_template
+argument_list|)
 argument_list|)
 argument_list|)
 expr_stmt|;
