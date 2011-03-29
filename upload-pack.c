e2d4bdf960b104a1ecb6e386c209b0a8e5d5847e
Junio C Hamano <gitster@pobox.com>
2011-03-29 10:24:59 -0700

    enable "no-done" extension only when serving over smart-http
    Do not advertise no-done capability when upload-pack is not serving over
    smart-http, as there is no way for this server to know when it should stop
    reading in-flight data from the client, even though it is necessary to
    drain all the in-flight data in order to unblock the client.
    
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    Acked-by: Shawn O. Pearce <spearce@spearce.org>
    
    cregit: cf2ad8e64175bcf4b2bb693a9e4c0a89076111dd http://github.com/git/git/commit/cf2ad8e64175bcf4b2bb693a9e4c0a89076111dd

--- upload-pack.c
+++ upload-pack.c
@@ -3563,7 +3563,7 @@ name|capabilities
 init|=
 literal|"multi_ack thin-pack side-band"
 literal|" side-band-64k ofs-delta shallow no-progress"
-literal|" include-tag multi_ack_detailed no-done"
+literal|" include-tag multi_ack_detailed"
 decl_stmt|;
 name|struct
 name|object
@@ -3598,7 +3598,7 @@ name|packet_write
 argument_list|(
 literal|1
 argument_list|,
-literal|"%s %s%c%s\n"
+literal|"%s %s%c%s%s\n"
 argument_list|,
 name|sha1_to_hex
 argument_list|(
@@ -3610,6 +3610,12 @@ argument_list|,
 literal|0
 argument_list|,
 name|capabilities
+argument_list|,
+name|stateless_rpc
+condition|?
+literal|" no-done"
+else|:
+literal|""
 argument_list|)
 expr_stmt|;
 else|else
