5b2a989787847f3a9c4ff8e4113da05136a06608
Jeff King <peff@peff.net>
2011-06-11 19:04:11 +0000

    git skew: a tool to find how big a clock skew exists in the history
    > As you probably guessed from the specificity of the number, I wrote a
    > short program to actually traverse and find the worst skew. It takes
    > about 5 seconds to run (unsurprisingly, since it is doing the same full
    > traversal that we end up doing in the above numbers). So we could
    > "autoskew" by setting up the configuration on clone, and then
    > periodically updating it as part of "git gc".
    
    This patch doesn't implement auto-detection of skew, but is the program
    I used to calculate, and would provide the basis for such
    auto-detection. It would be interesting to see average skew numbers for
    popular repositories. You can run it as "git skew --all".
    
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    Signed-off-by: Ævar Arnfjörð Bjarmason <avarab@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    cregit: 188c35e36d22c39074a8b0ee411483a405065a9a http://github.com/git/git/commit/188c35e36d22c39074a8b0ee411483a405065a9a

--- git.c
+++ git.c
@@ -2430,6 +2430,14 @@ name|cmd_pack_refs
 block|,
 name|RUN_SETUP
 block|}
+block|,
+block|{
+literal|"skew"
+block|,
+name|cmd_skew
+block|,
+name|RUN_SETUP
+block|}
 block|, 	}
 decl_stmt|;
 name|int
