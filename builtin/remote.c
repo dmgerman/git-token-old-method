75652a64761979ce2ad653de382c4913a674febe
Carlos Martín Nieto <cmn@elego.de>
2011-10-15 07:04:25 +0200

    fetch: honor the user-provided refspecs when pruning refs
    If the user gave us refspecs on the command line, we should use those
    when deciding whether to prune a ref instead of relying on the
    refspecs in the config.
    
    Previously, running
    
        git fetch --prune origin refs/heads/master:refs/remotes/origin/master
    
    would delete every other ref under the origin namespace because we
    were using the refspec to filter the available refs but using the
    configured refspec to figure out if a ref had been deleted on the
    remote. This is clearly the wrong thing to do.
    
    Change prune_refs and get_stale_heads to simply accept a list of
    references and a list of refspecs. The caller of either function needs
    to decide what refspecs should be used to decide whether a ref is
    stale.
    
    Signed-off-by: Carlos Martín Nieto <cmn@elego.de>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    cregit: ed43de6ec35dfd4c4bd33ae9b5f2ebe38282209f http://github.com/git/git/commit/ed43de6ec35dfd4c4bd33ae9b5f2ebe38282209f

--- builtin/remote.c
+++ builtin/remote.c
@@ -1955,6 +1955,14 @@ argument_list|(
 name|states
 operator|->
 name|remote
+operator|->
+name|fetch
+argument_list|,
+name|states
+operator|->
+name|remote
+operator|->
+name|fetch_refspec_nr
 argument_list|,
 name|fetch_map
 argument_list|)
