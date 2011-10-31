069501c90e7c59103b8a86eb8a5b4dda79833108
Vincent van Ravesteijn <vfr@lyx.org>
2011-10-31 20:12:43 +0100

    Compile fix for MSVC: Include <io.h>
    This include is needed for _commit(..) which is used in mingw.h.
    
    Signed-off-by: Vincent van Ravesteijn <vfr@lyx.org>
    Acked-by: Erik Faye-Lund <kusmabite@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    cregit: 7b05949be007952c5f7708aac50e731534740574 http://github.com/git/git/commit/7b05949be007952c5f7708aac50e731534740574

--- compat/msvc.h
+++ compat/msvc.h
@@ -25,6 +25,11 @@ include|#
 directive|include
 file|<malloc.h>
 end_include
+begin_include
+include|#
+directive|include
+file|<io.h>
+end_include
 begin_comment
 comment|/* porting function */
 end_comment
