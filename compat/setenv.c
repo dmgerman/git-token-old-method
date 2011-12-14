803492e7264b4b2ca4f8b47618ecd5e3d7bb28ca
Erik Faye-Lund <kusmabite@gmail.com>
2011-12-14 15:07:09 +0100

    compat/setenv.c: error if name contains '='
    According to POSIX, setenv should error out with EINVAL if it's
    asked to set an environment variable whose name contains an equals
    sign. Implement this detail in our compatibility-fallback.
    
    Signed-off-by: Erik Faye-Lund <kusmabite@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    cregit: 6ac1b2a3b8bd970e9fc175c42927f00d4d465bbf http://github.com/git/git/commit/6ac1b2a3b8bd970e9fc175c42927f00d4d465bbf

--- compat/setenv.c
+++ compat/setenv.c
@@ -40,6 +40,13 @@ condition|(
 operator|!
 name|name
 operator|||
+name|strchr
+argument_list|(
+name|name
+argument_list|,
+literal|'='
+argument_list|)
+operator|||
 operator|!
 name|value
 condition|)
