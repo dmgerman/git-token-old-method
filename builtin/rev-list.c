f378724d137a7d93aafcff9b05b050c8fa495f93
Clemens Buchacher <drizzd@aon.at>
2012-02-13 21:17:11 +0100

    git rev-list: fix invalid typecast
    git rev-list passes rev_list_info, not rev_list objects. Without this
    fix, rev-list enables or disables the --verify-objects option depending
    on a read from an undefined memory location.
    
    Signed-off-by: Clemens Buchacher <drizzd@aon.at>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    cregit: cb8da70547af5cd5c8e03be5a236971951e81277 http://github.com/git/git/commit/cb8da70547af5cd5c8e03be5a236971951e81277

--- builtin/rev-list.c
+++ builtin/rev-list.c
@@ -798,7 +798,7 @@ name|cb_data
 parameter_list|)
 block|{
 name|struct
-name|rev_info
+name|rev_list_info
 modifier|*
 name|info
 init|=
@@ -818,6 +818,8 @@ expr_stmt|;
 if|if
 condition|(
 name|info
+operator|->
+name|revs
 operator|->
 name|verify_objects
 operator|&&
