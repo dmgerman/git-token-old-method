a9adaed8d969510cb685fbf43478343c291333e4
Jeff King <peff@peff.net>
2012-04-13 02:18:35 -0400

    http: clean up leak in init_curl_http_auth
    When we have a credential to give to curl, we must copy it
    into a "user:pass" buffer and then hand the buffer to curl.
    Old versions of curl did not copy the buffer, and we were
    expected to keep it valid. Newer versions of curl will copy
    the buffer.
    
    Our solution was to use a strbuf and detach it, giving
    ownership of the resulting buffer to curl. However, this
    meant that we were leaking the buffer on newer versions of
    curl, since curl was just copying it and throwing away the
    string we passed. Furthermore, when we replaced a
    credential (e.g., because our original one was rejected), we
    were also leaking on both old and new versions of curl.
    
    This got even worse in the last patch, which started
    replacing the credential (and thus leaking) on every http
    request.
    
    Instead, let's use a static buffer to make the ownership
    more clear and less leaky.  We already keep a static "struct
    credential", so we are only handling a single credential at
    a time, anyway.
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    cregit: aa0834a04e1d9d3ab81ecd4a4a138233e6e234ed http://github.com/git/git/commit/aa0834a04e1d9d3ab81ecd4a4a138233e6e234ed

--- http.c
+++ http.c
@@ -1200,6 +1200,7 @@ operator|.
 name|username
 condition|)
 block|{
+specifier|static
 name|struct
 name|strbuf
 name|up
@@ -1210,6 +1211,12 @@ name|credential_fill
 argument_list|(
 operator|&
 name|http_auth
+argument_list|)
+expr_stmt|;
+name|strbuf_reset
+argument_list|(
+operator|&
+name|up
 argument_list|)
 expr_stmt|;
 name|strbuf_addf
@@ -1234,13 +1241,9 @@ name|result
 argument_list|,
 name|CURLOPT_USERPWD
 argument_list|,
-name|strbuf_detach
-argument_list|(
-operator|&
 name|up
-argument_list|,
-name|NULL
-argument_list|)
+operator|.
+name|buf
 argument_list|)
 expr_stmt|;
 block|}
