94a7945ab232532ffdf83383959993032c6a8457
Jeff King <peff@peff.net>
2012-05-21 18:19:51 -0400

    fetch-pack: sort the list of incoming refs
    Having the list sorted means we can avoid some quadratic
    algorithms when comparing lists.
    
    These should typically be sorted already, but they do come
    from the remote, so let's be extra careful. Our ref-sorting
    implementation does a mergesort, so we do not have to care
    about performance degrading in the common case that the list
    is already sorted.
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    cregit: 9e8e704f0bc14da325aa63f639c6dc782c81e26f http://github.com/git/git/commit/9e8e704f0bc14da325aa63f639c6dc782c81e26f

--- builtin/fetch-pack.c
+++ builtin/fetch-pack.c
@@ -4119,6 +4119,14 @@ index|[
 literal|20
 index|]
 decl_stmt|;
+name|sort_ref_list
+argument_list|(
+operator|&
+name|ref
+argument_list|,
+name|ref_compare_name
+argument_list|)
+expr_stmt|;
 if|if
 condition|(
 name|is_repository_shallow
