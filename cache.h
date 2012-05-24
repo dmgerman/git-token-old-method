2f433ce0ec3f9d57e1ca5127b5e99e44649532d5
Jeff King <peff@peff.net>
2012-05-24 19:27:24 -0400

    ident: let callers omit name with fmt_indent
    Most callers want to see all of "$name <$email> $date", but
    a few want only limited parts, omitting the date, or even
    the name. We already have IDENT_NO_DATE to handle the date
    part, but there's not a good option for getting just the
    email. Callers have to done one of:
    
      1. Call ident_default_email; this does not respect
         environment variables, nor does it promise to trim
         whitespace or other crud from the result.
    
      2. Call git_{committer,author}_info; this returns the name
         and email, leaving the caller to parse out the wanted
         bits.
    
    This patch adds IDENT_NO_NAME; it stops short of adding
    IDENT_NO_EMAIL, as no callers want it (nor are likely to),
    and it complicates the error handling of the function.
    
    When no name is requested, the angle brackets (<>) around
    the email address are also omitted.
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    cregit: c15e1987aec562107bd8e4a9fdeebf6d27d0e53a http://github.com/git/git/commit/c15e1987aec562107bd8e4a9fdeebf6d27d0e53a

--- cache.h
+++ cache.h
@@ -5768,6 +5768,13 @@ directive|define
 name|IDENT_NO_DATE
 value|2
 end_define
+begin_define
+DECL|macro|IDENT_NO_NAME
+define|#
+directive|define
+name|IDENT_NO_NAME
+value|4
+end_define
 begin_function_decl
 specifier|extern
 specifier|const
