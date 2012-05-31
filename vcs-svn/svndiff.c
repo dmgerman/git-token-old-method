0a15df615267181f59b7e462d0021a89065d6917
David Barr <davidbarr@google.com>
2012-06-01 00:41:25 +1000

    vcs-svn: avoid self-assignment in dummy initialization of pre_off
    Without this change, clang complains:
    
     vcs-svn/svndiff.c:298:3: warning: Assigned value is garbage or undefined
                     off_t pre_off = pre_off; /* stupid GCC... */
                     ^               ~~~~~~~
    
    This code uses an old and common idiom for suppressing an
    "uninitialized variable" warning, and clang is wrong to warn about it.
    The idiom tells the compiler to leave the variable uninitialized,
    which saves a few bytes of code size, and, more importantly, allows
    valgrind to check at runtime that the variable is properly initialized
    by the time it is used.
    
    But MSVC and clang do not know that idiom, so let's avoid it in
    vcs-svn/ code.
    
    Initialize pre_off to -1, a recognizably meaningless value, to allow
    future code changes that cause pre_off to be used before it is
    initialized to be caught early.
    
    Signed-off-by: David Barr <davidbarr@google.com>
    Signed-off-by: Jonathan Nieder <jrnieder@gmail.com>
    
    cregit: 4a5de8dd79879d737e172a5d7e4196c6bf99e57d http://github.com/git/git/commit/4a5de8dd79879d737e172a5d7e4196c6bf99e57d

--- vcs-svn/svndiff.c
+++ vcs-svn/svndiff.c
@@ -1568,9 +1568,9 @@ comment|/* For each window: */
 name|off_t
 name|pre_off
 init|=
-name|pre_off
+operator|-
+literal|1
 decl_stmt|;
-comment|/* stupid GCC... */
 name|size_t
 name|pre_len
 decl_stmt|;
