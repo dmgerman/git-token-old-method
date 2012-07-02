3cb6096b619fc0e8d901ca98bcca221f76da3c60
Junio C Hamano <gitster@pobox.com>
2012-07-02 10:00:40 -0700

    sha1_name.c: many short names can only be committish
    We know that the token "$name" that appear in "$name^{commit}",
    "$name^4", "$name~4" etc. can only name a committish (either a
    commit or a tag that peels to a commit).  Teach get_short_sha1() to
    take advantage of that knowledge when disambiguating an abbreviated
    SHA-1 given as an object name.
    
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    cregit: e2643617d75e53e5a69278f8d7382553d1c14cf0 http://github.com/git/git/commit/e2643617d75e53e5a69278f8d7382553d1c14cf0

--- cache.h
+++ cache.h
@@ -5119,6 +5119,13 @@ directive|define
 name|GET_SHA1_COMMIT
 value|02
 end_define
+begin_define
+DECL|macro|GET_SHA1_COMMITTISH
+define|#
+directive|define
+name|GET_SHA1_COMMITTISH
+value|04
+end_define
 begin_function_decl
 specifier|extern
 name|int
