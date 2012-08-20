c6bcb0d9d29808b9b6179909047ea8c40e7ca01e
Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
2012-08-20 19:31:54 +0700

    i18n: blame: mark parseopt strings for translation
    Signed-off-by: Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    cregit: efd2a8bd38acc2b764b7eaad97693297feb87781 http://github.com/git/git/commit/efd2a8bd38acc2b764b7eaad97693297feb87781

--- builtin/blame.c
+++ builtin/blame.c
@@ -94,7 +94,10 @@ name|char
 name|blame_usage
 index|[]
 init|=
+name|N_
+argument_list|(
 literal|"git blame [options] [rev-opts] [rev] [--] file"
+argument_list|)
 decl_stmt|;
 end_decl_stmt
 begin_decl_stmt
@@ -111,7 +114,10 @@ name|blame_usage
 block|,
 literal|""
 block|,
+name|N_
+argument_list|(
 literal|"[rev-opts] are documented in git-rev-list(1)"
+argument_list|)
 block|,
 name|NULL
 block|}
@@ -11793,7 +11799,10 @@ argument_list|,
 operator|&
 name|incremental
 argument_list|,
+name|N_
+argument_list|(
 literal|"Show blame entries as we find them, incrementally"
+argument_list|)
 argument_list|)
 block|,
 name|OPT_BOOLEAN
@@ -11805,7 +11814,10 @@ argument_list|,
 operator|&
 name|blank_boundary
 argument_list|,
+name|N_
+argument_list|(
 literal|"Show blank SHA-1 for boundary commits (Default: off)"
+argument_list|)
 argument_list|)
 block|,
 name|OPT_BOOLEAN
@@ -11817,7 +11829,10 @@ argument_list|,
 operator|&
 name|show_root
 argument_list|,
+name|N_
+argument_list|(
 literal|"Do not treat root commits as boundaries (Default: off)"
+argument_list|)
 argument_list|)
 block|,
 name|OPT_BOOLEAN
@@ -11829,7 +11844,10 @@ argument_list|,
 operator|&
 name|show_stats
 argument_list|,
+name|N_
+argument_list|(
 literal|"Show work cost statistics"
+argument_list|)
 argument_list|)
 block|,
 name|OPT_BIT
@@ -11841,7 +11859,10 @@ argument_list|,
 operator|&
 name|output_option
 argument_list|,
+name|N_
+argument_list|(
 literal|"Show output score for blame entries"
+argument_list|)
 argument_list|,
 name|OUTPUT_SHOW_SCORE
 argument_list|)
@@ -11855,7 +11876,10 @@ argument_list|,
 operator|&
 name|output_option
 argument_list|,
+name|N_
+argument_list|(
 literal|"Show original filename (Default: auto)"
+argument_list|)
 argument_list|,
 name|OUTPUT_SHOW_NAME
 argument_list|)
@@ -11869,7 +11893,10 @@ argument_list|,
 operator|&
 name|output_option
 argument_list|,
+name|N_
+argument_list|(
 literal|"Show original linenumber (Default: off)"
+argument_list|)
 argument_list|,
 name|OUTPUT_SHOW_NUMBER
 argument_list|)
@@ -11883,7 +11910,10 @@ argument_list|,
 operator|&
 name|output_option
 argument_list|,
+name|N_
+argument_list|(
 literal|"Show in a format designed for machine consumption"
+argument_list|)
 argument_list|,
 name|OUTPUT_PORCELAIN
 argument_list|)
@@ -11897,7 +11927,10 @@ argument_list|,
 operator|&
 name|output_option
 argument_list|,
+name|N_
+argument_list|(
 literal|"Show porcelain format with per-line commit information"
+argument_list|)
 argument_list|,
 name|OUTPUT_PORCELAIN
 operator||
@@ -11913,7 +11946,10 @@ argument_list|,
 operator|&
 name|output_option
 argument_list|,
+name|N_
+argument_list|(
 literal|"Use the same output mode as git-annotate (Default: off)"
+argument_list|)
 argument_list|,
 name|OUTPUT_ANNOTATE_COMPAT
 argument_list|)
@@ -11927,7 +11963,10 @@ argument_list|,
 operator|&
 name|output_option
 argument_list|,
+name|N_
+argument_list|(
 literal|"Show raw timestamp (Default: off)"
+argument_list|)
 argument_list|,
 name|OUTPUT_RAW_TIMESTAMP
 argument_list|)
@@ -11941,7 +11980,10 @@ argument_list|,
 operator|&
 name|output_option
 argument_list|,
+name|N_
+argument_list|(
 literal|"Show long commit SHA1 (Default: off)"
+argument_list|)
 argument_list|,
 name|OUTPUT_LONG_OBJECT_NAME
 argument_list|)
@@ -11955,7 +11997,10 @@ argument_list|,
 operator|&
 name|output_option
 argument_list|,
+name|N_
+argument_list|(
 literal|"Suppress author name and timestamp (Default: off)"
+argument_list|)
 argument_list|,
 name|OUTPUT_NO_AUTHOR
 argument_list|)
@@ -11969,7 +12014,10 @@ argument_list|,
 operator|&
 name|output_option
 argument_list|,
+name|N_
+argument_list|(
 literal|"Show author email instead of name (Default: off)"
+argument_list|)
 argument_list|,
 name|OUTPUT_SHOW_EMAIL
 argument_list|)
@@ -11983,7 +12031,10 @@ argument_list|,
 operator|&
 name|xdl_opts
 argument_list|,
+name|N_
+argument_list|(
 literal|"Ignore whitespace differences"
+argument_list|)
 argument_list|,
 name|XDF_IGNORE_WHITESPACE
 argument_list|)
@@ -11997,7 +12048,10 @@ argument_list|,
 operator|&
 name|xdl_opts
 argument_list|,
+name|N_
+argument_list|(
 literal|"Spend extra cycles to find better match"
+argument_list|)
 argument_list|,
 name|XDF_NEED_MINIMAL
 argument_list|)
@@ -12011,9 +12065,15 @@ argument_list|,
 operator|&
 name|revs_file
 argument_list|,
+name|N_
+argument_list|(
 literal|"file"
+argument_list|)
 argument_list|,
+name|N_
+argument_list|(
 literal|"Use revisions from<file> instead of calling git-rev-list"
+argument_list|)
 argument_list|)
 block|,
 name|OPT_STRING
@@ -12025,9 +12085,15 @@ argument_list|,
 operator|&
 name|contents_from
 argument_list|,
+name|N_
+argument_list|(
 literal|"file"
+argument_list|)
 argument_list|,
+name|N_
+argument_list|(
 literal|"Use<file>'s contents as the final image"
+argument_list|)
 argument_list|)
 block|,
 block|{
@@ -12040,9 +12106,15 @@ block|,
 operator|&
 name|opt
 block|,
+name|N_
+argument_list|(
 literal|"score"
+argument_list|)
 block|,
+name|N_
+argument_list|(
 literal|"Find line copies within and across files"
+argument_list|)
 block|,
 name|PARSE_OPT_OPTARG
 block|,
@@ -12059,9 +12131,15 @@ block|,
 operator|&
 name|opt
 block|,
+name|N_
+argument_list|(
 literal|"score"
+argument_list|)
 block|,
+name|N_
+argument_list|(
 literal|"Find line movements within and across files"
+argument_list|)
 block|,
 name|PARSE_OPT_OPTARG
 block|,
@@ -12077,9 +12155,15 @@ argument_list|,
 operator|&
 name|bottomtop
 argument_list|,
+name|N_
+argument_list|(
 literal|"n,m"
+argument_list|)
 argument_list|,
+name|N_
+argument_list|(
 literal|"Process only line range n,m, counting from 1"
+argument_list|)
 argument_list|,
 name|blame_bottomtop_callback
 argument_list|)
