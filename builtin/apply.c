542dabc7424c424e79277fa0acbcd1fb6281855c
Junio C Hamano <gitster@pobox.com>
2012-08-24 22:48:55 -0700

    apply: compute patch->def_name correctly under -p0
    Back when "git apply" was written, we made sure that the user can
    skip more than the default number of path components (i.e. 1) by
    giving "-p<n>", but the logic for doing so was built around the
    notion of "we skip N slashes and stop".  This obviously does not
    work well when running under -p0 where we do not want to skip any,
    but still want to skip SP/HT that separates the pathnames of
    preimage and postimage and want to reject absolute pathnames.
    
    Stop using "stop_at_slash()", and instead introduce a new helper
    "skip_tree_prefix()" with similar logic but works correctly even for
    the -p0 case.
    
    This is an ancient bug, but has been masked for a long time because
    most of the patches are text and have other clues to tell us the
    name of the preimage and the postimage.
    
    Noticed by Colin McCabe.
    
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    cregit: 6a2abdc12516cd3801bceef4ccfba399c962a074 http://github.com/git/git/commit/6a2abdc12516cd3801bceef4ccfba399c962a074

--- builtin/apply.c
+++ builtin/apply.c
@@ -5261,13 +5261,16 @@ literal|1
 return|;
 block|}
 end_function
+begin_comment
+comment|/*  * Skip p_value leading components from "line"; as we do not accept  * absolute paths, return NULL in that case.  */
+end_comment
 begin_function
-DECL|function|stop_at_slash
+DECL|function|skip_tree_prefix
 specifier|static
 specifier|const
 name|char
 modifier|*
-name|stop_at_slash
+name|skip_tree_prefix
 parameter_list|(
 specifier|const
 name|char
@@ -5280,12 +5283,35 @@ parameter_list|)
 block|{
 name|int
 name|nslash
-init|=
-name|p_value
 decl_stmt|;
 name|int
 name|i
 decl_stmt|;
+if|if
+condition|(
+operator|!
+name|p_value
+condition|)
+return|return
+operator|(
+name|llen
+operator|&&
+name|line
+index|[
+literal|0
+index|]
+operator|==
+literal|'/'
+operator|)
+condition|?
+name|NULL
+else|:
+name|line
+return|;
+name|nslash
+operator|=
+name|p_value
+expr_stmt|;
 for|for
 control|(
 name|i
@@ -5320,10 +5346,20 @@ operator|<=
 literal|0
 condition|)
 return|return
+operator|(
+name|i
+operator|==
+literal|0
+operator|)
+condition|?
+name|NULL
+else|:
 operator|&
 name|line
 index|[
 name|i
+operator|+
+literal|1
 index|]
 return|;
 block|}
@@ -5422,10 +5458,10 @@ condition|)
 goto|goto
 name|free_and_fail1
 goto|;
-comment|/* advance to the first slash */
+comment|/* strip the a/b prefix including trailing slash */
 name|cp
 operator|=
-name|stop_at_slash
+name|skip_tree_prefix
 argument_list|(
 name|first
 operator|.
@@ -5436,17 +5472,10 @@ operator|.
 name|len
 argument_list|)
 expr_stmt|;
-comment|/* we do not accept absolute paths */
 if|if
 condition|(
 operator|!
 name|cp
-operator|||
-name|cp
-operator|==
-name|first
-operator|.
-name|buf
 condition|)
 goto|goto
 name|free_and_fail1
@@ -5459,8 +5488,6 @@ argument_list|,
 literal|0
 argument_list|,
 name|cp
-operator|+
-literal|1
 operator|-
 name|first
 operator|.
@@ -5523,7 +5550,7 @@ name|free_and_fail1
 goto|;
 name|cp
 operator|=
-name|stop_at_slash
+name|skip_tree_prefix
 argument_list|(
 name|sp
 operator|.
@@ -5538,12 +5565,6 @@ if|if
 condition|(
 operator|!
 name|cp
-operator|||
-name|cp
-operator|==
-name|sp
-operator|.
-name|buf
 condition|)
 goto|goto
 name|free_and_fail1
@@ -5554,8 +5575,6 @@ condition|(
 name|strcmp
 argument_list|(
 name|cp
-operator|+
-literal|1
 argument_list|,
 name|first
 operator|.
@@ -5584,7 +5603,7 @@ block|}
 comment|/* unquoted second */
 name|cp
 operator|=
-name|stop_at_slash
+name|skip_tree_prefix
 argument_list|(
 name|second
 argument_list|,
@@ -5599,17 +5618,10 @@ if|if
 condition|(
 operator|!
 name|cp
-operator|||
-name|cp
-operator|==
-name|second
 condition|)
 goto|goto
 name|free_and_fail1
 goto|;
-name|cp
-operator|++
-expr_stmt|;
 if|if
 condition|(
 name|line
@@ -5621,8 +5633,6 @@ operator|!=
 name|first
 operator|.
 name|len
-operator|+
-literal|1
 operator|||
 name|memcmp
 argument_list|(
@@ -5670,7 +5680,7 @@ block|}
 comment|/* unquoted first name */
 name|name
 operator|=
-name|stop_at_slash
+name|skip_tree_prefix
 argument_list|(
 name|line
 argument_list|,
@@ -5681,17 +5691,10 @@ if|if
 condition|(
 operator|!
 name|name
-operator|||
-name|name
-operator|==
-name|line
 condition|)
 return|return
 name|NULL
 return|;
-name|name
-operator|++
-expr_stmt|;
 comment|/* 	 * since the first name is unquoted, a dq if exists must be 	 * the beginning of the second name. 	 */
 for|for
 control|(
@@ -5745,7 +5748,7 @@ name|free_and_fail2
 goto|;
 name|np
 operator|=
-name|stop_at_slash
+name|skip_tree_prefix
 argument_list|(
 name|sp
 operator|.
@@ -5760,19 +5763,10 @@ if|if
 condition|(
 operator|!
 name|np
-operator|||
-name|np
-operator|==
-name|sp
-operator|.
-name|buf
 condition|)
 goto|goto
 name|free_and_fail2
 goto|;
-name|np
-operator|++
-expr_stmt|;
 name|len
 operator|=
 name|sp
@@ -5907,17 +5901,38 @@ case|:
 case|case
 literal|' '
 case|:
+comment|/* 			 * Is this the separator between the preimage 			 * and the postimage pathname?  Again, we are 			 * only interested in the case where there is 			 * no rename, as this is only to set def_name 			 * and a rename patch has the names elsewhere 			 * in an unambiguous form. 			 */
+if|if
+condition|(
+operator|!
+name|name
+index|[
+name|len
+operator|+
+literal|1
+index|]
+condition|)
+return|return
+name|NULL
+return|;
+comment|/* no postimage name */
 name|second
 operator|=
-name|stop_at_slash
+name|skip_tree_prefix
 argument_list|(
 name|name
 operator|+
 name|len
+operator|+
+literal|1
 argument_list|,
 name|line_len
 operator|-
+operator|(
 name|len
+operator|+
+literal|1
+operator|)
 argument_list|)
 expr_stmt|;
 if|if
@@ -5928,9 +5943,7 @@ condition|)
 return|return
 name|NULL
 return|;
-name|second
-operator|++
-expr_stmt|;
+comment|/* 			 * Does len bytes starting at "name" and "second" 			 * (that are separated by one HT or SP we just 			 * found) exactly match? 			 */
 if|if
 condition|(
 name|second
@@ -5950,7 +5963,6 @@ argument_list|,
 name|len
 argument_list|)
 condition|)
-block|{
 return|return
 name|xmemdupz
 argument_list|(
@@ -5959,7 +5971,6 @@ argument_list|,
 name|len
 argument_list|)
 return|;
-block|}
 block|}
 block|}
 block|}
