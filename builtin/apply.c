ac093d6bd25ba381b2adb8d5c1f93d9595a7cf07
Junio C Hamano <gitster@pobox.com>
2012-10-12 15:53:46 -0700

    apply.c:update_pre_post_images(): the preimage can be truncated
    5166714 (apply: Allow blank context lines to match beyond EOF,
    2010-03-06) and then later 0c3ef98 (apply: Allow blank *trailing*
    context lines to match beyond EOF, 2010-04-08) taught "git apply"
    to trim new blank lines at the end in the patch text when matching
    the contents being patched and the preimage recorded in the patch,
    under --whitespace=fix mode.
    
    When a preimage is modified to match the current contents in
    preparation for such a "fixed" patch application, the context lines
    in the postimage must be updated to match (otherwise, it would
    reintroduce whitespace breakages), and update_pre_post_images()
    function is responsible for doing this.  However, this function was
    not updated to take into account a case where the removal of
    trailing blank lines reduces the number of lines in the preimage,
    and triggered an assertion error.
    
    The logic to fix the postimage by copying the corrected context
    lines from the preimage was not prepared to handle this case,
    either, but it was protected by the assert() and only got exposed
    when the assertion is corrected.
    
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    cregit: 5de7166d46d25fed3b8a181c3842cf4350ecdea4 http://github.com/git/git/commit/5de7166d46d25fed3b8a181c3842cf4350ecdea4

--- builtin/apply.c
+++ builtin/apply.c
@@ -10291,6 +10291,8 @@ name|int
 name|i
 decl_stmt|,
 name|ctx
+decl_stmt|,
+name|reduced
 decl_stmt|;
 name|char
 modifier|*
@@ -10321,10 +10323,20 @@ argument_list|)
 expr_stmt|;
 name|assert
 argument_list|(
+name|postlen
+condition|?
 name|fixed_preimage
 operator|.
 name|nr
 operator|==
+name|preimage
+operator|->
+name|nr
+else|:
+name|fixed_preimage
+operator|.
+name|nr
+operator|<=
 name|preimage
 operator|->
 name|nr
@@ -10338,8 +10350,8 @@ literal|0
 init|;
 name|i
 operator|<
-name|preimage
-operator|->
+name|fixed_preimage
+operator|.
 name|nr
 condition|;
 name|i
@@ -10411,6 +10423,8 @@ expr_stmt|;
 for|for
 control|(
 name|i
+operator|=
+name|reduced
 operator|=
 name|ctx
 operator|=
@@ -10519,6 +10533,7 @@ name|ctx
 operator|++
 expr_stmt|;
 block|}
+comment|/* 		 * preimage is expected to run out, if the caller 		 * fixed addition of trailing blank lines. 		 */
 if|if
 condition|(
 name|preimage
@@ -10527,14 +10542,12 @@ name|nr
 operator|<=
 name|ctx
 condition|)
-name|die
-argument_list|(
-name|_
-argument_list|(
-literal|"oops"
-argument_list|)
-argument_list|)
+block|{
+name|reduced
+operator|++
 expr_stmt|;
+continue|continue;
+block|}
 comment|/* and copy it in, while fixing the line length */
 name|len
 operator|=
@@ -10589,6 +10602,12 @@ operator|-
 name|postimage
 operator|->
 name|buf
+expr_stmt|;
+name|postimage
+operator|->
+name|nr
+operator|-=
+name|reduced
 expr_stmt|;
 block|}
 end_function
