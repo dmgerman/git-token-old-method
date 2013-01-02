fa6fe8733d19c9bff3bef70aa33c90f1260a6dea
Antoine Pelisse <apelisse@gmail.com>
2013-01-02 19:42:50 +0100

    merge: Honor prepare-commit-msg return code
    65969d4 (merge: honor prepare-commit-msg hook, 2011-02-14) tried to
    make "git commit" and "git merge" consistent, because a merge that
    required user assistance has to be concluded with "git commit", but
    back then only "git commit" triggered prepare-commit-msg hook.
    
    When it added a call to run the prepare-commit-msg hook, however, it
    forgot to check the exit code from the hook like "git commit" does,
    and ended up replacing one inconsistency with another.
    
    When prepare-commit-msg hook that is run from "git merge" exits with
    a non-zero status, abort the commit.
    
    Signed-off-by: Antoine Pelisse <apelisse@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    cregit: 3e4141d08c8dc1964d53c2ce13de8876d29e6436 http://github.com/git/git/commit/3e4141d08c8dc1964d53c2ce13de8876d29e6436

--- builtin/merge.c
+++ builtin/merge.c
@@ -5723,6 +5723,8 @@ operator|&
 name|msg
 argument_list|)
 expr_stmt|;
+if|if
+condition|(
 name|run_hook
 argument_list|(
 name|get_index_file
@@ -5738,6 +5740,13 @@ argument_list|,
 literal|"merge"
 argument_list|,
 name|NULL
+argument_list|,
+name|NULL
+argument_list|)
+condition|)
+name|abort_commit
+argument_list|(
+name|remoteheads
 argument_list|,
 name|NULL
 argument_list|)
