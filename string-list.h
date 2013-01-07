0416480f56536908e4f41533f7310d1f94e82d55
Junio C Hamano <gitster@pobox.com>
2013-01-07 12:24:55 -0800

    string-list: allow case-insensitive string list
    Some string list needs to be searched case insensitively, and for
    that to work correctly, the string needs to be sorted case
    insensitively from the beginning.
    
    Allow a custom comparison function to be defined on a string list
    instance and use it throughout in place of strcmp().
    
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    cregit: 8dd5afc926acb9829ebf56e9b78826a5242cd638 http://github.com/git/git/commit/8dd5afc926acb9829ebf56e9b78826a5242cd638

--- string-list.h
+++ string-list.h
@@ -28,6 +28,25 @@ decl_stmt|;
 block|}
 struct|;
 end_struct
+begin_typedef
+DECL|typedef|compare_strings_fn
+typedef|typedef
+name|int
+function_decl|(
+modifier|*
+name|compare_strings_fn
+function_decl|)
+parameter_list|(
+specifier|const
+name|char
+modifier|*
+parameter_list|,
+specifier|const
+name|char
+modifier|*
+parameter_list|)
+function_decl|;
+end_typedef
 begin_struct
 DECL|struct|string_list
 struct|struct
@@ -54,6 +73,11 @@ name|strdup_strings
 range|:
 literal|1
 decl_stmt|;
+DECL|member|cmp
+name|compare_strings_fn
+name|cmp
+decl_stmt|;
+comment|/* NULL uses strcmp() */
 block|}
 struct|;
 end_struct
