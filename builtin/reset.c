1af617c12ea8f2a332eb55c09c8c3338635bada7
Martin von Zweigbergk <martinvonz@gmail.com>
2013-01-14 21:47:38 -0800

    reset.c: remove unnecessary variable 'i'
    Throughout most of parse_args(), the variable 'i' remains at 0. Many
    references are still made to the variable even when it could only have
    the value 0. This made at least me, who has relatively little
    experience with C programming styles, think that parts of the function
    was meant to be part of a loop. To avoid such confusion, remove the
    variable and also the 'argc' parameter and check for NULL trailing
    argv instead.
    
    Signed-off-by: Martin von Zweigbergk <martinvonz@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    cregit: dca48cf520933c028f807f65175c29482a209f23 http://github.com/git/git/commit/dca48cf520933c028f807f65175c29482a209f23

--- builtin/reset.c
+++ builtin/reset.c
@@ -1110,9 +1110,6 @@ modifier|*
 modifier|*
 name|parse_args
 parameter_list|(
-name|int
-name|argc
-parameter_list|,
 specifier|const
 name|char
 modifier|*
@@ -1131,11 +1128,6 @@ modifier|*
 name|rev_ret
 parameter_list|)
 block|{
-name|int
-name|i
-init|=
-literal|0
-decl_stmt|;
 specifier|const
 name|char
 modifier|*
@@ -1150,12 +1142,13 @@ index|[
 literal|20
 index|]
 decl_stmt|;
-comment|/* 	 * Possible arguments are: 	 * 	 * git reset [-opts]<rev><paths>... 	 * git reset [-opts]<rev> --<paths>... 	 * git reset [-opts] --<paths>... 	 * git reset [-opts]<paths>... 	 * 	 * At this point, argv[i] points immediately after [-opts]. 	 */
+comment|/* 	 * Possible arguments are: 	 * 	 * git reset [-opts]<rev><paths>... 	 * git reset [-opts]<rev> --<paths>... 	 * git reset [-opts] --<paths>... 	 * git reset [-opts]<paths>... 	 * 	 * At this point, argv points immediately after [-opts]. 	 */
 if|if
 condition|(
-name|i
-operator|<
-name|argc
+name|argv
+index|[
+literal|0
+index|]
 condition|)
 block|{
 if|if
@@ -1165,14 +1158,14 @@ name|strcmp
 argument_list|(
 name|argv
 index|[
-name|i
+literal|0
 index|]
 argument_list|,
 literal|"--"
 argument_list|)
 condition|)
 block|{
-name|i
+name|argv
 operator|++
 expr_stmt|;
 comment|/* reset to HEAD, possibly with paths */
@@ -1180,19 +1173,16 @@ block|}
 elseif|else
 if|if
 condition|(
-name|i
-operator|+
+name|argv
+index|[
 literal|1
-operator|<
-name|argc
+index|]
 operator|&&
 operator|!
 name|strcmp
 argument_list|(
 name|argv
 index|[
-name|i
-operator|+
 literal|1
 index|]
 argument_list|,
@@ -1204,15 +1194,15 @@ name|rev
 operator|=
 name|argv
 index|[
-name|i
+literal|0
 index|]
 expr_stmt|;
-name|i
+name|argv
 operator|+=
 literal|2
 expr_stmt|;
 block|}
-comment|/* 		 * Otherwise, argv[i] could be either<rev> or<paths> and 		 * has to be unambiguous. 		 */
+comment|/* 		 * Otherwise, argv[0] could be either<rev> or<paths> and 		 * has to be unambiguous. 		 */
 elseif|else
 if|if
 condition|(
@@ -1221,31 +1211,29 @@ name|get_sha1_committish
 argument_list|(
 name|argv
 index|[
-name|i
+literal|0
 index|]
 argument_list|,
 name|unused
 argument_list|)
 condition|)
 block|{
-comment|/* 			 * Ok, argv[i] looks like a rev; it should not 			 * be a filename. 			 */
+comment|/* 			 * Ok, argv[0] looks like a rev; it should not 			 * be a filename. 			 */
 name|verify_non_filename
 argument_list|(
 name|prefix
 argument_list|,
 name|argv
 index|[
-name|i
+literal|0
 index|]
 argument_list|)
 expr_stmt|;
 name|rev
 operator|=
+operator|*
 name|argv
-index|[
-name|i
 operator|++
-index|]
 expr_stmt|;
 block|}
 else|else
@@ -1257,7 +1245,7 @@ name|prefix
 argument_list|,
 name|argv
 index|[
-name|i
+literal|0
 index|]
 argument_list|,
 literal|1
@@ -1271,17 +1259,16 @@ operator|=
 name|rev
 expr_stmt|;
 return|return
-name|i
-operator|<
-name|argc
+name|argv
+index|[
+literal|0
+index|]
 condition|?
 name|get_pathspec
 argument_list|(
 name|prefix
 argument_list|,
 name|argv
-operator|+
-name|i
 argument_list|)
 else|:
 name|NULL
@@ -1527,8 +1514,6 @@ name|pathspec
 operator|=
 name|parse_args
 argument_list|(
-name|argc
-argument_list|,
 name|argv
 argument_list|,
 name|prefix
