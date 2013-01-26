a84a82a4786c90f5820f87ea8a00930d338b6fb1
Jeff King <peff@peff.net>
2013-01-26 17:40:38 -0500

    fetch: run gc --auto after fetching
    We generally try to run "gc --auto" after any commands that
    might introduce a large number of new objects. An obvious
    place to do so is after running "fetch", which may introduce
    new loose objects or packs (depending on the size of the
    fetch).
    
    While an active developer repository will probably
    eventually trigger a "gc --auto" on another action (e.g.,
    git-rebase), there are two good reasons why it is nicer to
    do it at fetch time:
    
      1. Read-only repositories which track an upstream (e.g., a
         continuous integration server which fetches and builds,
         but never makes new commits) will accrue loose objects
         and small packs, but never coalesce them into a more
         efficient larger pack.
    
      2. Fetching is often already perceived to be slow to the
         user, since they have to wait on the network. It's much
         more pleasant to include a potentially slow auto-gc as
         part of the already-long network fetch than in the
         middle of productive work with git-rebase or similar.
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    cregit: 131b8fcbfbea0acee8d78a8f9b2b3fee4285aee5 http://github.com/git/git/commit/131b8fcbfbea0acee8d78a8f9b2b3fee4285aee5

--- builtin/fetch.c
+++ builtin/fetch.c
@@ -5318,6 +5318,21 @@ name|result
 init|=
 literal|0
 decl_stmt|;
+specifier|static
+specifier|const
+name|char
+modifier|*
+name|argv_gc_auto
+index|[]
+init|=
+block|{
+literal|"gc"
+block|,
+literal|"--auto"
+block|,
+name|NULL
+block|, 	}
+decl_stmt|;
 name|packet_trace_identity
 argument_list|(
 literal|"fetch"
@@ -5692,6 +5707,13 @@ operator|&
 name|list
 argument_list|,
 literal|0
+argument_list|)
+expr_stmt|;
+name|run_command_v_opt
+argument_list|(
+name|argv_gc_auto
+argument_list|,
+name|RUN_GIT_CMD
 argument_list|)
 expr_stmt|;
 return|return
