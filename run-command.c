4e8eb5da22204ace69b12f9fb5748d550a191ef9
Stephen Boyd <sboyd@codeaurora.org>
2013-01-30 18:01:05 -0800

    run-command: be more informative about what failed
    While debugging an error with verify_signed_buffer() the error
    messages from run-command weren't very useful:
    
     error: cannot create pipe for gpg: Too many open files
     error: could not run gpg.
    
    because they didn't indicate *which* pipe couldn't be created.
    
    Print which pipe failed to be created in the error message so we
    can more easily debug similar problems in the future.
    
    For example, the above error now prints:
    
     error: cannot create standard error pipe for gpg: Too many open files
     error: could not run gpg.
    
    Signed-off-by: Stephen Boyd <sboyd@codeaurora.org>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    cregit: 939296c4a42b0eb22542f0fe2e50b53993489eff http://github.com/git/git/commit/939296c4a42b0eb22542f0fe2e50b53993489eff

--- run-command.c
+++ run-command.c
@@ -1297,6 +1297,10 @@ name|failed_errno
 init|=
 name|failed_errno
 decl_stmt|;
+name|char
+modifier|*
+name|str
+decl_stmt|;
 comment|/* 	 * In case of errors we must keep the promise to close FDs 	 * that have been passed in via ->in and ->out. 	 */
 name|need_in
 operator|=
@@ -1344,6 +1348,10 @@ name|cmd
 operator|->
 name|out
 argument_list|)
+expr_stmt|;
+name|str
+operator|=
+literal|"standard input"
 expr_stmt|;
 goto|goto
 name|fail_pipe
@@ -1418,6 +1426,10 @@ name|cmd
 operator|->
 name|in
 argument_list|)
+expr_stmt|;
+name|str
+operator|=
+literal|"standard output"
 expr_stmt|;
 goto|goto
 name|fail_pipe
@@ -1511,11 +1523,17 @@ operator|->
 name|out
 argument_list|)
 expr_stmt|;
+name|str
+operator|=
+literal|"standard error"
+expr_stmt|;
 name|fail_pipe
 label|:
 name|error
 argument_list|(
-literal|"cannot create pipe for %s: %s"
+literal|"cannot create %s pipe for %s: %s"
+argument_list|,
+name|str
 argument_list|,
 name|cmd
 operator|->
