8878ddc7b23528cd0f667cafb55873b123cb17b2
David Aguilar <davvid@gmail.com>
2013-02-23 16:50:17 -0800

    contrib/credential: use a lowercase "usage:" string
    Make the usage string consistent with Git.
    
    Reviewed-by: Jonathan Nieder <jrnieder@gmail.com>
    Signed-off-by: David Aguilar <davvid@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    cregit: c358ed756e6a6c6f762b453dd3698fa06bbb9664 http://github.com/git/git/commit/c358ed756e6a6c6f762b453dd3698fa06bbb9664

--- contrib/credential/wincred/git-credential-wincred.c
+++ contrib/credential/wincred/git-credential-wincred.c
@@ -1797,7 +1797,7 @@ name|char
 modifier|*
 name|usage
 init|=
-literal|"Usage: git credential-wincred<get|store|erase>\n"
+literal|"usage: git credential-wincred<get|store|erase>\n"
 decl_stmt|;
 if|if
 condition|(
