fe06539388f6c02ee75ceb8d576d4e026dc0f73f
Karsten Blees <karsten.blees@gmail.com>
2013-02-28 00:57:48 +0100

    name-hash.c: fix endless loop with core.ignorecase=true
    With core.ignorecase=true, name-hash.c builds a case insensitive index of
    all tracked directories. Currently, the existing cache entry structures are
    added multiple times to the same hashtable (with different name lengths and
    hash codes). However, there's only one dir_next pointer, which gets
    completely messed up in case of hash collisions. In the worst case, this
    causes an endless loop if ce == ce->dir_next (see t7062).
    
    Use a separate hashtable and separate structures for the directory index
    so that each directory entry has its own next pointer. Use reference
    counting to track which directory entry contains files.
    
    There are only slight changes to the name-hash.c API:
    - new free_name_hash() used by read_cache.c::discard_index()
    - remove_name_hash() takes an additional index_state parameter
    - index_name_exists() for a directory (trailing '/') may return a cache
      entry that has been removed (CE_UNHASHED). This is not a problem as the
      return value is only used to check if the directory exists (dir.c) or to
      normalize casing of directory names (read-cache.c).
    
    Getting rid of cache_entry.dir_next reduces memory consumption, especially
    with core.ignorecase=false (which doesn't use that member at all).
    
    With core.ignorecase=true, building the directory index is slightly faster
    as we add / check the parent directory first (instead of going through all
    directory levels for each file in the index). E.g. with WebKit (~200k
    files, ~7k dirs), time spent in lazy_init_name_hash is reduced from 176ms
    to 130ms.
    
    Signed-off-by: Karsten Blees <blees@dcon.de>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    cregit: 2092678cd5265e40dfb2b8e3adc8f2075faeece4 http://github.com/git/git/commit/2092678cd5265e40dfb2b8e3adc8f2075faeece4

--- read-cache.c
+++ read-cache.c
@@ -199,6 +199,8 @@ index|]
 decl_stmt|;
 name|remove_name_hash
 argument_list|(
+name|istate
+argument_list|,
 name|old
 argument_list|)
 expr_stmt|;
@@ -2168,6 +2170,8 @@ argument_list|)
 expr_stmt|;
 name|remove_name_hash
 argument_list|(
+name|istate
+argument_list|,
 name|ce
 argument_list|)
 expr_stmt|;
@@ -2291,6 +2295,8 @@ name|CE_REMOVE
 condition|)
 name|remove_name_hash
 argument_list|(
+name|istate
+argument_list|,
 name|ce_array
 index|[
 name|i
@@ -7428,18 +7434,9 @@ name|nsec
 operator|=
 literal|0
 expr_stmt|;
-name|istate
-operator|->
-name|name_hash_initialized
-operator|=
-literal|0
-expr_stmt|;
-name|free_hash
+name|free_name_hash
 argument_list|(
-operator|&
 name|istate
-operator|->
-name|name_hash
 argument_list|)
 expr_stmt|;
 name|cache_tree_free
