fc4e4158d8a764f0a3983951add583041809fa79
Philip Oakley <philipoakley@iee.org>
2013-04-02 23:39:49 +0100

    builtin/help.c: split "-a" processing into two
    "help -a" (help all) gives the list of available commands and then
    further gives hints on the use of "git help".   Separate these into
    two steps, because we will add "help -g" (help guides) that want to
    also show the overall hints after it is done.
    
    While at it, change the definition of the "-a" option to use OPT_BOOL,
    not the deprecated OPT_BOOLEAN.  We do not behave differently when
    the user gives the "-a" option multiple times, e.g. "git help -a -a".
    
    Signed-off-by: Philip Oakley <philipoakley@iee.org>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    cregit: 15f7d4943807a50e01d2fd85bcdbd44361808dd2 http://github.com/git/git/commit/15f7d4943807a50e01d2fd85bcdbd44361808dd2

--- builtin/help.c
+++ builtin/help.c
@@ -173,7 +173,7 @@ name|builtin_help_options
 index|[]
 init|=
 block|{
-name|OPT_BOOLEAN
+name|OPT_BOOL
 argument_list|(
 literal|'a'
 argument_list|,
@@ -2392,6 +2392,12 @@ operator|&
 name|other_cmds
 argument_list|)
 expr_stmt|;
+block|}
+if|if
+condition|(
+name|show_all
+condition|)
+block|{
 name|printf
 argument_list|(
 literal|"%s\n"
@@ -2402,6 +2408,7 @@ name|git_more_info_string
 argument_list|)
 argument_list|)
 expr_stmt|;
+comment|/* 		* We're done. Ignore any remaining args 		*/
 return|return
 literal|0
 return|;
