b0e015f3498fa434e0a5c2800cf8c563f756c6e3
Felipe Contreras <felipe.contreras@gmail.com>
2013-04-10 17:15:52 -0400

    transport-helper: report errors properly
    If a push fails because the remote-helper died (with fast-export),
    the user may not see any error message. We do correctly die with a
    failed exit code, as we notice that the helper has died while
    reading back the ref status from the helper. However, we don't print
    any message. This is OK if the helper itself printed a useful error
    message, but we cannot count on that; let's let the user know that
    the helper failed.
    
    In the long run, it may make more sense to propagate the error back
    up to push, so that it can present the usual status table and give a
    nicer message. But this is a much simpler fix that can help
    immediately.
    
    While we're adding tests, let's also confirm that the remote-helper
    dying is also detected when importing refs. We currently do so
    robustly when the helper uses the "done" feature (and that is what
    we test).  We cannot do so reliably when the helper does not use the
    "done" feature, but it is not even worth testing; the right solution
    is for the helper to start using "done".
    
    Suggested-by: Jeff King <peff@peff.net>
    Signed-off-by: Felipe Contreras <felipe.contreras@gmail.com>
    Signed-off-by: Jeff King <peff@peff.net>
    Acked-by: Sverre Rabbelier <srabbelier@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    cregit: 81d340d40af506eda3182190b6132575547fa4c5 http://github.com/git/git/commit/81d340d40af506eda3182190b6132575547fa4c5

--- transport-helper.c
+++ transport-helper.c
@@ -283,9 +283,9 @@ argument_list|,
 literal|"Debug: Remote helper quit.\n"
 argument_list|)
 expr_stmt|;
-name|exit
+name|die
 argument_list|(
-literal|128
+literal|"Reading from remote helper failed"
 argument_list|)
 expr_stmt|;
 block|}
