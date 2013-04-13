6cf5ae77b182025b86c604060eb4fff989ee49e6
Lukas Fleischer <git@cryptocrack.de>
2013-04-13 15:28:30 +0200

    attr.c: extract read_index_data() as read_blob_data_from_index()
    Extract the read_index_data() function from attr.c and move it to
    read-cache.c; rename it to read_blob_data_from_index() and update
    the function signature of it to align better with index/cache API
    functions.
    
    This allows for reusing the function in convert.c later.
    
    Signed-off-by: Lukas Fleischer <git@cryptocrack.de>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    cregit: 29fb37b272debaf4f5f6eb7cf476de9274492930 http://github.com/git/git/commit/29fb37b272debaf4f5f6eb7cf476de9274492930

--- cache.h
+++ cache.h
@@ -1536,6 +1536,16 @@ name|pathspec
 parameter_list|)
 value|unmerge_index(&the_index, pathspec)
 end_define
+begin_define
+DECL|macro|read_blob_data_from_cache
+define|#
+directive|define
+name|read_blob_data_from_cache
+parameter_list|(
+name|path
+parameter_list|)
+value|read_blob_data_from_index(&the_index, (path))
+end_define
 begin_endif
 endif|#
 directive|endif
@@ -2669,6 +2679,22 @@ name|char
 modifier|*
 parameter_list|,
 name|int
+parameter_list|)
+function_decl|;
+end_function_decl
+begin_function_decl
+specifier|extern
+name|void
+modifier|*
+name|read_blob_data_from_index
+parameter_list|(
+name|struct
+name|index_state
+modifier|*
+parameter_list|,
+specifier|const
+name|char
+modifier|*
 parameter_list|)
 function_decl|;
 end_function_decl
