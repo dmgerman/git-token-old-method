9073130f241c13be3c09f17f676f33504b626a6a
Felipe Contreras <felipe.contreras@gmail.com>
2013-05-07 16:55:07 -0500

    sha1_name: remove unnecessary braces
    Signed-off-by: Felipe Contreras <felipe.contreras@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    cregit: b5f769a0d71bc3f13f916db849ef6b4768359c4e http://github.com/git/git/commit/b5f769a0d71bc3f13f916db849ef6b4768359c4e

--- sha1_name.c
+++ sha1_name.c
@@ -2379,7 +2379,6 @@ name|ret
 operator|>
 literal|0
 condition|)
-block|{
 comment|/* substitute this branch name and restart */
 return|return
 name|get_sha1_1
@@ -2397,7 +2396,6 @@ argument_list|,
 literal|0
 argument_list|)
 return|;
-block|}
 elseif|else
 if|if
 condition|(
@@ -2405,12 +2403,10 @@ name|ret
 operator|==
 literal|0
 condition|)
-block|{
 return|return
 operator|-
 literal|1
 return|;
-block|}
 comment|/* allow "@{...}" to mean the current branch reflog */
 name|refs_found
 operator|=
