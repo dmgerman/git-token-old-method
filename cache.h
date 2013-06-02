1b48508606cd6cfd530d10211027fe8426be4d0a
René Scharfe <rene.scharfe@lsrfire.ath.cx>
2013-06-02 17:46:51 +0200

    cache: mark cache_entry pointers const
    Add const for pointers that are only dereferenced for reading by the
    inline functions copy_cache_entry and ce_mode_from_stat.  This allows
    callers to pass in const pointers.
    
    Signed-off-by: René Scharfe <rene.scharfe@lsrfire.ath.cx>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    cregit: 20d142b48c8f2f50662db095e193836d0a8f5ad1 http://github.com/git/git/commit/20d142b48c8f2f50662db095e193836d0a8f5ad1

--- cache.h
+++ cache.h
@@ -737,6 +737,7 @@ name|cache_entry
 modifier|*
 name|dst
 parameter_list|,
+specifier|const
 name|struct
 name|cache_entry
 modifier|*
@@ -933,6 +934,7 @@ name|unsigned
 name|int
 name|ce_mode_from_stat
 parameter_list|(
+specifier|const
 name|struct
 name|cache_entry
 modifier|*
