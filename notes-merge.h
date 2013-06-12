357fd60870562462ec302b2e07b49de78407bade
Johan Herland <johan@herland.net>
2013-06-12 02:13:01 +0200

    Move create_notes_commit() from notes-merge.c into notes-utils.c
    create_notes_commit() is needed by both the notes-merge code, and by
    commit_notes() in notes-utils. Since it is generally useful, and not
    bound to the notes-merge machinery, we move it from (the more specific)
    notes-merge to (the more general) notes-utils.
    
    Signed-off-by: Johan Herland <johan@herland.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    cregit: bf9a05ba46a9d464424d22482aaa54abdd28b77d http://github.com/git/git/commit/bf9a05ba46a9d464424d22482aaa54abdd28b77d

--- notes-merge.h
+++ notes-merge.h
@@ -103,36 +103,6 @@ parameter_list|)
 function_decl|;
 end_function_decl
 begin_comment
-comment|/*  * Create new notes commit from the given notes tree  *  * Properties of the created commit:  * - tree: the result of converting t to a tree object with write_notes_tree().  * - parents: the given parents OR (if NULL) the commit referenced by t->ref.  * - author/committer: the default determined by commmit_tree().  * - commit message: msg  *  * The resulting commit SHA1 is stored in result_sha1.  */
-end_comment
-begin_function_decl
-name|void
-name|create_notes_commit
-parameter_list|(
-name|struct
-name|notes_tree
-modifier|*
-name|t
-parameter_list|,
-name|struct
-name|commit_list
-modifier|*
-name|parents
-parameter_list|,
-specifier|const
-name|struct
-name|strbuf
-modifier|*
-name|msg
-parameter_list|,
-name|unsigned
-name|char
-modifier|*
-name|result_sha1
-parameter_list|)
-function_decl|;
-end_function_decl
-begin_comment
 comment|/*  * Merge notes from o->remote_ref into o->local_ref  *  * The given notes_tree 'local_tree' must be the notes_tree referenced by the  * o->local_ref. This is the notes_tree in which the object-level merge is  * performed.  *  * The commits given by the two refs are merged, producing one of the following  * outcomes:  *  * 1. The merge trivially results in an existing commit (e.g. fast-forward or  *    already-up-to-date). 'local_tree' is untouched, the SHA1 of the result  *    is written into 'result_sha1' and 0 is returned.  * 2. The merge successfully completes, producing a merge commit. local_tree  *    contains the updated notes tree, the SHA1 of the resulting commit is  *    written into 'result_sha1', and 1 is returned.  * 3. The merge results in conflicts. This is similar to #2 in that the  *    partial merge result (i.e. merge result minus the unmerged entries)  *    are stored in 'local_tree', and the SHA1 or the resulting commit  *    (to be amended when the conflicts have been resolved) is written into  *    'result_sha1'. The unmerged entries are written into the  *    .git/NOTES_MERGE_WORKTREE directory with conflict markers.  *    -1 is returned.  *  * Both o->local_ref and o->remote_ref must be given (non-NULL), but either ref  * (although not both) may refer to a non-existing notes ref, in which case  * that notes ref is interpreted as an empty notes tree, and the merge  * trivially results in what the other ref points to.  */
 end_comment
 begin_function_decl
