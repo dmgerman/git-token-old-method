e0678e1d825f05715425d10214921d02bb07f899
Jeff King <peff@peff.net>
2013-06-21 03:05:39 -0400

    transport-helper: be quiet on read errors from helpers
    Prior to commit 81d340d4, we did not print any error message
    if a remote transport helper died unexpectedly. If a helper
    did not print any error message (e.g., because it crashed),
    the user could be left confused. That commit tried to
    rectify the situation by printing a note that the helper
    exited unexpectedly.
    
    However, this makes a much more common case worse: when a
    helper does die with a useful message, we print the extra
    "Reading from 'git-remote-foo failed" message. This can also
    end up confusing users, as they may not even know what
    remote helpers are (e.g., the fact that http support comes
    through git-remote-https is purely an implementation detail
    that most users do not know or care about).
    
    Since we do not have a good way of knowing whether the
    helper printed a useful error, and since the common failure
    mode is for it to do so, let's default to remaining quiet.
    Debuggers can dig further by setting GIT_TRANSPORT_HELPER_DEBUG.
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    cregit: 266f1fdfa99f5d29ca7ce455966e7960c00a82e4 http://github.com/git/git/commit/266f1fdfa99f5d29ca7ce455966e7960c00a82e4

--- transport-helper.c
+++ transport-helper.c
@@ -298,11 +298,9 @@ argument_list|,
 literal|"Debug: Remote helper quit.\n"
 argument_list|)
 expr_stmt|;
-name|die
+name|exit
 argument_list|(
-literal|"Reading from helper 'git-remote-%s' failed"
-argument_list|,
-name|name
+literal|128
 argument_list|)
 expr_stmt|;
 block|}
