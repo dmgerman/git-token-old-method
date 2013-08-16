8784087935518e196cb4acab164f57dd70d535f0
Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
2013-08-16 16:52:04 +0700

    shallow: add setup_temporary_shallow()
    This function is like setup_alternate_shallow() except that it does
    not lock $GIT_DIR/shallow.  It is supposed to be used when a program
    generates temporary shallow for use by another program, then throw
    the shallow file away.
    
    Signed-off-by: Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    cregit: 08ea65ad13faaf24f57732cdc8af3d830d4b367f http://github.com/git/git/commit/08ea65ad13faaf24f57732cdc8af3d830d4b367f

--- commit.h
+++ commit.h
@@ -1208,6 +1208,16 @@ parameter_list|)
 function_decl|;
 end_function_decl
 begin_function_decl
+specifier|extern
+name|char
+modifier|*
+name|setup_temporary_shallow
+parameter_list|(
+name|void
+parameter_list|)
+function_decl|;
+end_function_decl
+begin_function_decl
 name|int
 name|is_descendant_of
 parameter_list|(
