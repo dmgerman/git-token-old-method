1c3e1ffd775b59e0dc12e16e7faa30d3f6eef33d
Thorsten Glaser <tg@debian.org>
2013-08-30 00:03:10 +0200

    builtin/tag.c: show tag name to hint in the message editor
    Display the tag name about to be added to the user during interactive
    editing.
    
    Signed-off-by: Thorsten Glaser <tg@debian.org>
    Signed-off-by: Richard Hartmann <richih@debian.org>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    cregit: d78f340ed6250dfe8de6e039acc56a013bcf3637 http://github.com/git/git/commit/d78f340ed6250dfe8de6e039acc56a013bcf3637

--- builtin/tag.c
+++ builtin/tag.c
@@ -1310,7 +1310,7 @@ index|[]
 init|=
 name|N_
 argument_list|(
-literal|"\nWrite a tag message\n"
+literal|"\nWrite a message for tag:\n  %s\n"
 literal|"Lines starting with '%c' will be ignored.\n"
 argument_list|)
 decl_stmt|;
@@ -1325,7 +1325,7 @@ index|[]
 init|=
 name|N_
 argument_list|(
-literal|"\nWrite a tag message\n"
+literal|"\nWrite a message for tag:\n  %s\n"
 literal|"Lines starting with '%c' will be kept; you may remove them"
 literal|" yourself if you want to.\n"
 argument_list|)
@@ -1856,6 +1856,8 @@ argument_list|(
 name|tag_template
 argument_list|)
 argument_list|,
+name|tag
+argument_list|,
 name|comment_line_char
 argument_list|)
 expr_stmt|;
@@ -1869,6 +1871,8 @@ name|_
 argument_list|(
 name|tag_template_nocleanup
 argument_list|)
+argument_list|,
+name|tag
 argument_list|,
 name|comment_line_char
 argument_list|)
