5c2f2b357f0b922c55f4a808030bebd0caf50ef0
Jeff King <peff@peff.net>
2013-09-24 01:19:08 -0400

    remote: do not copy "origin" string literal
    Our default_remote_name starts at "origin", but may be
    overridden by the config file. In the former case, we
    allocate a new string, but in the latter case, we point to
    the remote name in an existing "struct branch".
    
    This gives the variable inconsistent free() semantics (we
    are sometimes responsible for freeing the string and
    sometimes pointing to somebody else's storage), and causes a
    small leak when the allocated string is overridden by
    config.
    
    We can fix both by simply dropping the extra copy and
    pointing to the string literal.
    
    Noticed-by: Felipe Contreras <felipe.contreras@gmail.com>
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    cregit: 11a6ba1c012e8275c70032fb2e847740a2f61865 http://github.com/git/git/commit/11a6ba1c012e8275c70032fb2e847740a2f61865

--- remote.c
+++ remote.c
@@ -3105,10 +3105,7 @@ comment|/* did this already */
 return|return;
 name|default_remote_name
 operator|=
-name|xstrdup
-argument_list|(
 literal|"origin"
-argument_list|)
 expr_stmt|;
 name|current_branch
 operator|=
