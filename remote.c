684d5119e9d8fbbf3a4ebf0ddcffd290c1b63a51
Michael Haggerty <mhagger@alum.mit.edu>
2013-10-30 06:32:57 +0100

    get_expanded_map(): avoid memory leak
    The old code could leak *expn_name if match_name_with_pattern()
    succeeded but ignore_symref_update() returned true.  So make sure that
    *expn_name is freed in any case.
    
    Signed-off-by: Michael Haggerty <mhagger@alum.mit.edu>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    cregit: e31a17f7416a85ee8c3260f8c8b7519ff7ea82b6 http://github.com/git/git/commit/e31a17f7416a85ee8c3260f8c8b7519ff7ea82b6

--- remote.c
+++ remote.c
@@ -8750,10 +8750,6 @@ init|=
 operator|&
 name|ret
 decl_stmt|;
-name|char
-modifier|*
-name|expn_name
-decl_stmt|;
 for|for
 control|(
 name|ref
@@ -8769,6 +8765,12 @@ operator|->
 name|next
 control|)
 block|{
+name|char
+modifier|*
+name|expn_name
+init|=
+name|NULL
+decl_stmt|;
 if|if
 condition|(
 name|strchr
@@ -8828,11 +8830,6 @@ argument_list|(
 name|expn_name
 argument_list|)
 expr_stmt|;
-name|free
-argument_list|(
-name|expn_name
-argument_list|)
-expr_stmt|;
 if|if
 condition|(
 name|refspec
@@ -8860,6 +8857,11 @@ operator|->
 name|next
 expr_stmt|;
 block|}
+name|free
+argument_list|(
+name|expn_name
+argument_list|)
+expr_stmt|;
 block|}
 return|return
 name|ret
