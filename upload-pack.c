55385fc2fc9ebd0a71a9b306f93f20c57d1799a3
Junio C Hamano <gitster@pobox.com>
2013-11-18 10:15:45 -0800

    Revert "upload-pack: send non-HEAD symbolic refs"
    This reverts commit 5e7dcad771cb873e278a0571b46910d7c32e2f6c; there
    may be unbounded number of symbolic refs in the repository, but the
    capability header line in the on-wire protocol has a rather low
    length limit.
    
    cregit: d007dbf7d6d647dbcf0f357545f43f36dec46f3b http://github.com/git/git/commit/d007dbf7d6d647dbcf0f357545f43f36dec46f3b

--- upload-pack.c
+++ upload-pack.c
@@ -3878,14 +3878,6 @@ operator|&
 name|symref
 argument_list|)
 expr_stmt|;
-name|for_each_namespaced_ref
-argument_list|(
-name|find_symref
-argument_list|,
-operator|&
-name|symref
-argument_list|)
-expr_stmt|;
 if|if
 condition|(
 name|advertise_refs
