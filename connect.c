725f7830df05266c53cd76f1969e9f1a88d30299
Torsten Bögershausen <tboegi@web.de>
2013-11-28 20:49:38 +0100

    git fetch: support host:/~repo
    The documentation (in urls.txt) says that
    
        "ssh://host:/~repo",
        "host:/~repo" or
        "host:~repo"
    
    specify the repository "repo" in the home directory at "host".
    
    This has not been working for "host:/~repo".
    
    Before commit 356bec "Support [address] in URLs", the comparison
    "url != hostname" could be used to determine if the URL had a scheme
    or not: "ssh://host/host" != "host".
    
    However, after 356bec "[::1]" was converted into "::1", yielding
    url != hostname as well.  To fix this regression, don't use
    "if (url != hostname)", but look at the separator instead.
    
    Rename the variable "c" into "separator" to make it easier to read.
    
    Signed-off-by: Torsten Bögershausen <tboegi@web.de>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    cregit: 6a59974869c0a6e9399101bc02223b4c00a8aff2 http://github.com/git/git/commit/6a59974869c0a6e9399101bc02223b4c00a8aff2

--- connect.c
+++ connect.c
@@ -2935,7 +2935,7 @@ modifier|*
 name|end
 decl_stmt|;
 name|int
-name|c
+name|separator
 decl_stmt|;
 name|enum
 name|protocol
@@ -3006,7 +3006,7 @@ name|host
 operator|+=
 literal|3
 expr_stmt|;
-name|c
+name|separator
 operator|=
 literal|'/'
 expr_stmt|;
@@ -3017,7 +3017,7 @@ name|host
 operator|=
 name|url
 expr_stmt|;
-name|c
+name|separator
 operator|=
 literal|':'
 expr_stmt|;
@@ -3086,7 +3086,7 @@ name|strchr
 argument_list|(
 name|end
 argument_list|,
-name|c
+name|separator
 argument_list|)
 expr_stmt|;
 if|if
@@ -3102,7 +3102,7 @@ condition|)
 block|{
 if|if
 condition|(
-name|c
+name|separator
 operator|==
 literal|':'
 condition|)
@@ -3167,10 +3167,6 @@ condition|(
 name|protocol
 operator|!=
 name|PROTO_LOCAL
-operator|&&
-name|host
-operator|!=
-name|url
 condition|)
 block|{
 name|char
@@ -3218,9 +3214,9 @@ name|protocol
 operator|==
 name|PROTO_SSH
 operator|&&
-name|host
-operator|!=
-name|url
+name|separator
+operator|==
+literal|'/'
 condition|)
 name|port
 operator|=
