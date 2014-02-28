72bf48eb8ce768f379cb574f3b61abe03e5bc151
Ralf Thielow <ralf.thielow@gmail.com>
2014-02-28 20:27:29 +0100

    help.c: rename function "pretty_print_string_list"
    The part "string_list" of the name of function
    "pretty_print_string_list" is just an implementation
    detail. The function pretty-prints command names so
    rename it to "pretty_print_cmdnames".
    
    Signed-off-by: Ralf Thielow <ralf.thielow@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    cregit: d10cb3dfab92bf24e2919ae3d4a16013dba0261f http://github.com/git/git/commit/d10cb3dfab92bf24e2919ae3d4a16013dba0261f

--- help.c
+++ help.c
@@ -528,10 +528,10 @@ expr_stmt|;
 block|}
 end_function
 begin_function
-DECL|function|pretty_print_string_list
+DECL|function|pretty_print_cmdnames
 specifier|static
 name|void
-name|pretty_print_string_list
+name|pretty_print_cmdnames
 parameter_list|(
 name|struct
 name|cmdnames
@@ -1236,7 +1236,7 @@ argument_list|(
 literal|'\n'
 argument_list|)
 expr_stmt|;
-name|pretty_print_string_list
+name|pretty_print_cmdnames
 argument_list|(
 name|main_cmds
 argument_list|,
@@ -1269,7 +1269,7 @@ argument_list|(
 literal|'\n'
 argument_list|)
 expr_stmt|;
-name|pretty_print_string_list
+name|pretty_print_cmdnames
 argument_list|(
 name|other_cmds
 argument_list|,
