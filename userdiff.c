309d39717d9ec0e53629e9c70827a3e876084171
Johannes Sixt <j6t@kdbg.org>
2014-03-21 22:07:14 +0100

    userdiff: support unsigned and long long suffixes of integer constants
    Do not split constants such as 123U, 456ll, 789UL at the first U or
    second L.
    
    Signed-off-by: Johannes Sixt <j6t@kdbg.org>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    cregit: abf8f9860248d8c213600974742f18dadaa8fbb5 http://github.com/git/git/commit/abf8f9860248d8c213600974742f18dadaa8fbb5

--- userdiff.c
+++ userdiff.c
@@ -282,7 +282,7 @@ literal|"^((struct|class|enum)[^;]*)$"
 argument_list|,
 comment|/* -- */
 literal|"[a-zA-Z_][a-zA-Z0-9_]*"
-literal|"|[-+0-9.e]+[fFlL]?|0[xXbB]?[0-9a-fA-F]+[lL]?"
+literal|"|[-+0-9.e]+[fFlL]?|0[xXbB]?[0-9a-fA-F]+[lLuU]*"
 literal|"|[-+*/<>%&^|=!]=|--|\\+\\+|<<=?|>>=?|&&|\\|\\||::|->\\*?|\\.\\*"
 argument_list|)
 block|,
