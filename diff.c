1fac63974270c091d969c56853059c3ff8631745
Jeff King <peff@peff.net>
2014-04-19 15:19:19 -0400

    run_external_diff: clean up error handling
    When the external diff reports an error, we try to clean up
    and die. However, we can make this process a bit simpler:
    
      1. We do not need to bother freeing memory, since we are
         about to exit.  Nor do we need to clean up our
         tempfiles, since the atexit() handler will do it for
         us. So we can die as soon as we see the error.
    
      3. We can just call die() rather than fprintf/exit. This
         does technically change our exit code, but the exit
         code of "1" is not meaningful here. In fact, it is
         probably wrong, since "1" from diff usually means
         "completed successfully, but there were differences".
    
    And while we're there, we can mark the error message for
    translation, and drop the full stop at the end to make it
    more like our other messages.
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    cregit: 89294d143d42db2540ec587d0bce20c6c7718051 http://github.com/git/git/commit/89294d143d42db2540ec587d0bce20c6c7718051

--- diff.c
+++ diff.c
@@ -16504,9 +16504,6 @@ name|env
 init|=
 name|ARGV_ARRAY_INIT
 decl_stmt|;
-name|int
-name|retval
-decl_stmt|;
 name|struct
 name|diff_queue_struct
 modifier|*
@@ -16709,8 +16706,8 @@ operator|->
 name|nr
 argument_list|)
 expr_stmt|;
-name|retval
-operator|=
+if|if
+condition|(
 name|run_command_v_opt_cd_env
 argument_list|(
 name|argv
@@ -16724,6 +16721,16 @@ argument_list|,
 name|env
 operator|.
 name|argv
+argument_list|)
+condition|)
+name|die
+argument_list|(
+name|_
+argument_list|(
+literal|"external diff died, stopping at %s"
+argument_list|)
+argument_list|,
+name|name
 argument_list|)
 expr_stmt|;
 name|remove_tempfile
@@ -16741,26 +16748,6 @@ operator|&
 name|env
 argument_list|)
 expr_stmt|;
-if|if
-condition|(
-name|retval
-condition|)
-block|{
-name|fprintf
-argument_list|(
-name|stderr
-argument_list|,
-literal|"external diff died, stopping at %s.\n"
-argument_list|,
-name|name
-argument_list|)
-expr_stmt|;
-name|exit
-argument_list|(
-literal|1
-argument_list|)
-expr_stmt|;
-block|}
 block|}
 end_function
 begin_function
