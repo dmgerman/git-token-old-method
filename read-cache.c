7ecb96144697d4d3a0a0a1b615f4bd557520d8e5
Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
2014-06-13 19:19:37 +0700

    read-cache: mark new entries for split index
    Make sure entry addition does not lead to unifying the index. We don't
    need to explicitly keep track of new entries. If ce->index is zero,
    they're new. Otherwise it's unlikely that they are new, but we'll do a
    thorough check later at writing time.
    
    Signed-off-by: Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    cregit: e0cf0d7de28ee90e1668049e32802a8dbbcc2950 http://github.com/git/git/commit/e0cf0d7de28ee90e1668049e32802a8dbbcc2950

--- read-cache.c
+++ read-cache.c
@@ -145,7 +145,7 @@ DECL|macro|EXTMASK
 define|#
 directive|define
 name|EXTMASK
-value|(RESOLVE_UNDO_CHANGED | CACHE_TREE_CHANGED)
+value|(RESOLVE_UNDO_CHANGED | CACHE_TREE_CHANGED | \ 		 CE_ENTRY_ADDED)
 end_define
 begin_decl_stmt
 DECL|variable|the_index
