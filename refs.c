8bfda943369c41bd919335d6d9fc8b4ca65843e3
Ronnie Sahlberg <sahlberg@google.com>
2014-06-20 07:42:57 -0700

    refs.c: make update_ref_write update a strbuf on failure
    Change update_ref_write to also update an error strbuf on failure.
    This makes the error available to ref_transaction_commit callers if the
    transaction failed due to update_ref_sha1/write_ref_sha1 failures.
    
    Reviewed-by: Jonathan Nieder <jrnieder@gmail.com>
    Signed-off-by: Ronnie Sahlberg <sahlberg@google.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    Acked-by: Michael Haggerty <mhagger@alum.mit.edu>
    
    cregit: c1703d7634a61b4d08d77fa0d9ca7e7ad92c3cb8 http://github.com/git/git/commit/c1703d7634a61b4d08d77fa0d9ca7e7ad92c3cb8

--- refs.c
+++ refs.c
@@ -15795,6 +15795,11 @@ name|ref_lock
 modifier|*
 name|lock
 parameter_list|,
+name|struct
+name|strbuf
+modifier|*
+name|err
+parameter_list|,
 name|enum
 name|action_on_err
 name|onerr
@@ -15821,6 +15826,19 @@ name|str
 init|=
 literal|"Cannot update the ref '%s'."
 decl_stmt|;
+if|if
+condition|(
+name|err
+condition|)
+name|strbuf_addf
+argument_list|(
+name|err
+argument_list|,
+name|str
+argument_list|,
+name|refname
+argument_list|)
+expr_stmt|;
 switch|switch
 condition|(
 name|onerr
@@ -16430,6 +16448,8 @@ name|sha1
 argument_list|,
 name|lock
 argument_list|,
+name|NULL
+argument_list|,
 name|onerr
 argument_list|)
 return|;
@@ -16893,6 +16913,8 @@ argument_list|,
 name|update
 operator|->
 name|lock
+argument_list|,
+name|err
 argument_list|,
 name|onerr
 argument_list|)
