fced56d71b4b3a5aa630e365cdc01ab3a0307246
Junio C Hamano <gitster@pobox.com>
2014-08-15 11:30:36 -0700

    send-pack: always send capabilities
    We tried to avoid sending one extra byte, NUL and nothing behind it
    to signal there is no protocol capabilities being sent, on the first
    command packet on the wire, but it just made the code look ugly.
    
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    cregit: 64de20a1265b676f9cc0e5f46379a017560fa333 http://github.com/git/git/commit/64de20a1265b676f9cc0e5f46379a017560fa333

--- send-pack.c
+++ send-pack.c
@@ -1380,18 +1380,7 @@ if|if
 condition|(
 operator|!
 name|cmds_sent
-operator|&&
-operator|(
-name|status_report
-operator|||
-name|use_sideband
-operator|||
-name|quiet
-operator|||
-name|agent_supported
-operator|)
 condition|)
-block|{
 name|packet_buf_write
 argument_list|(
 operator|&
@@ -1441,7 +1430,6 @@ else|:
 literal|""
 argument_list|)
 expr_stmt|;
-block|}
 else|else
 name|packet_buf_write
 argument_list|(
