d0b57bbbd1d7420ce63ac6413a823ee22be4c7a1
Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
2014-08-16 10:08:05 +0700

    diff --stat: mark any file larger than core.bigfilethreshold binary
    Too large files may lead to failure to allocate memory. If it happens
    here, it could impact quite a few commands that involve
    diff. Moreover, too large files are inefficient to compare anyway (and
    most likely non-text), so mark them binary and skip looking at their
    content.
    
    Noticed-by: Dale R. Worley <worley@alum.mit.edu>
    Signed-off-by: Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    cregit: 6bf3b813486b4528feca39d599c256f662defc14 http://github.com/git/git/commit/6bf3b813486b4528feca39d599c256f662defc14

--- diffcore.h
+++ diffcore.h
@@ -252,6 +252,13 @@ directive|define
 name|CHECK_SIZE_ONLY
 value|1
 end_define
+begin_define
+DECL|macro|CHECK_BINARY
+define|#
+directive|define
+name|CHECK_BINARY
+value|2
+end_define
 begin_function_decl
 specifier|extern
 name|int
