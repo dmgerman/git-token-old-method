c98a295bdff1d72a418944604ed9b8084768a3de
René Scharfe <l.s.r@web.de>
2014-08-19 21:10:48 +0200

    run-command: introduce child_process_init()
    Add a helper function for initializing those struct child_process
    variables for which the macro CHILD_PROCESS_INIT can't be used.
    
    Suggested-by: Jeff King <peff@peff.net>
    Signed-off-by: Rene Scharfe <l.s.r@web.de>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    cregit: 483bbd4e4ce8a5c717cd47d732893317a14c965a http://github.com/git/git/commit/483bbd4e4ce8a5c717cd47d732893317a14c965a

--- run-command.h
+++ run-command.h
@@ -137,6 +137,16 @@ name|CHILD_PROCESS_INIT
 value|{ NULL, ARGV_ARRAY_INIT }
 end_define
 begin_function_decl
+name|void
+name|child_process_init
+parameter_list|(
+name|struct
+name|child_process
+modifier|*
+parameter_list|)
+function_decl|;
+end_function_decl
+begin_function_decl
 name|int
 name|start_command
 parameter_list|(
