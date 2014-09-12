048c94ea607eb6cc5605c352c0c0544395f8833b
Jeff King <peff@peff.net>
2014-09-11 23:38:30 -0400

    fsck: return non-zero status on missing ref tips
    Fsck tries hard to detect missing objects, and will complain
    (and exit non-zero) about any inter-object links that are
    missing. However, it will not exit non-zero for any missing
    ref tips, meaning that a severely broken repository may
    still pass "git fsck && echo ok".
    
    The problem is that we use for_each_ref to iterate over the
    ref tips, which hides broken tips. It does at least print an
    error from the refs.c code, but fsck does not ever see the
    ref and cannot note the problem in its exit code. We can solve
    this by using for_each_rawref and noting the error ourselves.
    
    In addition to adding tests for this case, we add tests for
    all types of missing-object links (all of which worked, but
    which we were not testing).
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    cregit: 30d1038d1bd538bf2f8abfb5f5dccf80327a26c9 http://github.com/git/git/commit/30d1038d1bd538bf2f8abfb5f5dccf80327a26c9

--- builtin/fsck.c
+++ builtin/fsck.c
@@ -2534,6 +2534,10 @@ name|sha1
 argument_list|)
 argument_list|)
 expr_stmt|;
+name|errors_found
+operator||=
+name|ERROR_REACHABLE
+expr_stmt|;
 comment|/* We'll continue with the rest despite the error.. */
 return|return
 literal|0
@@ -2608,7 +2612,7 @@ argument_list|,
 name|NULL
 argument_list|)
 expr_stmt|;
-name|for_each_ref
+name|for_each_rawref
 argument_list|(
 name|fsck_handle_ref
 argument_list|,
