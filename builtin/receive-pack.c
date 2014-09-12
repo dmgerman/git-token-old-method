500f1fef5a3e4ffd00e7c748b92b294842a685f2
Junio C Hamano <gitster@pobox.com>
2014-09-12 15:48:07 -0700

    receive-pack: allow hooks to ignore its standard input stream
    The pre-receive and post-receive hooks were designed to be an
    improvement over old style update and post-update hooks, which take
    the update information on their command line and are limited by the
    command line length limit.  The same information is fed from the
    standard input to pre/post-receive hooks instead to lift this
    limitation.  It has been mandatory for these new style hooks to
    consume the update information fully from the standard input stream.
    Otherwise, they would risk killing the receive-pack process via
    SIGPIPE.
    
    If a hook does not want to look at all the information, it is easy
    to send its standard input to /dev/null (perhaps a niche use of hook
    might need to know only the fact that a push was made, without
    having to know what objects have been pushed to update which refs),
    and this has already been done by existing hooks that are written
    carefully.
    
    However, because there is no good way to consistently fail hooks
    that do not consume the input fully (a small push may result in a
    short update record that may fit within the pipe buffer, to which
    the receive-pack process may manage to write before the hook has a
    chance to exit without reading anything, which will not result in a
    death-by-SIGPIPE of receive-pack), it can lead to a hard to diagnose
    "once in a blue moon" phantom failure.
    
    Lift this "hooks must consume their input fully" mandate.  A mandate
    that is not enforced strictly is not helping us to catch mistakes in
    hooks.  If a hook has a good reason to decide the outcome of its
    operation without reading the information we feed it, let it do so
    as it pleases.
    
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    cregit: ec7dbd145bd8ea27d958dcb16e19b5f0ef3fd643 http://github.com/git/git/commit/ec7dbd145bd8ea27d958dcb16e19b5f0ef3fd643

--- builtin/receive-pack.c
+++ builtin/receive-pack.c
@@ -84,6 +84,11 @@ include|#
 directive|include
 file|"version.h"
 end_include
+begin_include
+include|#
+directive|include
+file|"sigchain.h"
+end_include
 begin_decl_stmt
 DECL|variable|receive_pack_usage
 specifier|static
@@ -1579,6 +1584,13 @@ return|return
 name|code
 return|;
 block|}
+name|sigchain_push
+argument_list|(
+name|SIGPIPE
+argument_list|,
+name|SIG_IGN
+argument_list|)
+expr_stmt|;
 while|while
 condition|(
 literal|1
@@ -1638,6 +1650,11 @@ name|finish_async
 argument_list|(
 operator|&
 name|muxer
+argument_list|)
+expr_stmt|;
+name|sigchain_pop
+argument_list|(
+name|SIGPIPE
 argument_list|)
 expr_stmt|;
 return|return
