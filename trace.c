864478c6a19a488e215c524287ca2662a376a1ea
Ben Walton <bdwalton@gmail.com>
2014-09-28 08:50:26 +0100

    trace.c: do not mark getnanotime() as "inline"
    Oracle Studio compilers don't allow for static variables in
    functions that are defined to be inline. GNU C does permit this.
    
    Let's reference the C99 standard though, which doesn't allow for
    inline functions to contain modifiable static variables.
    
    Signed-off-by: Ben Walton <bdwalton@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    cregit: 6433d569753e8d33d3f7beca16a65f2fc481aade http://github.com/git/git/commit/6433d569753e8d33d3f7beca16a65f2fc481aade

--- trace.c
+++ trace.c
@@ -1851,7 +1851,6 @@ comment|/*  * Returns nanoseconds since the epoch (01/01/1970), for performance 
 end_comment
 begin_function
 DECL|function|getnanotime
-specifier|inline
 name|uint64_t
 name|getnanotime
 parameter_list|(
