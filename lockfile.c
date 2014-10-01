12c3531e60218b59654a7c786e1727b2bec47789
Michael Haggerty <mhagger@alum.mit.edu>
2014-10-01 12:28:21 +0200

    commit_lock_file(): die() if called for unlocked lockfile object
    It was previously a bug to call commit_lock_file() with a lock_file
    object that was not active (an illegal access would happen within the
    function).  It was presumably never done, but this would be an easy
    programming error to overlook.  So before continuing, do a consistency
    check that the lock_file object really is locked.
    
    Helped-by: Johannes Sixt <j6t@kdbg.org>
    Signed-off-by: Michael Haggerty <mhagger@alum.mit.edu>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    cregit: 8a1c7533e2ee468a505656abab364780b15004fc http://github.com/git/git/commit/8a1c7533e2ee468a505656abab364780b15004fc

--- lockfile.c
+++ lockfile.c
@@ -1107,6 +1107,21 @@ index|]
 decl_stmt|;
 if|if
 condition|(
+operator|!
+name|lk
+operator|->
+name|filename
+index|[
+literal|0
+index|]
+condition|)
+name|die
+argument_list|(
+literal|"BUG: attempt to commit unlocked object"
+argument_list|)
+expr_stmt|;
+if|if
+condition|(
 name|close_lock_file
 argument_list|(
 name|lk
