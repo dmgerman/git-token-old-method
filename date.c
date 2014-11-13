7fcbbad719c3fc40c70ca626344eb71c196d36de
Jeff King <peff@peff.net>
2014-11-13 06:04:52 -0500

    pass TIME_DATE_NOW to approxidate future-check
    The approxidate functions accept an extra "now" parameter to
    avoid calling time() themselves. We use this in our test
    suite to make sure we have a consistent time for computing
    relative dates. However, deep in the bowels of approxidate,
    we also call time() to check whether possible dates are far
    in the future. Let's make sure that the "now" override makes
    it to that spot, too, so we can consistently test that
    feature.
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    cregit: 073281e2aea7177e27de8a46c5463e2f83833412 http://github.com/git/git/commit/073281e2aea7177e27de8a46c5463e2f83833412

--- date.c
+++ date.c
@@ -2373,11 +2373,11 @@ name|struct
 name|tm
 modifier|*
 name|tm
-parameter_list|)
-block|{
+parameter_list|,
 name|time_t
 name|now
-decl_stmt|;
+parameter_list|)
+block|{
 name|struct
 name|tm
 name|now_tm
@@ -2514,6 +2514,11 @@ case|:
 case|case
 literal|'.'
 case|:
+if|if
+condition|(
+operator|!
+name|now
+condition|)
 name|now
 operator|=
 name|time
@@ -2847,6 +2852,8 @@ argument_list|,
 name|end
 argument_list|,
 name|tm
+argument_list|,
+literal|0
 argument_list|)
 decl_stmt|;
 if|if
@@ -5500,6 +5507,9 @@ parameter_list|,
 name|int
 modifier|*
 name|num
+parameter_list|,
+name|time_t
+name|now
 parameter_list|)
 block|{
 name|char
@@ -5564,6 +5574,8 @@ argument_list|,
 name|end
 argument_list|,
 name|tm
+argument_list|,
+name|now
 argument_list|)
 decl_stmt|;
 if|if
@@ -5876,6 +5888,8 @@ name|tm
 argument_list|,
 operator|&
 name|number
+argument_list|,
+name|time_sec
 argument_list|)
 expr_stmt|;
 name|touched
