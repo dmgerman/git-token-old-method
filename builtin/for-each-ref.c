a2c83821eae33df89ecf92516458689437863228
Ralf Thielow <ralf.thielow@gmail.com>
2014-11-28 19:00:11 +0100

    for-each-ref: correct spelling of Tcl in option description
    Tcl is conventionally spelled "Tcl". The description of
    option "--tcl", however, spells it "tcl". Let's follow
    the convention.
    
    Reported-by: Hartmut Henkel <hartmut_henkel@gmx.de>
    Signed-off-by: Ralf Thielow <ralf.thielow@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    cregit: b799a696b221ff72d25cc06ac9fd7535ff08fc88 http://github.com/git/git/commit/b799a696b221ff72d25cc06ac9fd7535ff08fc88

--- builtin/for-each-ref.c
+++ builtin/for-each-ref.c
@@ -6150,7 +6150,7 @@ name|quote_style
 argument_list|,
 name|N_
 argument_list|(
-literal|"quote placeholders suitably for tcl"
+literal|"quote placeholders suitably for Tcl"
 argument_list|)
 argument_list|,
 name|QUOTE_TCL
