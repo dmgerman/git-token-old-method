0bac047abeaa05666ecc801a69ab86eaeebb0431
Michael J Gruber <git@drmicha.warpmail.net>
2014-12-08 17:28:45 +0100

    branch: allow -f with -m and -d
    -f/--force is the standard way to force an action, and is used by branch
    for the recreation of existing branches, but not for deleting unmerged
    branches nor for renaming to an existing branch.
    
    Make "-m -f" equivalent to "-M" and "-d -f" equivalent to" -D", i.e.
    allow -f/--force to be used with -m/-d also.
    
    For the list modes, "-f" is simply ignored.
    
    Signed-off-by: Michael J Gruber <git@drmicha.warpmail.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    cregit: 356e91f2ecc581f3a446bba566ab1756dc57a583 http://github.com/git/git/commit/356e91f2ecc581f3a446bba566ab1756dc57a583

--- builtin/branch.c
+++ builtin/branch.c
@@ -4396,7 +4396,7 @@ name|rename
 init|=
 literal|0
 decl_stmt|,
-name|force_create
+name|force
 init|=
 literal|0
 decl_stmt|,
@@ -4794,11 +4794,11 @@ block|,
 name|OPT__FORCE
 argument_list|(
 operator|&
-name|force_create
+name|force
 argument_list|,
 name|N_
 argument_list|(
-literal|"force creation (when already exists)"
+literal|"force creation, move/rename, deletion"
 argument_list|)
 argument_list|)
 block|,
@@ -5060,10 +5060,6 @@ name|rename
 operator|+
 operator|!
 operator|!
-name|force_create
-operator|+
-operator|!
-operator|!
 name|new_upstream
 operator|+
 name|list
@@ -5122,6 +5118,20 @@ expr_stmt|;
 name|colopts
 operator|=
 literal|0
+expr_stmt|;
+block|}
+if|if
+condition|(
+name|force
+condition|)
+block|{
+name|delete
+operator|*=
+literal|2
+expr_stmt|;
+name|rename
+operator|*=
+literal|2
 expr_stmt|;
 block|}
 if|if
@@ -5874,7 +5884,7 @@ index|]
 else|:
 name|head
 argument_list|,
-name|force_create
+name|force
 argument_list|,
 name|reflog
 argument_list|,
