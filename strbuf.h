c35e1f11eefe34dfa386c09261120cc857dab455
Stefan Beller <sbeller@google.com>
2015-01-16 04:04:51 -0500

    strbuf.h: unify documentation comments beginnings
    The prior patch uses "/**" to denote "documentation"
    comments that we pulled from api-strbuf.txt. Let's use a
    consistent style for similar comments that were already in
    strbuf.h.
    
    Signed-off-by: Stefan Beller <sbeller@google.com>
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    cregit: 6afbbdda3339349feae91161f4e530d511c4db76 http://github.com/git/git/commit/6afbbdda3339349feae91161f4e530d511c4db76

--- strbuf.h
+++ strbuf.h
@@ -1169,7 +1169,7 @@ return|;
 block|}
 end_function
 begin_comment
-comment|/*  * Split str (of length slen) at the specified terminator character.  * Return a null-terminated array of pointers to strbuf objects  * holding the substrings.  The substrings include the terminator,  * except for the last substring, which might be unterminated if the  * original string did not end with a terminator.  If max is positive,  * then split the string into at most max substrings (with the last  * substring containing everything following the (max-1)th terminator  * character).  *  * For lighter-weight alternatives, see string_list_split() and  * string_list_split_in_place().  */
+comment|/**  * Split str (of length slen) at the specified terminator character.  * Return a null-terminated array of pointers to strbuf objects  * holding the substrings.  The substrings include the terminator,  * except for the last substring, which might be unterminated if the  * original string did not end with a terminator.  If max is positive,  * then split the string into at most max substrings (with the last  * substring containing everything following the (max-1)th terminator  * character).  *  * For lighter-weight alternatives, see string_list_split() and  * string_list_split_in_place().  */
 end_comment
 begin_function_decl
 specifier|extern
@@ -1194,7 +1194,7 @@ parameter_list|)
 function_decl|;
 end_function_decl
 begin_comment
-comment|/*  * Split a NUL-terminated string at the specified terminator  * character.  See strbuf_split_buf() for more information.  */
+comment|/**  * Split a NUL-terminated string at the specified terminator  * character.  See strbuf_split_buf() for more information.  */
 end_comment
 begin_function
 DECL|function|strbuf_split_str
@@ -1236,7 +1236,7 @@ return|;
 block|}
 end_function
 begin_comment
-comment|/*  * Split a strbuf at the specified terminator character.  See  * strbuf_split_buf() for more information.  */
+comment|/**  * Split a strbuf at the specified terminator character.  See  * strbuf_split_buf() for more information.  */
 end_comment
 begin_function
 DECL|function|strbuf_split_max
@@ -1280,7 +1280,7 @@ return|;
 block|}
 end_function
 begin_comment
-comment|/*  * Split a strbuf at the specified terminator character.  See  * strbuf_split_buf() for more information.  */
+comment|/**  * Split a strbuf at the specified terminator character.  See  * strbuf_split_buf() for more information.  */
 end_comment
 begin_function
 DECL|function|strbuf_split
@@ -1315,7 +1315,7 @@ return|;
 block|}
 end_function
 begin_comment
-comment|/*  * Free a NULL-terminated list of strbufs (for example, the return  * values of the strbuf_split*() functions).  */
+comment|/**  * Free a NULL-terminated list of strbufs (for example, the return  * values of the strbuf_split*() functions).  */
 end_comment
 begin_function_decl
 specifier|extern
@@ -1382,7 +1382,7 @@ parameter_list|)
 function_decl|;
 end_function_decl
 begin_comment
-comment|/*  * Append s to sb, with the characters '<', '>', '&' and '"' converted  * into XML entities.  */
+comment|/**  * Append s to sb, with the characters '<', '>', '&' and '"' converted  * into XML entities.  */
 end_comment
 begin_function_decl
 specifier|extern
@@ -1558,7 +1558,7 @@ parameter_list|)
 function_decl|;
 end_function_decl
 begin_comment
-comment|/*  * Create a newly allocated string using printf format. You can do this easily  * with a strbuf, but this provides a shortcut to save a few lines.  */
+comment|/**  * Create a newly allocated string using printf format. You can do this easily  * with a strbuf, but this provides a shortcut to save a few lines.  */
 end_comment
 begin_macro
 name|__attribute__
