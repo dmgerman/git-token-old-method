569fc1582f2d6dee705c1539904684500afb2497
brian m. carlson <sandals@crustytoothpaste.net>
2015-03-13 23:39:31 +0000

    zip: use GIT_SHA1_HEXSZ for trailers
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    cregit: aeecdcd4c1c1a380e45aecadd8042a6a90ee3a20 http://github.com/git/git/commit/aeecdcd4c1c1a380e45aecadd8042a6a90ee3a20

--- archive-zip.c
+++ archive-zip.c
@@ -2489,7 +2489,7 @@ name|comment_length
 argument_list|,
 name|sha1
 condition|?
-literal|40
+name|GIT_SHA1_HEXSZ
 else|:
 literal|0
 argument_list|)
@@ -2526,7 +2526,7 @@ argument_list|(
 name|sha1
 argument_list|)
 argument_list|,
-literal|40
+name|GIT_SHA1_HEXSZ
 argument_list|)
 expr_stmt|;
 block|}
