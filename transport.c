245749c77918ad3547b49b980b6fca12f7e8831f
Jeff King <peff@peff.net>
2015-03-19 16:38:35 -0400

    fetch_refs_via_pack: free extra copy of refs
    When fetch_refs_via_pack calls fetch_pack(), we pass a
    list of refs to fetch, and the function returns either a
    copy of that list, with the fetched items filled in, or
    NULL. We check the return value to see whether the fetch was
    successful, but do not otherwise look at the copy, and
    simply leak it at the end of the function.
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    cregit: 626df76e3df3a0d8ecee07098706547648e6c96f http://github.com/git/git/commit/626df76e3df3a0d8ecee07098706547648e6c96f

--- transport.c
+++ transport.c
@@ -2940,7 +2940,6 @@ name|transport
 operator|->
 name|data
 decl_stmt|;
-specifier|const
 name|struct
 name|ref
 modifier|*
@@ -3217,10 +3216,17 @@ operator|->
 name|conn
 argument_list|)
 condition|)
+block|{
+name|free_refs
+argument_list|(
+name|refs
+argument_list|)
+expr_stmt|;
 name|refs
 operator|=
 name|NULL
 expr_stmt|;
+block|}
 name|data
 operator|->
 name|conn
@@ -3246,6 +3252,11 @@ expr_stmt|;
 name|free_refs
 argument_list|(
 name|refs_tmp
+argument_list|)
+expr_stmt|;
+name|free_refs
+argument_list|(
+name|refs
 argument_list|)
 expr_stmt|;
 name|free
