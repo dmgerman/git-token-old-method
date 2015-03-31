b8ddcddc774f82d945e4a4e8ceb7ca4c7ece2c44
Stefan Beller <sbeller@google.com>
2015-03-30 18:22:10 -0700

    pack-bitmap.c: fix a memleak
    Signed-off-by: Stefan Beller <sbeller@google.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    cregit: f86a3747abc742359397e3050b1fbef297415de5 http://github.com/git/git/commit/f86a3747abc742359397e3050b1fbef297415de5

--- pack-bitmap.c
+++ pack-bitmap.c
@@ -4543,6 +4543,11 @@ argument_list|,
 literal|"Mismatch!\n"
 argument_list|)
 expr_stmt|;
+name|free
+argument_list|(
+name|result
+argument_list|)
+expr_stmt|;
 block|}
 end_function
 begin_function
