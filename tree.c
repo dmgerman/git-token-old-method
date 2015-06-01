53d327f72a246c7138f98201045d2ff7f2a1fa70
Jeff King <peff@peff.net>
2015-06-01 05:56:26 -0400

    add quieter versions of parse_{tree,commit}
    When we call parse_commit, it will complain to stderr if the
    object does not exist or cannot be read. This means that we
    may produce useless error messages if this situation is
    expected (e.g., because the object is marked UNINTERESTING,
    or because revs->ignore_missing_links is set).
    
    We can fix this by adding a new "parse_X_gently" form that
    takes a flag to suppress the messages. The existing
    "parse_X" form is already gentle in the sense that it
    returns an error rather than dying, and we could in theory
    just add a "quiet" flag to it (with existing callers passing
    "0"). But doing it this way means we do not have to disturb
    existing callers.
    
    Note also that the new flag is "quiet_on_missing", and not
    just "quiet". We could add a flag to suppress _all_ errors,
    but besides being a more invasive change (we would have to
    pass the flag down to sub-functions, too), there is a good
    reason not to: we would never want to use it. Missing a
    linked object is expected in some circumstances, but it is
    never expected to have a malformed commit, or to get a tree
    when we wanted a commit.  We should always complain about
    these corruptions.
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    cregit: 9cc2b07a7c95fad0bb5e3a7a8db29bebdb90d92b http://github.com/git/git/commit/9cc2b07a7c95fad0bb5e3a7a8db29bebdb90d92b

--- tree.c
+++ tree.c
@@ -1088,14 +1088,17 @@ return|;
 block|}
 end_function
 begin_function
-DECL|function|parse_tree
+DECL|function|parse_tree_gently
 name|int
-name|parse_tree
+name|parse_tree_gently
 parameter_list|(
 name|struct
 name|tree
 modifier|*
 name|item
+parameter_list|,
+name|int
+name|quiet_on_missing
 parameter_list|)
 block|{
 name|enum
@@ -1144,6 +1147,11 @@ operator|!
 name|buffer
 condition|)
 return|return
+name|quiet_on_missing
+condition|?
+operator|-
+literal|1
+else|:
 name|error
 argument_list|(
 literal|"Could not read %s"
