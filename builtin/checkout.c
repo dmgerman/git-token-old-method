d792c1694deb3fafd5df2b75cac07ad0e721e3d2
Eric Sunshine <sunshine@sunshineco.com>
2015-07-06 13:30:47 -0400

    checkout: prepare_linked_checkout: drop now-unused 'new' argument
    The only references to 'new' were folded out by the last two patches.
    
    Signed-off-by: Eric Sunshine <sunshine@sunshineco.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    cregit: 3c3e7f5b57f94b8f5fcba5042cf2412625c24d4a http://github.com/git/git/commit/3c3e7f5b57f94b8f5fcba5042cf2412625c24d4a

--- builtin/checkout.c
+++ builtin/checkout.c
@@ -4616,11 +4616,6 @@ name|struct
 name|checkout_opts
 modifier|*
 name|opts
-parameter_list|,
-name|struct
-name|branch_info
-modifier|*
-name|new
 parameter_list|)
 block|{
 name|struct
@@ -6867,8 +6862,6 @@ return|return
 name|prepare_linked_checkout
 argument_list|(
 name|opts
-argument_list|,
-name|new
 argument_list|)
 return|;
 block|}
