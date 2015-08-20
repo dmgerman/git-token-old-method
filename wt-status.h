50d129b24087e51a6c9cd825e06d78c614da933b
SZEDER Gábor <szeder@ira.uka.de>
2015-08-20 16:06:27 +0200

    wt-status: move #include "pathspec.h" to the header
    The declaration of 'struct wt_status' requires the declararion of 'struct
    pathspec'.
    
    Signed-off-by: SZEDER Gábor <szeder@ira.uka.de>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    cregit: 7aa9b9ba02c5cfab5078cc7a7b250bbbb74b316f http://github.com/git/git/commit/7aa9b9ba02c5cfab5078cc7a7b250bbbb74b316f

--- wt-status.h
+++ wt-status.h
@@ -25,6 +25,11 @@ include|#
 directive|include
 file|"color.h"
 end_include
+begin_include
+include|#
+directive|include
+file|"pathspec.h"
+end_include
 begin_enum
 DECL|enum|color_wt_status
 enum|enum
