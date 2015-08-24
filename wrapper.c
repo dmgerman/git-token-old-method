eecbcdeacb1cdd119a5c57ea97e6020358de1fc9
Junio C Hamano <gitster@pobox.com>
2015-08-24 09:39:48 -0700

    write_file_v(): do not leave incomplete line at the end
    All existing callers to this function use it to produce a text file
    or an empty file, and a new callsite that mimick them must end their
    payload with a LF.  If they forget to do so, the resulting file will
    end with an incomplete line.
    
    Teach write_file_v() to complete the incomplete line, if exists, so
    that the callers do not have to.
    
    With this, the caller-side fix in builtin/am.c becomes unnecessary.
    
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    cregit: e7ffa38c6e726e8014b76297b06f78e008deb2d0 http://github.com/git/git/commit/e7ffa38c6e726e8014b76297b06f78e008deb2d0

--- wrapper.c
+++ wrapper.c
@@ -3002,6 +3002,12 @@ argument_list|,
 name|params
 argument_list|)
 expr_stmt|;
+name|strbuf_complete_line
+argument_list|(
+operator|&
+name|sb
+argument_list|)
+expr_stmt|;
 if|if
 condition|(
 name|write_in_full
