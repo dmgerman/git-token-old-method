77d90c29befea011ac634d7ea1c67f1aec0665ae
Jeff King <peff@peff.net>
2015-09-24 17:06:44 -0400

    find_short_object_filename: convert sprintf to xsnprintf
    We use sprintf() to format some hex data into a buffer. The
    buffer is clearly long enough, and using snprintf here is
    not necessary. And in fact, it does not really make anything
    easier to audit, as the size we feed to snprintf accounts
    for the magic extra 42 bytes found in each alt->name field
    of struct alternate_object_database (which is there exactly
    to do this formatting).
    
    Still, it is nice to remove an sprintf call and replace it
    with an xsnprintf and explanatory comment, which makes it
    easier to audit the code base for overflows.
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    cregit: c3bb0ac796c21490f478914441526817e4685606 http://github.com/git/git/commit/c3bb0ac796c21490f478914441526817e4685606

--- sha1_name.c
+++ sha1_name.c
@@ -465,9 +465,14 @@ name|next
 operator|=
 name|alt_odb_list
 expr_stmt|;
-name|sprintf
+name|xsnprintf
 argument_list|(
 name|hex
+argument_list|,
+sizeof|sizeof
+argument_list|(
+name|hex
+argument_list|)
 argument_list|,
 literal|"%.2s"
 argument_list|,
@@ -503,11 +508,14 @@ name|DIR
 modifier|*
 name|dir
 decl_stmt|;
-name|sprintf
+comment|/* 		 * every alt_odb struct has 42 extra bytes after the base 		 * for exactly this purpose 		 */
+name|xsnprintf
 argument_list|(
 name|alt
 operator|->
 name|name
+argument_list|,
+literal|42
 argument_list|,
 literal|"%.2s/"
 argument_list|,
