e5b19a9f277de9b91469a194262cbda443a624c4
Johannes Schindelin <johannes.schindelin@gmx.de>
2015-10-26 14:14:58 +0100

    imap-send: only use CURLOPT_LOGIN_OPTIONS if it is actually available
    This fixes the compilation on an older Linux that was used to debug
    test failures when upgrading Git for Windows to Git v2.3.0.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    cregit: 71d92575251bdb8b5e1b5b3a5c649bb63d9fc116 http://github.com/git/git/commit/71d92575251bdb8b5e1b5b3a5c649bb63d9fc116

--- imap-send.c
+++ imap-send.c
@@ -7863,6 +7863,18 @@ operator|.
 name|auth_method
 condition|)
 block|{
+if|#
+directive|if
+name|LIBCURL_VERSION_NUM
+operator|<
+literal|0x072200
+name|warning
+argument_list|(
+literal|"No LOGIN_OPTIONS support in this cURL version"
+argument_list|)
+expr_stmt|;
+else|#
+directive|else
 name|struct
 name|strbuf
 name|auth
@@ -7904,6 +7916,8 @@ operator|&
 name|auth
 argument_list|)
 expr_stmt|;
+endif|#
+directive|endif
 block|}
 if|if
 condition|(
