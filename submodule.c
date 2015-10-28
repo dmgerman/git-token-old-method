67a06deee6195b40d9d891ac24832f300e802da5
Jeff King <peff@peff.net>
2015-10-28 10:07:25 -0400

    add_submodule_odb: initialize alt_odb list earlier
    The add_submodule_odb function tries to add a submodule's
    object store as an "alternate". It needs the existing list
    to be initialized (from the objects/info/alternates file)
    for two reasons:
    
      1. We look for duplicates with the existing alternate
         stores, but obviously this doesn't work if we haven't
         loaded any yet.
    
      2. We link our new entry into the list by prepending it to
         alt_odb_list. But we do _not_ modify alt_odb_tail.
         This variable starts as NULL, and is a signal to the
         alt_odb code that the list has not yet been
         initialized.
    
         We then call read_info_alternates on the submodule (to
         recursively load its alternates), which will try to
         append to that tail, assuming it has been initialized.
         This causes us to segfault if it is NULL.
    
    This rarely comes up in practice, because we will have
    initialized the alt_odb any time we do an object lookup. So
    you can trigger this only when:
    
      - you try to access a submodule (e.g., a diff with
        diff.submodule=log)
    
      - the access happens before any other object has been
        accessed (e.g., because the diff is between the working
        tree and the index)
    
      - the submodule contains an alternates file (so we try to
        add an entry to the NULL alt_odb_tail)
    
    To fix this, we just need to call prepare_alt_odb at the
    start of the function (and if we have already initialized,
    it is a noop).
    
    Note that we can remove the prepare_alt_odb call from the
    end. It is guaranteed to be a noop, since we will have
    called it earlier.
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    cregit: 9a6e4f032ef7ca45a71639ff5e60cbb9b8a6e556 http://github.com/git/git/commit/9a6e4f032ef7ca45a71639ff5e60cbb9b8a6e556

--- submodule.c
+++ submodule.c
@@ -234,6 +234,9 @@ name|done
 goto|;
 block|}
 comment|/* avoid adding it twice */
+name|prepare_alt_odb
+argument_list|()
+expr_stmt|;
 for|for
 control|(
 name|alt_odb
@@ -367,9 +370,6 @@ name|buf
 argument_list|,
 literal|0
 argument_list|)
-expr_stmt|;
-name|prepare_alt_odb
-argument_list|()
 expr_stmt|;
 name|done
 label|:
