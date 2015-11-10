68b7e6791056797e18b8a85778f3c266a09942ad
brian m. carlson <sandals@crustytoothpaste.net>
2015-11-10 02:22:26 +0000

    object: introduce get_object_hash macro.
    This macro is a temporary change to ease the transition of struct object
    to use struct object_id.  It takes an argument of struct object and
    returns the object's hash.  Provide this hash next to struct object for
    easier conversion.
    
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>
    Signed-off-by: Jeff King <peff@peff.net>
    
    cregit: 3c4270107fec2c1d85b7b1a6f8b0aeebf3193b28 http://github.com/git/git/commit/3c4270107fec2c1d85b7b1a6f8b0aeebf3193b28

--- object.h
+++ object.h
@@ -145,6 +145,16 @@ decl_stmt|;
 block|}
 struct|;
 end_struct
+begin_define
+DECL|macro|get_object_hash
+define|#
+directive|define
+name|get_object_hash
+parameter_list|(
+name|x
+parameter_list|)
+value|((x).sha1)
+end_define
 begin_function_decl
 specifier|extern
 specifier|const
