df6d581f94b496eeae000b2eef1971f8e5b09776
Jeff King <peff@peff.net>
2015-12-10 16:33:05 -0500

    ident: make xgetpwuid_self() a static local helper
    This function is defined in wrapper.c, but nobody besides
    ident.c uses it. And nobody is likely to in the future,
    either, as anything that cares about the user's name should
    be going through the ident code.
    
    Moving it here is a cleanup of the global namespace, but it
    will also enable further cleanups inside ident.c.
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    cregit: e850194c83985396435b01b70d2db00b6d3af02e http://github.com/git/git/commit/e850194c83985396435b01b70d2db00b6d3af02e

--- git-compat-util.h
+++ git-compat-util.h
@@ -5027,19 +5027,6 @@ name|path
 parameter_list|)
 function_decl|;
 end_function_decl
-begin_comment
-comment|/* Get the passwd entry for the UID of the current process. */
-end_comment
-begin_function_decl
-name|struct
-name|passwd
-modifier|*
-name|xgetpwuid_self
-parameter_list|(
-name|void
-parameter_list|)
-function_decl|;
-end_function_decl
 begin_ifdef
 ifdef|#
 directive|ifdef
