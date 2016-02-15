e4526d9fb77905cc71c3432570d706368e0cc2df
Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
2016-02-15 16:03:36 +0700

    dir.c: fix match_pathname()
    Given the pattern "1/2/3/4" and the path "1/2/3/4/f", the pattern
    prefix is "1/2/3/4". We will compare and remove the prefix from both
    pattern and path and come to this code
    
    	/*
    	 * If the whole pattern did not have a wildcard,
    	 * then our prefix match is all we need; we
    	 * do not need to call fnmatch at all.
    	 */
    	if (!patternlen && !namelen)
    		return 1;
    
    where patternlen is zero (full pattern consumed) and the remaining
    path in "name" is "/f". We fail to realize it's matched in this case
    and fall back to fnmatch(), which also fails to catch it. Fix it.
    
    Signed-off-by: Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    cregit: a60ea8fb66945a886ea53fd3f41e61cc5fb3201e http://github.com/git/git/commit/a60ea8fb66945a886ea53fd3f41e61cc5fb3201e

--- dir.c
+++ dir.c
@@ -4142,8 +4142,15 @@ condition|(
 operator|!
 name|patternlen
 operator|&&
+operator|(
 operator|!
 name|namelen
+operator|||
+operator|*
+name|name
+operator|==
+literal|'/'
+operator|)
 condition|)
 return|return
 literal|1
