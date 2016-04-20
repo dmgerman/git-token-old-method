f910f7300ae0ac29b4462dbbb1f51e6531a88e3f
Johannes Schindelin <johannes.schindelin@gmx.de>
2016-04-20 08:38:03 +0200

    replace --edit: respect core.editor
    We simply need to read the config, is all.
    
    This fixes https://github.com/git-for-windows/git/issues/733
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    cregit: 36b14370dbafbe6682ea6b21c8de012253bff451 http://github.com/git/git/commit/36b14370dbafbe6682ea6b21c8de012253bff451

--- builtin/replace.c
+++ builtin/replace.c
@@ -2490,6 +2490,13 @@ name|check_replace_refs
 operator|=
 literal|0
 expr_stmt|;
+name|git_config
+argument_list|(
+name|git_default_config
+argument_list|,
+name|NULL
+argument_list|)
+expr_stmt|;
 name|argc
 operator|=
 name|parse_options
