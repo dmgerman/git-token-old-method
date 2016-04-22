645eff619c4f95c07530498773c168beb6dd4e74
Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
2016-04-22 20:01:31 +0700

    wt-status.c: make wt_status_check_rebase() work on any worktree
    This is a preparation step for find_shared_symref() to detect if any
    worktree is being rebased.
    
    Signed-off-by: Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    cregit: 81eff27b0fb652f80a43bdf18c18cd9bc25840df http://github.com/git/git/commit/81eff27b0fb652f80a43bdf18c18cd9bc25840df

--- wt-status.h
+++ wt-status.h
@@ -30,6 +30,11 @@ include|#
 directive|include
 file|"pathspec.h"
 end_include
+begin_struct_decl
+struct_decl|struct
+name|worktree
+struct_decl|;
+end_struct_decl
 begin_enum
 DECL|enum|color_wt_status
 enum|enum
@@ -449,6 +454,12 @@ begin_function_decl
 name|int
 name|wt_status_check_rebase
 parameter_list|(
+specifier|const
+name|struct
+name|worktree
+modifier|*
+name|wt
+parameter_list|,
 name|struct
 name|wt_status_state
 modifier|*
