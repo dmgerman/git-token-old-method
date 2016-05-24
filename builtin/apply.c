f914daefdbc19847cc14971e13060426931306d9
Christian Couder <christian.couder@gmail.com>
2016-05-24 10:10:56 +0200

    builtin/apply: move 'diffstat' global into 'struct apply_state'
    To libify the apply functionality the 'diffstat' variable should
    not be static and global to the file. Let's move it into
    'struct apply_state'.
    
    Reviewed-by: Stefan Beller <sbeller@google.com>
    Signed-off-by: Christian Couder <chriscool@tuxfamily.org>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    cregit: c4f5c39862671e7de1ffca959220ef94385b1972 http://github.com/git/git/commit/c4f5c39862671e7de1ffca959220ef94385b1972

--- builtin/apply.c
+++ builtin/apply.c
@@ -108,6 +108,12 @@ name|int
 name|update_index
 decl_stmt|;
 comment|/* check_index&& apply */
+comment|/* These control cosmetic aspect of the output */
+DECL|member|diffstat
+name|int
+name|diffstat
+decl_stmt|;
+comment|/* just show a diffstat, and don't actually apply */
 comment|/* These boolean parameters control how the apply is done */
 DECL|member|allow_overlap
 name|int
@@ -133,7 +139,7 @@ block|}
 struct|;
 end_struct
 begin_comment
-comment|/*  *  --stat does just a diffstat, and doesn't actually apply  *  --numstat does numeric diffstat, and doesn't actually apply  *  --index-info shows the old and new index info for paths if available.  */
+comment|/*  *  --numstat does numeric diffstat, and doesn't actually apply  *  --index-info shows the old and new index info for paths if available.  */
 end_comment
 begin_decl_stmt
 DECL|variable|newfd
@@ -159,13 +165,6 @@ DECL|variable|p_value_known
 specifier|static
 name|int
 name|p_value_known
-decl_stmt|;
-end_decl_stmt
-begin_decl_stmt
-DECL|variable|diffstat
-specifier|static
-name|int
-name|diffstat
 decl_stmt|;
 end_decl_stmt
 begin_decl_stmt
@@ -21825,6 +21824,8 @@ argument_list|)
 expr_stmt|;
 if|if
 condition|(
+name|state
+operator|->
 name|diffstat
 condition|)
 name|stat_patch_list
@@ -22401,6 +22402,8 @@ argument_list|,
 literal|"stat"
 argument_list|,
 operator|&
+name|state
+operator|.
 name|diffstat
 argument_list|,
 name|N_
@@ -22904,6 +22907,8 @@ operator|!
 name|force_apply
 operator|&&
 operator|(
+name|state
+operator|.
 name|diffstat
 operator|||
 name|numstat
