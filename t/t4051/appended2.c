bec1182a607093b38cc7b1f3963e24204a8f699a
René Scharfe <l.s.r@web.de>
2016-05-31 22:00:38 +0200

    t4051: rewrite, add more tests
    Remove the tests that checked against a fixed result and replace them
    with more focused checks of desired properties of the created diffs.
    That way we get more detailed and meaningful diagnostics.
    
    Store test file contents in files in a subdirectory in order to avoid
    cluttering the test script with them.
    
    Use tagged commits to store the changes to test diff -W against instead
    of using changes to the worktree.  Use the worktree instead to try and
    apply the generated patch in order to validate it.
    
    Document unwanted features: trailing empty lines, too much context for
    appended functions, insufficient context at the end with -U0.
    
    Helped-by: Junio C Hamano <gitster@pobox.com>
    Signed-off-by: Rene Scharfe <l.s.r@web.de>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    cregit: d3621de789ab57739f48b065751089d828b50240 http://github.com/git/git/commit/d3621de789ab57739f48b065751089d828b50240

---
+++ t/t4051/appended2.c
@@ -0,0 +1,21 @@
+begin_unit
+begin_expr_stmt
+name|printf
+argument_list|(
+literal|"Begin of second part\n"
+argument_list|)
+expr_stmt|;
+end_expr_stmt
+begin_comment
+comment|/* 	 * Lorem ipsum dolor sit amet, consectetuer sadipscing elitr, 	 * sed diam nonumy eirmod tempor invidunt ut labore et dolore 	 * magna aliquyam erat, sed diam voluptua. At vero eos et 	 * accusam et justo duo dolores et ea rebum. Stet clita kasd 	 * gubergren, no sea takimata sanctus est Lorem ipsum dolor 	 * sit amet. 	 * 	 * Lorem ipsum dolor sit amet, consectetuer sadipscing elitr, 	 * sed diam nonumy eirmod tempor invidunt ut labore et dolore 	 * magna aliquyam erat, sed diam voluptua. At vero eos et 	 * accusam et justo duo dolores et ea rebum. Stet clita kasd 	 * gubergren, no sea takimata sanctus est Lorem ipsum dolor 	 * sit amet. 	 * 	 * Lorem ipsum dolor sit amet, consectetuer sadipscing elitr, 	 * sed diam nonumy eirmod tempor invidunt ut labore et dolore 	 * magna aliquyam erat, sed diam voluptua. At vero eos et 	 * accusam et justo duo dolores et ea rebum. Stet clita kasd 	 * gubergren, no sea takimata sanctus est Lorem ipsum dolor 	 * sit amet. 	 * 	 * Lorem ipsum dolor sit amet, consectetuer sadipscing elitr, 	 * sed diam nonumy eirmod tempor invidunt ut labore et dolore 	 * magna aliquyam erat, sed diam voluptua. At vero eos et 	 * accusam et justo duo dolores et ea rebum. Stet clita kasd 	 * gubergren, no sea takimata sanctus est Lorem ipsum dolor 	 * sit amet. 	 * 	 */
+end_comment
+begin_return
+return|return
+literal|0
+return|;
+end_return
+begin_comment
+unit|}
+comment|// End of second part
+end_comment
+end_unit
